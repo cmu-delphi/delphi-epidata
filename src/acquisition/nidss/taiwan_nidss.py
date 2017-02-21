769ae3fe9ff2dd158515c7719a4909a40960886c
undefx <dfarrow0@gmail.com>
2017-02-20 21:54:13 -0800

    update nidss

--- src/acquisition/nidss/taiwan_nidss.py
+++ src/acquisition/nidss/taiwan_nidss.py
@@ -1,4 +1,4 @@
-'''
+"""
 ===============
 === Purpose ===
 ===============
@@ -11,33 +11,35 @@ System (NIDSS): http://nidss.cdc.gov.tw/en/
 === Changelog ===
 =================
 
+2017-02-20
+  * update for significant website changes (flu only)
 2016-07-19
- + Sample/debug usage in function `main`
- * Update for change to NIDSS website (__EVENTVALIDATION -> __VIEWSTATEGENERATOR)
+  + Sample/debug usage in function `main`
+  * Update for change to NIDSS website (__EVENTVALIDATION -> __VIEWSTATEGENERATOR)
 2015-10-14
- * Update for change to NIDSS website (page starts on flu by default now)
+  * Update for change to NIDSS website (page starts on flu by default now)
 2015-08-20
- + Aggregate dengue data
+  + Aggregate dengue data
 2015-08-10
- * Original version, inspired by healthtweets.py
-'''
+  * Original version, inspired by healthtweets.py
+"""
 
-# built-in libraries
+# standard library
 import argparse
 import base64
 import re
 import urllib.parse
-# external libraries
+# third party
 import requests
-# local files
-from epiweek import range_epiweeks, add_epiweeks, delta_epiweeks
+# first party
+from epiweek import range_epiweeks, add_epiweeks, delta_epiweeks, check_epiweek
 
 
 class NIDSS:
-  ''' An API for scraping the NIDSS site '''
+  """An API for scraping the NIDSS site."""
 
   # The page where the flu data is kept
-  BASE_URL = 'http://nidss.cdc.gov.tw/en/CDCWNH01.aspx?dc=wnh'
+  FLU_URL = 'https://nidss.cdc.gov.tw/en/CDCWNH01.aspx?dc=wnh'
 
   # Link to the dengue data
   DENGUE_URL = 'http://nidss.cdc.gov.tw/Download/Weekly_Age_County_Gender_061.csv'
@@ -103,33 +105,8 @@ class NIDSS:
     'Taitung_County': 'Eastern',
   }
 
-  def __init__(self):
-    # Start a new session
-    self.client = requests.Session()
-    # Spoof the user agent
-    self.client.headers.update({
-      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.130 Safari/537.36',
-    })
-    # Get the required metadata
-    response = self.client.get(NIDSS.BASE_URL)
-    if response.status_code != 200:
-      raise Exception('initial request failed [%d]' % response.status_code)
-    self.latest_week, self.release_date = self._get_metadata(response.text)
-    magic_params = self._get_magic_paramters(response.text)
-    ## Switch to Influenza
-    #epi_params = self._get_epi_parameters(self.latest_week, self.latest_week, False)
-    #event_params = self._get_event_parameters('ctl00$NIDSSContentPlace$CDCWNH_query1$ICD_CTGRY')
-    #data = {}
-    #data.update(magic_params)
-    #data.update(epi_params)
-    #data.update(event_params)
-    #response = self.client.post(NIDSS.BASE_URL, data=data)
-    #if response.status_code != 200:
-    #  raise Exception('category change failed [%d]' % response.status_code)
-    # Get the required metadata
-    self.magic_params = self._get_magic_paramters(response.text)
-
-  def _get_metadata(self, html):
+  @staticmethod
+  def _get_metadata(html):
     issue_pattern = re.compile('^.*Latest available data: Week (\\d+), (\\d{4})\\..*$')
     release_pattern = re.compile('^.*Data as of \\d+:\\d+:\\d+, (\\d{4})/(\\d{2})/(\\d{2})\\..*$')
     issue, release = None, None
@@ -146,156 +123,59 @@ class NIDSS:
       raise Exception('metadata not found')
     return issue, release
 
-  def _get_magic_paramters(self, html):
-    lines = [line.strip() for line in html.split('\n') if 'input type="hidden"' in line]
-    params = {}
-    for line in lines:
-      name, value = None, None
-      parts = line.split(' ')
-      for part in parts:
-        if part[:5] == 'name=':
-          name = part[6:-1]
-        if part[:6] == 'value=':
-          value = part[7:-1]
-      if name is not None and value is not None:
-        params[name] = value
-    #if '__VIEWSTATE' not in params or '__EVENTVALIDATION' not in params:
-    if '__VIEWSTATE' not in params or '__VIEWSTATEGENERATOR' not in params:
-      raise Exception('magic parameters not found')
-    return params
-
-  def _get_event_parameters(self, target=None):
-    params = {
-      '__EVENTARGUMENT': '',
-      '__LASTFOCUS': '',
-    }
-    if target is not None:
-      params['__EVENTTARGET'] = target
-    return params
-
-  def _get_epi_parameters(self, yearweek_start, yearweek_end, influenza):
-    category = urllib.parse.unquote('%E9%A1%9E%E6%B5%81%E6%84%9F')
-    year1, week1 = yearweek_start // 100, yearweek_start % 100
-    year2, week2 = yearweek_end // 100, yearweek_end % 100
-    if influenza:
-      age, sub = 'rb_AGE2_0', '4'
-    else:
-      age, sub = 'rb_AGE1_0', '1'
-    return {
-      'ctl00$NIDSSContentPlace$CDCWNH_query1$ddl_DateType': 'yw',
-      'ctl00$NIDSSContentPlace$CDCWNH_query1$ddl_yw_y_s': '%04d' % (year1),
-      'ctl00$NIDSSContentPlace$CDCWNH_query1$ddl_yw_w_s': '%02d' % (week1),
-      'ctl00$NIDSSContentPlace$CDCWNH_query1$ddl_yw_y_e': '%04d' % (year2),
-      'ctl00$NIDSSContentPlace$CDCWNH_query1$ddl_yw_w_e': '%02d' % (week2),
-      'ctl00$NIDSSContentPlace$CDCWNH_query1$ddl_CASE_TYPE': '[1]',
-      'ctl00$NIDSSContentPlace$CDCWNH_query1$ICD_CTGRY': category,
-      'ctl00$NIDSSContentPlace$CDCWNH_query1$ICD_SUBCTGRY': sub,
-      'ctl00$NIDSSContentPlace$CDCWNH_query1$cb_AREA0': 'on',
-      'ctl00$NIDSSContentPlace$CDCWNH_query1$cb_AREA1': 'on',
-      'ctl00$NIDSSContentPlace$CDCWNH_query1$cb_AREA2': 'on',
-      'ctl00$NIDSSContentPlace$CDCWNH_query1$cb_AREA3': 'on',
-      'ctl00$NIDSSContentPlace$CDCWNH_query1$cb_AREA4': 'on',
-      'ctl00$NIDSSContentPlace$CDCWNH_query1$cb_AREA5': 'on',
-      'ctl00$NIDSSContentPlace$CDCWNH_query1$cb_AREA6': 'on',
-      'ctl00$NIDSSContentPlace$CDCWNH_query1$AGE': age,
-    }
-
-  def _parse_table(self, html):
-    # Just a quick hack to extract values from an HTML table
-    html = html.strip()
-    html = html[html.find('</tr>') + 9:-19]
-    rows = html.split('</tr><tr>')
-    for (i, row) in enumerate(rows):
-      row = row.replace('/', '')
-      row = row.replace('<th>', '<td>')
-      row = [x.strip() for x in row.split('<td>') if x != '']
-      rows[i] = row
-    header, data = rows[0], rows[1:]
-    locations = []
-    for j in range(len(data)):
-      locations.append(data[j][0])
-      data[j] = data[j][1:]
-    result = {}
-    for i in range(len(header)):
-      week = int(header[i])
-      for j in range(len(data)):
-        location = locations[j]
-        if week not in result:
-          result[week] = {}
-        result[week][location] = data[j][i]
-    # Return results indexed by week and location
-    return result
-
-  def get_latest_week(self):
-    return self.latest_week
-
-  def get_release_date(self):
-    return self.release_date
-
-  def get_flu_data(self, first_week, last_week):
-    # Check week order
-    if first_week > last_week:
-      first_week, last_week = last_week, first_week
-    # Bounds check
-    if not 200814 <= first_week <= self.latest_week:
-      raise Exception('first week out of range')
-    if not 200814 <= last_week <= self.latest_week:
-      raise Exception('last week out of range')
-    # Update the "page"
-    epi_params = self._get_epi_parameters(first_week, last_week, True)
-    event_params = self._get_event_parameters()
-    data = {
-      'ctl00$NIDSSContentPlace$CDCWNH_query1$btnSend': 'Query',
-    }
-    data.update(self.magic_params)
-    data.update(epi_params)
-    data.update(event_params)
-    response = self.client.post(NIDSS.BASE_URL, data=data)
-    if response.status_code != 200:
-      raise Exception('query failed [%d]' % response.status_code)
-    magic_params = self._get_magic_paramters(response.text)
-    # Export ILI
-    data = {
-      'ctl00$NIDSSContentPlace$btnExcel1.x': '0',
-      'ctl00$NIDSSContentPlace$btnExcel1.y': '0',
-    }
-    data.update(magic_params)
-    data.update(epi_params)
-    response = self.client.post(NIDSS.BASE_URL, data=data)
-    if response.status_code != 200:
-      raise Exception('export ILI failed [%d]' % response.status_code)
-    ili = response.text
-    # Export visits
-    data = {
-      'ctl00$NIDSSContentPlace$btnExcel2.x': '0',
-      'ctl00$NIDSSContentPlace$btnExcel2.y': '0',
-    }
-    data.update(magic_params)
-    data.update(epi_params)
-    response = self.client.post(NIDSS.BASE_URL, data=data)
-    if response.status_code != 200:
-      raise Exception('export visits failed [%d]' % response.status_code)
-    visits = response.text
-    # Parse the data
-    ili, visits = self._parse_table(ili), self._parse_table(visits)
-    weeks1, weeks2 = sorted(list(ili.keys())), sorted(list(visits.keys()))
-    # Sanity check
-    if weeks1 != weeks2:
-      raise Exception('ILI and visits are reported on different weeks')
-    # Merge ILI and visits
+  @staticmethod
+  def _get_flu_data(html):
+    week_pattern = re.compile('^categories: \\[(.*)\\],$')
+    value_pattern = re.compile('^series: \\[(.*)\\],$')
     data = {}
-    for week in weeks1:
-      data[week] = {}
-      for location in ili[week].keys():
-        data[week][location] = {
-          'ili': float(ili[week][location]),
-          'visits': int(visits[week][location]),
-        }
-    # Return results indexed by week and location
+    parsing_ili = True
+    for line in html.split('\n'):
+      line = line.strip()
+      match = week_pattern.match(line)
+      if match is not None:
+        weeks = [int(x[1:-1]) for x in match.group(1).split(',')]
+        for week in weeks:
+          check_epiweek(week)
+          if week not in data:
+            data[week] = {}
+      match = value_pattern.match(line)
+      if match is not None:
+        for item in match.group(1).split('},{'):
+          parts = item.replace('{', '').replace('}', '').strip().split(' ')
+          location = parts[1][1:-2]
+          def num(value):
+            if parsing_ili:
+              return float(value)
+            else:
+              if '.' in value:
+                raise Exception('expected type int for visits')
+              return int(value)
+          values = [num(x) for x in parts[3][1:-1].split(',')]
+          unit = 'ili' if parsing_ili else 'visits'
+          if len(weeks) != len(values):
+            raise Exception('len(weeks) != len(values)')
+          for week, value in zip(weeks, values):
+            if location not in data[week]:
+              data[week][location] = {}
+            data[week][location][unit] = value
+        parsing_ili = False
+    if len(data) == 0:
+      raise Exception('no data')
     return data
 
-  def get_dengue_data(self, first_week, last_week):
-    return NIDSS.get_dengue_data()
+  @staticmethod
+  def get_flu_data():
+    # Fetch the flu page
+    response = requests.get(NIDSS.FLU_URL)
+    if response.status_code != 200:
+      raise Exception('request failed [%d]' % response.status_code)
+    html = response.text
+    # Parse metadata
+    latest_week, release_date = NIDSS._get_metadata(html)
+    # Parse flu data
+    data = NIDSS._get_flu_data(html)
+    # Return results indexed by week and location
+    return latest_week, release_date, data
 
   @staticmethod
   def get_dengue_data(first_week, last_week):
@@ -355,16 +235,23 @@ class NIDSS:
 def main():
   # Args and usage
   parser = argparse.ArgumentParser()
-  parser.add_argument('epiweek', action='store', type=int, help='fetch data on this epiweek (ex: 201537)')
+  parser.add_argument(
+    'epiweek',
+    action='store',
+    type=int,
+    help='fetch data on this epiweek (ex: 201537)'
+  )
   args = parser.parse_args()
   ew = args.epiweek
 
   # Get the data
-  api = NIDSS()
-  fdata = api.get_flu_data(ew, ew)
-  ddata = api.get_dengue_data(ew, ew)
+  latest_week, release_date, fdata = NIDSS.get_flu_data()
+  ddata = NIDSS.get_dengue_data(ew, ew)
 
   # Print the results
+  print('*** Meta ***')
+  print('latest_week:', latest_week)
+  print('release_date:', release_date)
   print('*** Flu ***')
   for region in sorted(list(fdata[ew].keys())):
     visits, ili = fdata[ew][region]['visits'], fdata[ew][region]['ili']
--- src/acquisition/nidss/taiwan_update.py
+++ src/acquisition/nidss/taiwan_update.py
@@ -61,18 +61,23 @@ count: the number of dengue cases
 === Changelog ===
 =================
 
+2017-02-20
+  + test mode
+  * API updated
+  * better organization
 2017-02-16
- * use secrets
+  * use secrets
 2015-08-20
- + Also storing dengue data
+  + Also storing dengue data
 2015-08-10
- * Original version, inspired by load_epidata_fluview.py
+  * Original version, inspired by load_epidata_fluview.py
 """
 
-# built-in libraries
-# external libraries
+# standard library
+import argparse
+# third party
 import mysql.connector
-# local libraries
+# first party
 from epiweek import *
 from taiwan_nidss import NIDSS
 import secrets
@@ -90,58 +95,82 @@ def get_rows(cnx):
   select.close()
   return (rows_flu, rows_dengue)
 
-# API initialization
-api = NIDSS()
-current_week = api.get_latest_week()
-release_date = api.get_release_date()
 
-# Database connection
-u, p = secrets.db.epi
-cnx = mysql.connector.connect(user=u, password=p, database='epidata')
-rows1 = get_rows(cnx)
-print('rows before (flu): %d' % (rows1[0]))
-print('rows before (dengue): %d' % (rows1[1]))
-insert = cnx.cursor()
-sql_flu = '''
-INSERT INTO
-  `nidss_flu` (`release_date`, `issue`, `epiweek`, `region`, `lag`, `visits`, `ili`)
-VALUES
-  (%s, %s, %s, %s, %s, %s, %s)
-ON DUPLICATE KEY UPDATE
-  `release_date` = least(`release_date`, %s), `visits` = %s, `ili` = %s
-'''
-sql_dengue = '''
-INSERT INTO
-  `nidss_dengue` (`epiweek`, `location`, `region`, `count`)
-VALUES
-  (%s, %s, %s, %s)
-ON DUPLICATE KEY UPDATE
-  `count` =  %s
-'''
+def update(test_mode=False):
+  # test mode
+  if test_mode:
+    print('test mode enabled: changes will not be saved')
 
-# Scrape the flu data from the past year
-data = api.get_flu_data(add_epiweeks(current_week, -51), current_week)
-for epiweek in sorted(list(data.keys())):
-  lag = delta_epiweeks(epiweek, current_week)
-  for region in data[epiweek].keys():
-    visits, ili = data[epiweek][region]['visits'], data[epiweek][region]['ili']
-    params1 = [release_date, current_week, epiweek, region, lag, visits, ili]
-    params2 = [release_date, visits, ili]
-    insert.execute(sql_flu, tuple(params1 + params2))
+  # Database connection
+  u, p = secrets.db.epi
+  cnx = mysql.connector.connect(user=u, password=p, database='epidata')
+  rows1 = get_rows(cnx)
+  print('rows before (flu): %d' % (rows1[0]))
+  print('rows before (dengue): %d' % (rows1[1]))
+  insert = cnx.cursor()
+  sql_flu = '''
+  INSERT INTO
+    `nidss_flu` (`release_date`, `issue`, `epiweek`, `region`, `lag`, `visits`, `ili`)
+  VALUES
+    (%s, %s, %s, %s, %s, %s, %s)
+  ON DUPLICATE KEY UPDATE
+    `release_date` = least(`release_date`, %s), `visits` = %s, `ili` = %s
+  '''
+  sql_dengue = '''
+  INSERT INTO
+    `nidss_dengue` (`epiweek`, `location`, `region`, `count`)
+  VALUES
+    (%s, %s, %s, %s)
+  ON DUPLICATE KEY UPDATE
+    `count` =  %s
+  '''
 
-# Scrape the dengue data from the past year
-data = api.get_dengue_data(add_epiweeks(current_week, -51), current_week)
-for epiweek in sorted(list(data.keys())):
-  for location in sorted(list(data[epiweek].keys())):
-    region = NIDSS.LOCATION_TO_REGION[location]
-    count = data[epiweek][location]
-    params = (epiweek, location, region, count, count)
-    insert.execute(sql_dengue, params)
+  # Scrape flu data
+  current_week, release_date, data = api.get_flu_data()
+  for epiweek in sorted(list(data.keys())):
+    lag = delta_epiweeks(epiweek, current_week)
+    for region in data[epiweek].keys():
+      visits, ili = data[epiweek][region]['visits'], data[epiweek][region]['ili']
+      params1 = [release_date, current_week, epiweek, region, lag, visits, ili]
+      params2 = [release_date, visits, ili]
+      insert.execute(sql_flu, tuple(params1 + params2))
 
-# Cleanup
-insert.close()
-cnx.commit()
-rows2 = get_rows(cnx)
-print('rows after (flu): %d (added %d)' % (rows2[0], rows2[0] - rows1[0]))
-print('rows after (dengue): %d (added %d)' % (rows2[1], rows2[1] - rows1[1]))
-cnx.close()
+  # Scrape dengue data from the past year
+  data = api.get_dengue_data(add_epiweeks(current_week, -51), current_week)
+  for epiweek in sorted(list(data.keys())):
+    for location in sorted(list(data[epiweek].keys())):
+      region = NIDSS.LOCATION_TO_REGION[location]
+      count = data[epiweek][location]
+      params = (epiweek, location, region, count, count)
+      insert.execute(sql_dengue, params)
+
+  # Cleanup
+  insert.close()
+  rows2 = get_rows(cnx)
+  print('rows after (flu): %d (added %d)' % (rows2[0], rows2[0] - rows1[0]))
+  print('rows after (dengue): %d (added %d)' % (rows2[1], rows2[1] - rows1[1]))
+  if test_mode:
+    print('test mode: changes not commited')
+  else:
+    cnx.commit()
+  cnx.close()
+
+
+def main():
+  # args and usage
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    '--test',
+    '-t',
+    action='store_true',
+    default=False,
+    help='test mode, do not commit changes'
+  )
+  args = parser.parse_args()
+
+  # fetch and store NIDSS data
+  update(args.test)
+
+
+if __name__ == '__main__':
+  main()
