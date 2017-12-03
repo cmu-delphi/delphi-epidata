6f2ac36ffd9b2c0cab22cf84cc40706472c2a1f7
Lisheng-Gao <answergls@gmail.com>
2017-12-02 19:31:45 -0500

    add quidel processing scripts

---
+++ src/acquisition/quidel/quidel.py
@@ -0,0 +1,239 @@
+'''
+===============
+=== Purpose ===
+===============
+
+A Python utility class to download and parse Quidel date, which is called
+by quidel_update.py
+
+
+=================
+=== Changelog ===
+=================
+
+2017-12-02:
+	* original version
+'''
+
+# standard
+from os import listdir
+from os.path import isfile, join
+
+import email, getpass, imaplib, os
+import datetime
+import math
+from collections import defaultdict
+
+# third party
+import pandas as pd
+import numpy as np
+
+# delphi
+import delphi.utils.epidate as ED
+import delphi.utils.epiweek as EW
+from delphi.utils.state_info import *
+import delphi.operations.secrets as secrets
+
+def word_map(row,terms):
+    for (k,v) in terms.items():
+        row = row.replace(k,v)
+    return row
+
+def dateToEpiweek(date, delimiter='-', shift=0):
+    curr_date = datetime.datetime.now().strftime('%Y-%m-%d')
+    cy,cm,cd = [int(x) for x in curr_date.split('-')]
+    try:
+        y,m,d = [int(x) for x in date.split(delimiter)]
+    except:
+        return -1
+
+    if cy*10000+cm*100+cd<y*10000+m*100+d:
+		# mark records from future as invalid
+        return -1
+    epidate = ED.EpiDate(y,m,d)
+    epidate = epidate.add_days(shift)
+    ew = epidate.get_ew()
+    return ew
+
+# convert measurment to time series format
+# startweek and endweek are inclusive
+def measurement_to_ts(m,index,startweek=0,endweek=999999):
+    res = {}
+    for r,rdict in m.items():
+        res[r]={}
+        for t,vals in rdict.items():
+            if index>=len(vals):
+                raise Exception("Index is invalid")
+            if t>=startweek and t<=endweek:
+                res[r][t] = vals[index]
+    return res
+
+class QuidelData:
+    def __init__(self, raw_path):
+        self.data_path = raw_path
+        self.excel_path = join(raw_path,'excel')
+        self.csv_path = join(raw_path,'csv')
+        self.xlsx_list = [f[:-5] for f in listdir(self.excel_path) if isfile(join(self.excel_path, f)) and f[-5:]=='.xlsx']
+        self.csv_list = [f[:-4] for f in listdir(self.csv_path) if isfile(join(self.csv_path, f)) and f[-4:]=='.csv']
+        self.map_terms = {
+            ' FL  34637"':'FL',
+        }
+        # hardcoded parameters
+        self.date_dim = 1
+        self.state_dim = 4
+        self.fields = [
+            'sofia_ser','date','fac_id','city','state','zip','age',
+            'fluA','fluB','fluAll','county','fac_type'
+        ]
+        self.fields_to_keep = ['fac_id','fluA','fluB','fluAll']
+        self.dims_to_keep = [self.fields.index(x) for x in self.fields_to_keep]
+        self.retrieve_excels()
+        self.prepare_csv()
+
+    def retrieve_excels(self):
+        detach_dir = self.excel_path # directory where to save attachments (default: current)
+
+        # connecting to the gmail imap server
+        m = imaplib.IMAP4_SSL("imap.gmail.com")
+        m.login(secrets.quidel.email_addr,secrets.quidel.email_pwd)
+        m.select("INBOX") # here you a can choose a mail box like INBOX instead
+        # use m.list() to get all the mailboxes
+        resp, items = m.search(None, "ALL") # you could filter using the IMAP rules here (check http://www.example-code.com/csharp/imap-search-critera.asp)
+        items = items[0].split() # getting the mails id
+
+        # The emailids are ordered from past to now
+        for emailid in items:
+            resp, data = m.fetch(emailid, "(RFC822)") # fetching the mail, "`(RFC822)`" means "get the whole stuff", but you can ask for headers only, etc
+            email_body = data[0][1] # getting the mail content
+            mail = email.message_from_string(email_body) # parsing the mail content to get a mail object
+
+            #Check if any attachments at all
+            if mail.get_content_maintype() != 'multipart':
+                continue
+
+            # we use walk to create a generator so we can iterate on the parts and forget about the recursive headach
+            for part in mail.walk():
+                # multipart are just containers, so we skip them
+                if part.get_content_maintype() == 'multipart':
+                    continue
+
+                # is this part an attachment ?
+                if part.get('Content-Disposition') is None:
+                    continue
+
+                filename = part.get_filename()
+                # check duplicates
+                if filename[-5:]!='.xlsx' or filename[:-5] in self.xlsx_list:
+                    continue
+
+                self.xlsx_list.append(filename[:-5])
+                att_path = os.path.join(detach_dir, filename)
+
+                #Check if its already there
+                if not os.path.isfile(att_path) :
+                    # finally write the stuff
+                    fp = open(att_path, 'wb')
+                    fp.write(part.get_payload(decode=True))
+                    fp.close()
+
+    def prepare_csv(self):
+        for f in self.xlsx_list:
+            if f in self.csv_list:
+                continue
+            df_dict = pd.read_excel(join(self.excel_path, f+'.xlsx'), sheetname=None)
+            for (_,df) in df_dict.items():
+                # convert data type for saving
+                types = df.apply(lambda x: pd.lib.infer_dtype(x.values))
+                # re-format date if needed
+                df['TestDate'] = df['TestDate'].apply(lambda x: x if '/' not in str(x) else '-'.join([
+                    '0'+d if len(d)==1 else d for d in [str(x).split('/')[i] for i in [2,0,1]]
+                ]))
+                df.to_csv(join(self.csv_path, f+'.csv'), index=False, encoding='utf-8')
+        self.csv_list = [f[:-4] for f in listdir(self.csv_path) if isfile(join(self.csv_path, f)) and f[-4:]=='.csv']
+
+    def load_csv(self, dims=None):
+        if dims is None:
+            dims = self.dims_to_keep
+        parsed_dict = defaultdict(dict)
+        for f in self.csv_list:
+            rf = open(join(self.csv_path,f+'.csv'))
+            lines = rf.readlines()
+            for l in lines[1:]:
+                l = word_map(l,self.map_terms)
+                row = l.strip().split(',')
+                date = row[self.date_dim]
+                state = row[self.state_dim]
+                if state not in parsed_dict[date]:
+                    parsed_dict[date][state] = []
+                parsed_dict[date][state].append([row[x] for x in dims])
+        return parsed_dict
+
+    # hardcoded aggregation function
+    # output: [#unique_device,fluA,fluB,fluAll,total]
+    def prepare_measurements(self,data_dict,use_epiweek=False,use_hhs=False,start_weekday=6):
+        buffer_dict = {}
+        SI = StateInfo()
+        if use_hhs:
+            region_list = SI.hhs
+        else:
+            region_list = SI.sta
+
+        day_shift = start_weekday - 6
+        time_map = lambda x:dateToEpiweek(x,'-',shift=day_shift) if use_epiweek else x
+        region_map = lambda x:SI.state_regions[x]['hhs'] \
+                    if use_hhs and x in SI.state_regions else x # a bit hacky
+        # first pass: prepare device_id set
+        device_dict = {}
+        for (date,daily_dict) in data_dict.items():
+            time = time_map(date)
+			if time == -1:
+				continue
+            if time not in device_dict:
+                device_dict[time]={}
+                for r in region_list:
+                    device_dict[time][r] = Set()
+            for (state,rec_list) in daily_dict.items():
+                region = region_map(state)
+                # get rid of non-US regions
+                if region not in region_list:
+                    continue
+                for rec in rec_list:
+                    fac = rec[0]
+                    device_dict[time][region].add(fac)
+
+        # second pass: prepare all measurements
+        for (date,daily_dict) in data_dict.items():
+            time = time_map(date)
+			if time == -1:
+				continue
+            if time not in buffer_dict:
+                buffer_dict[time]={}
+                for r in region_list:
+                    buffer_dict[time][r] = [0.0]*8
+
+            for (state,rec_list) in daily_dict.items():
+                region = region_map(state)
+                # get rid of non-US regions
+                if region not in region_list:
+                    continue
+                for rec in rec_list:
+                    fac_num = float(len(device_dict[time][region]))
+                    buffer_dict[time][region]= np.add(
+                        buffer_dict[time][region],[
+                            rec[1]=='positive',
+                            rec[2]=='positive',
+                            rec[3]=='positive',
+                            1.0,
+                            float(rec[1]=='positive')/fac_num,
+                            float(rec[2]=='positive')/fac_num,
+                            float(rec[3]=='positive')/fac_num,
+                            1.0/fac_num,
+                    ]).tolist()
+        # switch two dims of dict
+        result_dict = {}
+        for r in region_list:
+            result_dict[r]={}
+            for (k,v) in buffer_dict.items():
+                result_dict[r][k]=v[r]
+
+        return result_dict
---
+++ src/acquisition/quidel/quidel_update.py
@@ -0,0 +1,143 @@
+'''
+===============
+=== Purpose ===
+===============
+
+Stores data provided by Quidel Corp., which contains flu lab test result.
+See: quidel.py
+
+
+=======================
+=== Data Dictionary ===
+=======================
+
+`quidel` is the table where quidel data is stored.
++----------+-------------+------+-----+---------+----------------+
+| Field    | Type        | Null | Key | Default | Extra          |
++----------+-------------+------+-----+---------+----------------+
+| id       | int(11)     | NO   | PRI | NULL    | auto_increment |
+| location | varchar(8)  | NO   | MUL | NULL    |                |
+| epiweek  | int(11)     | NO   | MUL | NULL    |                |
+| value    | float       | NO   |     | NULL    |                |
++----------+-------------+------+-----+---------+----------------+
+id: unique identifier for each record
+location: hhs1-10
+epiweek: the epiweek during which the queries were executed
+value: number of total test records per facility, within each epiweek
+
+=================
+=== Changelog ===
+=================
+
+2017-12-02
+  * original version
+'''
+
+# standard library
+import argparse
+
+# third party
+import mysql.connector
+
+# first party
+import quidel
+import delphi.operations.secrets as secrets
+from delphi.utils.epidate import EpiDate
+import delphi.utils.epiweek as flu
+
+LOCATIONS = ['hhs%d'%i for i in range(1,11)]
+DATAPATH = '/quidel_data'
+
+def update(locations, terms, first=None, last=None):
+  # connect to the database
+  u, p = secrets.db.epi
+  cnx = mysql.connector.connect(user=u, password=p, database='epidata')
+  cur = cnx.cursor()
+
+  def get_num_rows():
+    cur.execute('SELECT count(1) `num` FROM `quidel`')
+    for (num,) in cur:
+      pass
+    return num
+
+  # check from 4 weeks preceeding the last week with data through this week
+  cur.execute('SELECT max(`epiweek`) `ew0`, yearweek(now(), 6) `ew1` FROM `quidel`')
+  for (ew0, ew1) in cur:
+    ew0 = 200401 if ew0 is None else flu.add_epiweeks(ew0, -4)
+  ew0 = ew0 if first is None else first
+  ew1 = ew1 if last is None else last
+  print('Checking epiweeks between %d and %d...' % (ew0, ew1))
+
+  # keep track of how many rows were added
+  rows_before = get_num_rows()
+
+  # check Quidel for new and/or revised data
+  sql = '''
+    INSERT INTO
+      `quidel` (`location`, `epiweek`, `value`)
+    VALUES
+      (%s, %s, %s)
+    ON DUPLICATE KEY UPDATE
+      `value` = %s
+  '''
+
+  total_rows = 0
+  qd = quidel.QuidelData(DATAPATH)
+  qd_data = qd.load_csv()
+  qd_measurements = qd.prepare_measurements(qd_data,start_weekday=4)
+  qd_ts = quidel.measurement_to_ts(qd_measurements,7,startweek=first,endweek=last)
+
+  for location in locations:
+	if location not in qd_ts:
+		continue
+	ews = sorted(qd_ts[location].keys())
+	num_missing = 0
+    for ew in ews:
+	  v = qd_ts[location][ew]
+      sql_data = (location, ew, v, v)
+      cur.execute(sql, sql_data)
+      total_rows += 1
+      if v == 0:
+        num_missing += 1
+    if num_missing > 0:
+      print(' [%s] missing %d/%d value(s)' % (location, num_missing, len(ews)))
+
+  # keep track of how many rows were added
+  rows_after = get_num_rows()
+  print('Inserted %d/%d row(s)'%(rows_after - rows_before, total_rows))
+
+  # cleanup
+  cur.close()
+  cnx.commit()
+  cnx.close()
+
+
+def main():
+  # args and usage
+  parser = argparse.ArgumentParser()
+  parser.add_argument('location', action='store', type=str, default=None, help='location(s) (ex: all; any of hhs1-10)')
+  parser.add_argument('--first', '-f', default=None, type=int, help='first epiweek override')
+  parser.add_argument('--last', '-l', default=None, type=int, help='last epiweek override')
+  args = parser.parse_args()
+
+  # sanity check
+  first, last = args.first, args.last
+  if first is not None:
+    flu.check_epiweek(first)
+  if last is not None:
+    flu.check_epiweek(last)
+  if first is not None and last is not None and first > last:
+    raise Exception('epiweeks in the wrong order')
+
+  # decide what to update
+  if args.location.lower() == 'all':
+    locations = LOCATIONS
+  else:
+    locations = args.location.lower().split(',')
+
+  # run the update
+  update(locations, first, last)
+
+
+if __name__ == '__main__':
+  main()
