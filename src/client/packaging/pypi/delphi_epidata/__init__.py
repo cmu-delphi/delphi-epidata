0cfbe99c06302d96fb76d4ffacae77267ed8eb64
undefx <dfarrow0@gmail.com>
2018-10-05 09:51:04 -0500

    package python client for PyPI
    This is one step toward resolving #2. The package can be installed with
    pip like:
    `pip install delphi-epidata`
    
    Note: the file `delphi_epidata.py` is missing from the package dir. This
    is simply to avoid (1) duplicating code within the repo while (2) not
    messing up the code structure since other projects expect to find
    `delphi_epidata.py` in a particular location.
    
    To build a new release of the package, first copy `delphi_epidata.py`
    into the package, then follow the instructions at
    https://packaging.python.org/tutorials/packaging-projects/

---
+++ src/client/packaging/pypi/delphi_epidata/__init__.py
@@ -0,0 +1,4 @@
+from .delphi_epidata import Epidata
+
+name = 'delphi_epidata'
+__version__ = '0.0.4'
---
+++ src/client/packaging/pypi/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+  long_description = f.read()
+
+setuptools.setup(
+  name='delphi_epidata',
+  version='0.0.4',
+  author='David Farrow',
+  author_email='dfarrow0@gmail.com',
+  description='A programmatic interface to Delphi\'s Epidata API.',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url='https://github.com/cmu-delphi/delphi-epidata',
+  packages=setuptools.find_packages(),
+  install_requires=[
+    'requests>=2.7.0',
+  ],
+  classifiers=[
+    'Programming Language :: Python',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Science/Research',
+    'Natural Language :: English',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+  ],
+)
