1c21d853d490dfd459a6803351358bf2dbac7cfc
rumackaaron <adr66@cornell.edu>
2018-11-21 16:11:36 -0500

    PAHO scraping and DB update

---
+++ src/acquisition/paho/paho_db_update.py
@@ -0,0 +1,280 @@
+"""
+===============
+=== Purpose ===
+===============
+
+Stores versioned PAHO Dengue data from PAHO website.
+
+=======================
+=== Data Dictionary ===
+=======================
+
+`paho_dengue` is where the Dengue data from PAHO is stored.
+
++----------------+-------------+------+-----+---------+----------------+
+| Field          | Type        | Null | Key | Default | Extra          |
++----------------+-------------+------+-----+---------+----------------+
+| id             | int(11)     | NO   | PRI | NULL    | auto_increment |
+| release_date   | date        | NO   | MUL | NULL    |                | 
+| issue          | int(11)     | NO   | MUL | NULL    |                |
+| epiweek        | int(11)     | NO   | MUL | NULL    |                |
+| region         | varchar(12) | NO   | MUL | NULL    |                |
+| lag            | int(11)     | NO   | MUL | NULL    |                |
+| total_pop      | int(11)     | NO   |     | NULL    |                |
+| serotype       | numchar(12) | NO   |     | NULL    |                |
+| num_dengue     | int(11)     | YES  |     | NULL    |                |
+| incidence_rate | double      | YES  |     | NULL    |                |
+| num_severe     | int(11)     | YES  |     | NULL    |                |
+| num_deaths     | int(11)     | YES  |     | NULL    |                |
++----------------+-------------+------+-----+---------+----------------+
+id: unique identifier for each record
+issue: the epiweek of publication (e.g. issue 201453 includes epiweeks up to
+  and including 2014w53, but not 2015w01 or following)
+epiweek: the epiweek during which the data was collected
+region: the name of the location (3 letter country code)
+lag: number of weeks between `epiweek` and `issue`
+total_pop: population of country in thousands
+serotype: identified dengue serotype
+num_dengue: cumulative cases of dengue in calendar year
+incidence_rate: (num_dengue/total_pop) * 100
+num_severe: cumulative cases of severe dengue in calendar year
+num_deaths: cumulative deaths in calendar year
+"""
+
+# TODO: small countries have names not recognized by pycountry
+#       could hardcode them
+# TODO: Obviously regions not recognized by pycountry
+        Should be aggregated and therefore redundant, maybe hardcode them
+
+import argparse
+import csv
+import datetime
+import glob
+import sys
+import subprocess
+import random
+from io import StringIO
+
+# third party
+import mysql.connector
+import pycountry
+
+# first party
+import delphi.operations.secrets as secrets
+from delphi.utils.epiweek import delta_epiweeks, join_epiweek, check_epiweek
+from delphi.utils.epidate import EpiDate
+
+def ensure_tables_exist():
+    (u,p) = secrets.db.epi
+    cnx = mysql.connector.connect(user=u,password=p,database='epidata')
+    try:
+        cursor = cnx.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS `paho_dengue` (
+                `id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                `release_date` DATE NOT NULL,
+                `issue` INT(11) NOT NULL,
+                `epiweek` INT(11) NOT NULL,
+                `lag` INT(11) NOT NULL,
+                `region` VARCHAR(12) NOT NULL,
+                `total_pop` INT(11) NOT NULL,
+                `serotype` VARCHAR(12) NOT NULL,
+                `num_dengue` INT(11) NOT NULL,
+                `incidence_rate` DOUBLE NOT NULL,
+                `num_severe` INT(11) NOT NULL,
+                `num_deaths` INT(11) NOT NULL,
+                UNIQUE KEY (`issue`, `epiweek`, `region`)
+            );
+        ''');
+        cnx.commit()
+    finally:
+        cnx.close()
+
+def safe_float(f):
+    try:
+        return float(f.replace(',',''))
+    except Exception:
+        return 0
+
+def safe_int(i):
+    try:
+        return int(i.replace(',',''))
+    except Exception:
+        return 0
+
+def get_rows(cnx, table='paho_dengue'):
+  # Count and return the number of rows in the `fluview` table.
+  select = cnx.cursor()
+  select.execute('SELECT count(1) num FROM %s' % table)
+  for (num,) in select:
+    pass
+  select.close()
+  return num
+
+def get_paho_data(row):
+    if row[0] == "\ufeffIncidence Rate (c)" and row != "Incidence Rate (c),(SD/D) x100 (e),CFR (f),ID,Country or Subregion,Deaths,EW,Confirmed,Epidemiological Week (a),Pop (no usar),Serotype,Severe Dengue (d),Total of Dengue Cases (b),Year,Population x 1000".split(","):
+        raise Exception('PAHO header row has changed')
+    if len(row) == 1 or row[0] == "Incidence Rate (c)":
+        # this is a header row
+        return None
+    try:
+        country = pycountry.countries.get(name=row[4]).alpha_2
+    except Exception:
+        try:
+            country = pycountry.countries.get(common_name=row[4]).alpha_2
+        except Exception:
+            try:
+                country = pycountry.countries.get(official_name=row[4]).alpha_2
+            except Exception:
+                return None
+    try:
+        check_epiweek(safe_int(row[13])*100 + safe_int(row[8]), safe_int(row[13])*100 + safe_int(row[6]))
+    except Exception:
+        return None
+    return {
+        'issue': safe_int(row[13])*100 + safe_int(row[6]),
+        'epiweek': safe_int(row[13])*100 + safe_int(row[8]),
+        'region': country,
+        'total_pop': safe_int(row[14]),
+        'serotype': row[10],
+        'num_dengue': safe_int(row[12]),
+        'incidence_rate': safe_float(row[0]),
+        'num_severe': safe_int(row[11]),
+        'num_deaths': safe_int(row[5]),
+        'severe_ratio': safe_float(row[1]),
+        'cfr': safe_float(row[2])
+    }
+
+def update_from_file(issue, date, filename, test_mode=False):
+    # Read PAHO data from CSV and insert into (or update) the database.
+
+    # Behavior with issue:
+    # PAHO has drop down menu for week, and selecting a given week
+    #   from that menu gives the data for that issue, not that EW
+    # Unsure what revisions, if any, that data goes through
+    # Current code ignores PAHO-given issue, is based on argument 'issue'
+
+    # database connection
+    u, p = secrets.db.epi
+    cnx = mysql.connector.connect(user=u, password=p, database='epidata')
+    rows1 = get_rows(cnx, 'paho_dengue')
+    print('rows before: %d' % (rows1))
+    insert = cnx.cursor()
+
+    # load the data, ignoring empty rows
+    print('loading data from %s as issued on %d' % (filename, issue))
+    with open(filename,'r',encoding='utf-8') as f:
+        c = f.read()
+    rows = []
+    for l in csv.reader(StringIO(c), delimiter=','):
+        rows.append(get_paho_data(l))
+    print(' loaded %d rows' % len(rows))
+    entries = [obj for obj in rows if obj]
+    print(' found %d entries' % len(entries))
+
+    sql = '''
+    INSERT INTO
+        `paho_dengue` (`release_date`, `issue`, `epiweek`, `region`, `lag`,
+        `total_pop`, `serotype`, `num_dengue`, `incidence_rate`,
+        `num_severe`, `num_deaths`)
+    VALUES
+        ('%s', %s, %s, '%s', %s, %s, '%s', %s, %s, %s, %s)
+    ON DUPLICATE KEY UPDATE
+        `release_date` = least(`release_date`, '%s'),
+        `total_pop` = %s,
+        `serotype` = '%s',
+        `num_dengue` = %s,
+        `incidence_rate` = %s,
+        `num_severe` = %s,
+        `num_deaths` = %s
+    '''
+
+    for row in entries:
+        if row['issue'] > issue: # Issued in a week that hasn't happened yet
+            continue
+        lag = delta_epiweeks(row['epiweek'], issue)
+        data_args = [row['total_pop'], row['serotype'], row['num_dengue'],
+                     row['incidence_rate'], row['num_severe'], row['num_deaths']]
+        
+        insert_args = [date,issue,row['epiweek'],row['region'],lag] + data_args
+        update_args = [date] + data_args
+        insert.execute(sql % tuple(insert_args + update_args))
+
+    # cleanup
+    insert.close()
+    if test_mode:
+        print('test mode, not committing')
+        rows2 = rows1
+    else:
+        cnx.commit()
+        rows2 = get_rows(cnx)
+    print('rows after: %d (added %d)' % (rows2,rows2-rows1))
+    cnx.close()
+
+def main():
+    # args and usage
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--test',
+        action='store_true',
+        help='do dry run only, do not update the database'
+    )
+    parser.add_argument(
+        '--file',
+        type=str,
+        help='load an existing zip file (otherwise fetch current data)'
+    )
+    parser.add_argument(
+        '--issue',
+        type=int,
+        help='issue of the file (e.g. 201740); used iff --file is given'
+    )
+    args = parser.parse_args()
+
+    if (args.file is None) != (args.issue is None):
+        raise Exception('--file and --issue must both be present or absent')
+
+    date = datetime.datetime.now().strftime('%Y-%m-%d')
+    print('assuming release date is today, %s' % date)
+
+    if args.file:
+        update_from_file(args.issue, date, args.file, test_mode=args.test)
+    else:
+        # Code doesn't always download all files, unreproducible errors
+        # Try a few times and hopefully one will work
+        ensure_tables_exist()
+        flag = 0
+        max_tries = 5
+        while flag < max_tries:
+            flag = flag + 1
+            tmp_dir = ''.join(random.choice('0123456789abcdefghijklmnopqrstuvwxyz') for i in range(8))
+            tmp_dir = 'downloads_' + tmp_dir
+            subprocess.call(["mkdir",tmp_dir])
+            # Use temporary directory to avoid data from different time
+            #   downloaded to same folder
+            get_paho_data(dir=tmp_dir)
+            issue = EpiDate.today().get_ew()
+            # Check to make sure we downloaded a file for every week
+            issueset = set()
+            files = glob.glob('%s/*.csv' % tmp_dir)
+            for filename in files:
+                with open(filename,'r') as f:
+                    _ = f.readline()
+                    data = f.readline().split(',')
+                    issueset.add(data[6])
+            db_error = False
+            if len(issueset) >= 53: # Shouldn't be more than 53
+                for filename in files:
+                    try:
+                        update_from_file(issue, date, filename, test_mode=args.test)
+                        subprocess.call(["rm",filename])
+                    except Exception:
+                        db_error = True
+                subprocess.call(["rm","-r",tmp_dir])
+                if not db_error:
+                    break # Exit loop with success
+            if flag >= max_tries:
+                print('WARNING: Database `paho_dengue` did not update successfully')
+
+if __name__ == '__main__':
+    main()
---
+++ src/acquisition/paho/paho_download.py
@@ -0,0 +1,164 @@
+
+# IMPORTANT: This code is extremely unstable.
+# Slight changes to the PAHO website may render this script partially or entirely useless.
+
+import os
+
+# Start up a browser
+from selenium.webdriver import Firefox
+from selenium.webdriver import FirefoxProfile
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.firefox.options import Options
+
+headerheight = 0
+
+def wait_for(browser, css_selector, delay=10):
+    try:
+        myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, css_selector)))
+        myElem = WebDriverWait(browser, delay).until(EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector)))
+        print('Success Loading %s' % (css_selector))
+    except TimeoutException:
+        print("Loading %s took too much time!" % (css_selector))
+        
+def find_and_click(browser, element):
+    element.location_once_scrolled_into_view
+    browser.switch_to.default_content()
+    browser.execute_script("window.scrollBy(0,-%d)"%headerheight)
+    browser.switch_to.frame(browser.find_element_by_tag_name("iframe"))
+    browser.switch_to.frame(browser.find_element_by_tag_name("iframe"))
+    element.click()
+
+def get_paho_data(offset=0, dir='downloads'):
+    opts = Options()
+    opts.set_headless()
+    assert opts.headless  # Operating in headless mode
+
+    fp = FirefoxProfile()
+    fp.set_preference("browser.download.folderList",2)
+    fp.set_preference("browser.download.manager.showWhenStarting",False)
+    fp.set_preference("browser.download.dir",os.path.abspath(dir))
+    fp.set_preference("browser.helperApps.neverAsk.saveToDisk","text/csv")
+
+    browser = Firefox(options=opts,firefox_profile=fp)
+    browser.get('http://www.paho.org/data/index.php/en/mnu-topics/indicadores-dengue-en/dengue-nacional-en/252-dengue-pais-ano-en.html?showall=&start=1')
+    tab1 = browser.window_handles[0]
+    browser.execute_script('''window.open("","_blank");''')
+    tab2 = browser.window_handles[1]
+    browser.switch_to.window(tab1)
+    
+    curr_offset = offset
+    
+    wait_for(browser, "div.rt-top-inner", delay=30)
+    header = browser.find_element_by_css_selector("div.rt-top-inner")
+    global headerheight
+    headerheight = header.rect["height"]
+
+    # The actual content of the data of this webpage is within 2 iframes, so we need to navigate into them first
+    browser.switch_to.frame(browser.find_element_by_tag_name("iframe"))
+    browser.switch_to.frame(browser.find_element_by_tag_name("iframe"))
+    
+    # Locate the button that allows to download the table
+    downloadoption = browser.find_elements_by_css_selector("div.tabToolbarButton.tab-widget.download")[0]
+    find_and_click(browser, downloadoption)
+
+    wait_for(browser, "div[data-tb-test-id='DownloadImage-Button']")
+
+    # Locate the button that prepares the table for download as an image
+    imagebutton = browser.find_elements_by_css_selector("div[data-tb-test-id='DownloadImage-Button']")[0]
+    find_and_click(browser, imagebutton)
+
+    wait_for(browser, ".tabDownloadFileButton[data-test-id='DownloadLink']")
+
+    # Locate the button that downloads the table as an image
+    downloadbutton = browser.find_elements_by_css_selector(".tabDownloadFileButton[data-test-id='DownloadLink']")[0]
+
+    # Extract session ID
+    href = downloadbutton.get_attribute("href")
+    startidx = href.index("sessions/") + len("sessions/")
+    endidx = href.index("/",startidx)
+    sessionid = href[startidx:endidx]
+
+    dataurl = "http://phip.paho.org/vizql/w/Casosdedengue_tben/v/ByLastAvailableEpiWeek/viewData/sessions/%s/views/18076444178507886853_9530488980060483892?maxrows=200&viz=%%7B%%22worksheet%%22:%%22W%%20By%%20Last%%20Available%%20EpiWeek%%22,%%22dashboard%%22:%%22By%%20Last%%20Available%%20Epi%%20Week%%22%%7D"%sessionid
+
+    wait_for(browser, "div[data-tb-test-id='CancelBtn-Button']")
+
+    # Cancel image download
+    cancelbutton = browser.find_elements_by_css_selector("div[data-tb-test-id='CancelBtn-Button']")[0]
+    find_and_click(browser, cancelbutton)
+
+    wait_for(browser, "div[id='tableau_base_widget_FilterPanel_0']")
+
+    # Default is to show data for current year, we want to get all years
+    # Clicks drop-down menu to open options
+    yearselector = browser.find_elements_by_css_selector("div[id='tableau_base_widget_FilterPanel_0']")[0]
+    find_and_click(browser, yearselector)
+
+    wait_for(browser, "div.facetOverflow")
+
+    # Find option for all years and click it
+    y = None
+    for i in browser.find_elements_by_css_selector("div.facetOverflow"):
+        if i.text == "(All)":
+            y = i
+    find_and_click(browser, y)
+    
+    for i in range(offset):
+        gp = browser.find_element_by_css_selector("div.wcGlassPane")
+        #print gp.is_enabled()
+        #print gp.is_selected()
+        #print gp.is_displayed()
+        try:
+            WebDriverWait(browser, 10).until(EC.staleness_of(gp))
+            print("Loaded next week % d" % (53-offset))
+        except TimeoutException:
+            print("Loading next week %d took too much time!" % (53-offset))
+        gp = browser.find_element_by_css_selector("div.wcGlassPane")
+        #print gp.is_enabled()
+        #print gp.is_selected()
+        #print gp.is_displayed()
+        x = browser.find_elements_by_css_selector("div.dijitReset.dijitSliderButtonContainer.dijitSliderButtonContainerH.tableauArrowDec")[0]
+        find_and_click(browser, x)
+
+    # Cycle through all weeks, downloading each week as a separate .csv
+    # Theoretically, need to cycle 53 times, but in practice only 54 works, unsure why
+    for i in range(54-offset):
+        # If something goes wrong for whatever reason, try from the beginning
+        try:
+            print('Loading week %d' % (53-i))
+            # (Re-)load URL
+            browser.switch_to.window(tab2)
+            browser.get(dataurl)
+
+            wait_for(browser, "li[id='tab-view-full-data']")
+            # Load data in better format ("Full Data")
+            full_data_tab = browser.find_elements_by_css_selector("li[id='tab-view-full-data']")[0]
+            full_data_tab.click()
+
+            wait_for(browser, "a.csvLink") # Sometimes this fails but the button is successfully clicked anyway, not sure why
+            # Actually download the data as a .csv (Will be downloaded to Firefox's default download destination)
+            data_links = browser.find_elements_by_css_selector("a.csvLink")
+            data_link = None
+            for i in data_links:
+                if i.get_property("href") != "":
+                    data_link = i
+                    break
+            data_link.click()
+
+            # Locate button that decreases the current week by 1
+            browser.switch_to.window(tab1)
+            wait_for(browser, "div.dijitReset.dijitSliderButtonContainer.dijitSliderButtonContainerH.tableauArrowDec")
+
+            x = browser.find_elements_by_css_selector("div.dijitReset.dijitSliderButtonContainer.dijitSliderButtonContainerH.tableauArrowDec")[0]
+            find_and_click(browser, x)
+            curr_offset += 1
+        except Exception as e:
+            print('Got exception %s\nTrying again from week %d' % (e,53-offset))
+            browser.quit()
+            get_paho_data(offset=curr_offset)
+    browser.quit()
+
+if __name__ == '__main__':
+    get_paho_data(dir='downloads/')
