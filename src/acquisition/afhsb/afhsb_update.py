a03d944428c19356583fd59262ed099f5ce65a50
Irene Li <mengzeli@Irenes-MacBook-Pro.local>
2019-09-10 12:13:51 -0400

    fixed bugs

--- src/acquisition/afhsb/afhsb_update.py
+++ src/acquisition/afhsb/afhsb_update.py
@@ -2,8 +2,8 @@
 from . import afhsb_sql, afhsb_csv, afhsb_utils
 
 def main():
-    afhsb_csv.aggregate_and_process()
-    afhsb_sql.init_all_tables(TARGET_DIR)
+    # afhsb_csv.aggregate_and_process()
+    afhsb_sql.init_all_tables_with_issue(datapath=TARGET_DIR, criterion="flucat")
 
 if __name__ == '__main__':
     main()
