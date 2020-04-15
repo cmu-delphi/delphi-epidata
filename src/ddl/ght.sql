731dc965943375e8027d10b26eee371398c4b51f
undefx <dfarrow@google.com>
2020-04-15 17:03:58 -0500

    add most outstanding table definitions
    - just for posterity/completeness
    - tested in the sense that the database image builds and runs 
    successfully with these tables
    - no tests specific to api endpoints that touch these tables
    - no documentation, just touched-up `desc` and `show create table` for 
    each source

---
+++ src/ddl/ght.sql
@@ -0,0 +1,28 @@
+/*
+TODO: document
+*/
+
+/*
++----------+-------------+------+-----+---------+----------------+
+| Field    | Type        | Null | Key | Default | Extra          |
++----------+-------------+------+-----+---------+----------------+
+| id       | int(11)     | NO   | PRI | NULL    | auto_increment |
+| query    | varchar(64) | NO   | MUL | NULL    |                |
+| location | varchar(8)  | NO   | MUL | NULL    |                |
+| epiweek  | int(11)     | NO   | MUL | NULL    |                |
+| value    | float       | NO   |     | NULL    |                |
++----------+-------------+------+-----+---------+----------------+
+*/
+
+CREATE TABLE `ght` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `query` varchar(64) NOT NULL,
+  `location` varchar(8) NOT NULL,
+  `epiweek` int(11) NOT NULL,
+  `value` float NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `query` (`query`,`location`,`epiweek`),
+  KEY `query_2` (`query`),
+  KEY `location` (`location`),
+  KEY `epiweek` (`epiweek`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
---
+++ src/ddl/kcdc_ili.sql
@@ -0,0 +1,29 @@
+/*
+TODO: document
+*/
+
+/*
++--------------+-------------+------+-----+---------+----------------+
+| Field        | Type        | Null | Key | Default | Extra          |
++--------------+-------------+------+-----+---------+----------------+
+| id           | int(11)     | NO   | PRI | NULL    | auto_increment |
+| release_date | date        | NO   |     | NULL    |                |
+| issue        | int(11)     | NO   | MUL | NULL    |                |
+| epiweek      | int(11)     | NO   |     | NULL    |                |
+| lag          | int(11)     | NO   |     | NULL    |                |
+| region       | varchar(12) | NO   |     | NULL    |                |
+| ili          | double      | NO   |     | NULL    |                |
++--------------+-------------+------+-----+---------+----------------+
+*/
+
+CREATE TABLE `kcdc_ili` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `release_date` date NOT NULL,
+  `issue` int(11) NOT NULL,
+  `epiweek` int(11) NOT NULL,
+  `lag` int(11) NOT NULL,
+  `region` varchar(12) NOT NULL,
+  `ili` double NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `issue` (`issue`,`epiweek`,`region`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
