aa08131ee0d2e196c3f6def201efd5671d2100df
undefx <dfarrow@google.com>
2020-04-20 15:26:48 -0500

    [covidcast acquisition] database
    - abstraction for interactions with the database table `covidcast`
    - part of the larger implementation, not yet used
    - ✔ All 5 tests passed! 100% (21/21) coverage.

---
+++ src/acquisition/covidcast/database.py
@@ -0,0 +1,85 @@
+"""A utility class that handles database operations related to covidcast."""
+
+# third party
+import mysql.connector
+
+# first party
+import delphi.operations.secrets as secrets
+
+
+class Database:
+  """A collection of covidcast database operations."""
+
+  DATABASE_NAME = 'epidata'
+
+  def connect(self, connector_impl=mysql.connector):
+    """Establish a connection to the database."""
+
+    u, p = secrets.db.epi
+    self._connection = connector_impl.connect(
+        host=secrets.db.host,
+        user=u,
+        password=p,
+        database=Database.DATABASE_NAME)
+    self._cursor = self._connection.cursor()
+
+  def disconnect(self, commit):
+    """Close the database connection.
+
+    commit: if true, commit changes, otherwise rollback
+    """
+
+    self._cursor.close()
+    if commit:
+      self._connection.commit()
+    self._connection.close()
+
+  def count_all_rows(self):
+    """Return the total number of rows in table `covidcast`."""
+
+    self._cursor.execute('SELECT count(1) FROM `covidcast`')
+
+    for (num,) in self._cursor:
+      return num
+
+  def insert_or_update(
+      self,
+      source,
+      signal,
+      time_type,
+      geo_type,
+      time_value,
+      geo_value,
+      value,
+      stderr,
+      sample_size,
+      direction):
+    """Insert a new row, or update an existing row, in the `covidcast` table.
+
+    See src/ddl/covidcast.sql for an explanation of each field.
+    """
+
+    sql = '''
+      INSERT INTO `covidcast` VALUES
+        (0, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+      ON DUPLICATE KEY UPDATE
+        `value` = VALUES(`value`),
+        `stderr` = VALUES(`stderr`),
+        `sample_size` = VALUES(`sample_size`),
+        `direction` = VALUES(`direction`)
+    '''
+
+    args = (
+      source,
+      signal,
+      time_type,
+      geo_type,
+      time_value,
+      geo_value,
+      value,
+      stderr,
+      sample_size,
+      direction,
+    )
+
+    self._cursor.execute(sql, args)
