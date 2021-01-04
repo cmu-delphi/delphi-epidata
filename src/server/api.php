ceacc847cc3414a6f9ee78da319f16844201d96d
mgithub40 <75716340+mgithub40@users.noreply.github.com>
2021-01-03 23:15:32 -0500

    Update api.php
    Adding COVID mobility source

--- src/server/api.php
+++ src/server/api.php
@@ -373,6 +373,50 @@ function get_gft($epiweeks, $locations) {
   return count($epidata) === 0 ? null : $epidata;
 }
 
+// queries the `Apple_Mobility_US` table
+//   $country (required): Mobility data country
+//   $state (required): Mobility data state
+//   $month (required): Mobility data month
+//   $year (required): Mobility data year
+function get_apple_mobility_data($country,$state,$month,$year) {
+  // basic query info
+  $applemobilitydata = array();
+  if($country === 'us'){	
+  $table = '`Apple_Mobility_US` am';
+  $fields = "am.`state`, am.`county_and_city`, am.`geo_type`, am.`date`, am.`driving`, am.`transit`, am.`walking`";	
+  // build the filter
+  $condition_state = filter_strings('am.`state`', $state);
+  $condition_month = filter_strings('month(`date`)', $month);
+  $condition_year = filter_strings('year(`date`)', $year);
+  $query = "SELECT {$fields} FROM {$table} WHERE ({$condition_state}) AND ({$condition_month}) AND ({$condition_year})";
+  // get the data from the database
+  execute_query($query, $applemobilitydata,array('state','county_and_city','geo_type','date'), null, array('driving','transit','walking'));
+  }
+  return count($applemobilitydata) === 0 ? null : $applemobilitydata;
+}
+
+// queries the `Google_Mobility_US` table
+//   $country (required): Mobility data country
+//   $state (required): Mobility data state
+//   $month (required): Mobility data month
+//   $year (required): Mobility data year
+function get_google_mobility_data($country,$state,$month,$year) {
+  // basic query info
+  $googlemobilitydata = array();
+  if($country === 'us'){
+  $table = '`Google_Mobility_US` gm';
+  $fields = "gm.`state`, gm.`county`, gm.`date`, gm.`retail and recreation`, gm.`grocery and pharmacy`, gm.`parks`, gm.`transit stations`, gm.`workplaces`, gm.`residential`";
+  // build the filter
+  $condition_state = filter_strings('gm.`state`', $state);
+  $condition_month = filter_strings('month(`date`)', $month);
+  $condition_year = filter_strings('year(`date`)', $year);
+  $query = "SELECT {$fields} FROM {$table} WHERE ({$condition_state}) AND ({$condition_month}) AND ({$condition_year})";
+  // get the data from the database
+  execute_query($query, $googlemobilitydata,array('state','county','date'), null, array('retail and recreation','grocery and pharmacy','parks','transit stations','workplaces','residential'));
+  }
+  return count($googlemobilitydata) === 0 ? null : $googlemobilitydata;
+}
+
 // queries the `ght` table
 //   $epiweeks (required): array of epiweek values/ranges
 //   $locations (required): array of location names
@@ -2028,7 +2072,24 @@ if(database_connect()) {
       $epidata = get_covid_hosp_facility_lookup($state, $ccn, $city, $zip, $fips_code);
       store_result($data, $epidata);
     }
-  } else {
+  } else if($source === 'mobility') {
+      if(require_all($data, array('type','country','state','month','year'))) {
+      // parse the request
+      $type = isset($_REQUEST['type']) ? strtolower($_REQUEST['type']) : null;
+      $country = isset($_REQUEST['country']) ? strtolower($_REQUEST['country']) : null;
+      $state = extract_values($_REQUEST['state'], 'str');
+      $month = extract_values($_REQUEST['month'], 'str');
+      $year = extract_values($_REQUEST['year'], 'str');	
+      $mobilitydata = null;  	
+      if($type === 'apple') {		
+      $mobilitydata = get_apple_mobility_data($country,$state,$month,$year);
+     }
+      else if($type === 'google'){
+      $mobilitydata = get_google_mobility_data($country,$state,$month,$year);	
+     }	
+      store_result($data, $mobilitydata);
+    }
+} else {
     $data['message'] = 'no data source specified';
   }
   // API analytics
