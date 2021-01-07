4698b8aea6584f8e80282c4de2ef58038b29e854
andrew <chinandrew96@gmail.com>
2021-01-07 08:51:15 -0800

    add as_of test

--- integrations/server/test_covidcast_nowcast.py
+++ integrations/server/test_covidcast_nowcast.py
@@ -20,10 +20,10 @@ class CovidcastTests(unittest.TestCase):
 
     # connect to the `epidata` database and clear the `covidcast` table
     cnx = mysql.connector.connect(
-        user='user',
-        password='pass',
-        host='delphi_database_epidata',
-        database='epidata')
+      user='user',
+      password='pass',
+      host='delphi_database_epidata',
+      database='epidata')
     cur = cnx.cursor()
     cur.execute('truncate table covidcast_nowcast')
     cnx.commit()
@@ -42,13 +42,11 @@ class CovidcastTests(unittest.TestCase):
     """Query without specifying an issue."""
 
     self.cur.execute(
-      f'''insert into covidcast_nowcast values (
-      0, 'src', 'sig', 'day', 'county', 20200101, '01001', 12345678, 2.5, 20200101, 2
-      )''')
-    self.cur.execute(
-      f'''insert into covidcast_nowcast values (
-      0, 'src', 'sig', 'day', 'county', 20200101, '01001', 12345678, 1.5, 20200102, 2
-      )''')
+      f'''insert into covidcast_nowcast values 
+      (0, 'src', 'sig', 'day', 'county', 20200101, '01001', 12345678, 3.5, 20200101, 2),
+      (0, 'src', 'sig', 'day', 'county', 20200101, '01001', 12345678, 2.5, 20200102, 2),
+      (0, 'src', 'sig', 'day', 'county', 20200101, '01001', 12345678, 1.5, 20200103, 2)''')
+
     self.cnx.commit()
     # make the request with specified issue date
     response = requests.get(BASE_URL, params={
@@ -69,7 +67,7 @@ class CovidcastTests(unittest.TestCase):
         'signal': 'sig',
         'time_value': 20200101,
         'geo_value': '01001',
-        'value': 2.5,
+        'value': 3.5,
         'issue': 20200101,
         'lag': 2,
        }],
@@ -96,7 +94,33 @@ class CovidcastTests(unittest.TestCase):
         'time_value': 20200101,
         'geo_value': '01001',
         'value': 1.5,
-        'issue': 20200102,
+        'issue': 20200103,
+        'lag': 2,
+       }],
+      'message': 'success',
+    })
+
+    response = requests.get(BASE_URL, params={
+      'source': 'covidcast_nowcast',
+      'data_source': 'src',
+      'signal': 'sig',
+      'time_type': 'day',
+      'geo_type': 'county',
+      'time_values': 20200101,
+      'geo_value': '01001',
+      'as_of': 20200101
+    })
+    response.raise_for_status()
+    response = response.json()
+
+    self.assertEqual(response, {
+      'result': 1,
+      'epidata': [{
+        'signal': 'sig',
+        'time_value': 20200101,
+        'geo_value': '01001',
+        'value': 3.5,
+        'issue': 20200101,
         'lag': 2,
        }],
       'message': 'success',
