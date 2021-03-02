a4f35da3f98717ce402039449144087484d16c76
Ben Smith <benjamin.y.smith@gmail.com>
2021-03-02 16:40:34 -0500

    reflow

--- src/acquisition/covidcast/signal_dash_data_generator.py
+++ src/acquisition/covidcast/signal_dash_data_generator.py
@@ -11,49 +11,62 @@ from enum import Enum
 # first party
 from logger import get_structured_logger
 
-class DashboardSignal(Enum):
-  # TODO: store this in config file or DB table instead of enum
-  CHANGE = ("change", "chng")
-  QUIDEL = ("quidel", "quidel")
 
-  def __init__(self, signal_name, source):
-    self.signal_name = signal_name
-    self.source = source
+class DashboardSignal(Enum):
+    # TODO: store this in config file or DB table instead of enum
+    CHANGE = ("change", "chng")
+    QUIDEL = ("quidel", "quidel")
+
+    def __init__(self, signal_name, source):
+        self.signal_name = signal_name
+        self.source = source
+
 
 def get_argument_parser():
-  """Define command line arguments."""
+    """Define command line arguments."""
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--log_file", help="filename for log output")
-  return parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--log_file", help="filename for log output")
+    return parser
+
 
 def get_latest_issue_date_from_metadata(dashboard_signal, metadata):
     df_for_source = metadata[metadata.data_source == dashboard_signal.source]
     return df_for_source["max_issue"].max()
 
+
 def get_latest_data_date_from_metadata(dashboard_signal, metadata):
     df_for_source = metadata[metadata.data_source == dashboard_signal.source]
     return df_for_source["max_time"].max()
-        
+
+
 def get_most_recent_issue_row_count(dashboard_signal, metadata):
     # TODO: Read database directly to calculate
     return None
 
+
 def get_coverage(dashboard_signal, metadata):
-    latest_data_date = get_latest_data_date_from_metadata(dashboard_signal, metadata)
+    latest_data_date = get_latest_data_date_from_metadata(
+        dashboard_signal, metadata)
     df_for_source = metadata[metadata.data_source == dashboard_signal.source]
-    # we need to do something smarter here -- both make this part of config (and allow multiple signals) and/or 
+    # we need to do something smarter here -- both make this part of config (and allow multiple signals) and/or
     # aggregate across all sources for a signal
     signal = df_for_source["signal"].iloc[0]
-    latest_data = covidcast.signal(dashboard_signal.source, signal, end_day=latest_data_date, start_day=latest_data_date)
+    latest_data = covidcast.signal(
+        dashboard_signal.source,
+        signal,
+        end_day=latest_data_date,
+        start_day=latest_data_date)
     coverage = latest_data[["time_value", "geo_type", "geo_value"]].copy()
     coverage.insert(0, 'signal_name', dashboard_signal.signal_name)
     coverage_list = list(coverage.itertuples(index=False, name=None))
     return coverage_list
 
+
 def get_dashboard_signals_to_generate():
     # TODO: read this from a config file or DB table
     return [DashboardSignal.CHANGE, DashboardSignal.QUIDEL]
+
 
 def main(args):
     """Generate data for the signal dashboard.
@@ -66,25 +79,34 @@ def main(args):
 
     logger = get_structured_logger(
         "metadata_cache_updater",
-        filename=log_file,log_exceptions=False)
+        filename=log_file, log_exceptions=False)
     start_time = time.time()
 
     signals_to_generate = get_dashboard_signals_to_generate()
     print(signals_to_generate)
-    
+
     metadata = covidcast.metadata()
 
     signal_status_list = []
     coverage_list = []
     for dashboard_signal in signals_to_generate:
-        latest_issue_date = get_latest_issue_date_from_metadata(dashboard_signal, metadata)
-        latest_data_date = get_latest_data_date_from_metadata(dashboard_signal, metadata)
-        latest_row_count = get_most_recent_issue_row_count(dashboard_signal, metadata)
+        latest_issue_date = get_latest_issue_date_from_metadata(
+            dashboard_signal,
+            metadata)
+        latest_data_date = get_latest_data_date_from_metadata(
+            dashboard_signal, metadata)
+        latest_row_count = get_most_recent_issue_row_count(
+            dashboard_signal, metadata)
         latest_coverage = get_coverage(dashboard_signal, metadata)
 
-        signal_status_list.append((dashboard_signal.signal_name, datetime.date.today(), latest_issue_date, latest_data_date, latest_row_count))
+        signal_status_list.append(
+            (dashboard_signal.signal_name,
+             datetime.date.today(),
+             latest_issue_date,
+             latest_data_date,
+             latest_row_count))
         coverage_list.append(latest_coverage)
-    
+
     print(signal_status_list)
     print(coverage_list)
 
@@ -97,5 +119,5 @@ def main(args):
 
 
 if __name__ == '__main__':
-  if not main(get_argument_parser().parse_args()):
-    sys.exit(1)
+    if not main(get_argument_parser().parse_args()):
+        sys.exit(1)
