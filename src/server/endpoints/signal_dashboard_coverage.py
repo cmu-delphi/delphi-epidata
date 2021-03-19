4994e576f1e4902b9465a20849071b769679e454
Samuel Gratzl <sam@sgratzl.com>
2021-03-19 10:33:34 -0400

    feat: add new signal dashboard endpoints

---
+++ src/server/endpoints/signal_dashboard_coverage.py
@@ -0,0 +1,30 @@
+from flask import Blueprint
+
+from .._query import execute_query, QueryBuilder
+from .._validate import extract_integer, extract_integers, extract_strings, require_all
+
+# first argument is the endpoint name
+bp = Blueprint("signal_dashboard_coverage", __name__)
+alias = None
+
+
+@bp.route("/", methods=("GET", "POST"))
+def handle():
+    fields_string = ["name", "date", "geo_type", "geo_value"]
+    fields_int = []
+    fields_float = []
+
+    query = """
+    SELECT enabled_signal.`name`,
+        coverage.`date`,
+        coverage.`geo_type`,
+        coverage.`geo_value`
+    FROM (SELECT `id`, `name`, `latest_coverage_update`
+        FROM `dashboard_signal`
+        WHERE `enabled`) AS enabled_signal
+    LEFT JOIN `dashboard_signal_coverage` AS coverage
+    ON enabled_signal.`latest_coverage_update` = coverage.`date`
+    """
+
+    # send query
+    return execute_query(query, {}, fields_string, fields_int, fields_float)
