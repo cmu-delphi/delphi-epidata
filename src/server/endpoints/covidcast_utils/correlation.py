326b5ff4d77e9df8ddeda408498624d2750c17b4
Samuel Gratzl <sam@sgratzl.com>
2021-04-08 14:35:49 -0400

    test: add unit tests

--- src/server/endpoints/covidcast_utils/correlation.py
+++ src/server/endpoints/covidcast_utils/correlation.py
@@ -31,17 +31,63 @@ class CorrelationResult:
         return asdict(self)
 
 
+@dataclass
+class Correlation:
+    r2: float
+
+    slope: float
+    """
+    y = slope * x + intercept
+    """
+    intercept: float
+    """
+    y = slope * x + intercept
+    """
+    samples: int
+    """
+    number of dates used for the regression line
+    """
+
+
+def lag_join(lag: int, x: pd.DataFrame, y: pd.DataFrame) -> pd.DataFrame:
+    # x_t_i ~ y_t_(i-lag)
+    # aka x_t_(i+lag) ~ y_t_i
+
+    if lag == 0:
+        x_shifted = x
+        y_shifted = y
+    elif lag > 0:
+        # x_t_i ~ y_shifted_t_i
+        # shift y such that y_t(i - lag) -> y_shifted_t_i
+        x_shifted = x
+        y_shifted = y.shift(lag, freq="D")
+    else:  # lag < 0
+        # x_shifted_t_i ~ y_t_i
+        # shift x such that x_t(i+lag) -> x_shifted_t_i
+        # lag < 0 -> - - lag = + lag
+        x_shifted = x.shift(-lag, freq="D")
+        y_shifted = y
+    # inner join to remove invalid pairs
+    r = x_shifted.join(y_shifted, how="inner", lsuffix="_x", rsuffix="_y")
+    return r.rename(columns=dict(value_x="x", value_y="y"))
+
+
 def compute_correlations(geo_type: str, geo_value: str, signal_source: str, signal_signal: str, lag: int, x: pd.DataFrame, y: pd.DataFrame) -> Iterable[CorrelationResult]:
     """
-    x,y ... DataFrame with "time_value" as index and "value" column
+    x,y ... DataFrame with "time_value" (Date) index and "value" (float) column
     """
-    xy = x.join(y, how="inner", lsuffix="_x", rsuffix="_y")
     for current_lag in range(-lag, lag + 1):
-        print(current_lag)
-        # TODO shift data by lag
-        yield compute_correlation(geo_type, geo_value, signal_source, signal_signal, current_lag, xy)
+        xy = lag_join(current_lag, x, y)
+        c = compute_correlation(xy)
+
+        yield CorrelationResult(geo_type, geo_value, signal_source, signal_signal, current_lag, r2=c.r2, intercept=c.intercept, slope=c.slope, samples=c.samples)
 
 
-def compute_correlation(geo_type: str, geo_value: str, signal_source: str, signal_signal: str, lag: int, xy: pd.DataFrame) -> CorrelationResult:
+def compute_correlation(xy: pd.DataFrame) -> Correlation:
+    if len(xy) < 2:
+        # won't be a useful one
+        return Correlation(0, 0, 0, len(xy))
+
     model = linregress(xy.to_numpy())
-    return CorrelationResult(geo_type, geo_value, signal_source, signal_signal, lag, r2=float(model.rvalue), intercept=float(model.intercept), slope=float(model.slope), samples=len(xy))
+    r2 = float(model.rvalue) ** 2
+    return Correlation(r2, float(model.slope), float(model.intercept), len(xy))
--- tests/server/endpoints/covidcast_utils/test_trend.py
+++ tests/server/endpoints/covidcast_utils/test_trend.py
@@ -1,61 +1,70 @@
-# standard library
+from typing import Tuple
 import unittest
+import pandas as pd
+from pandas.testing import assert_frame_equal
 
-from delphi.epidata.server.endpoints.covidcast_utils.trend import compute_trend_value, compute_trend_class, TrendEnum, compute_trend, Trend
+from delphi.epidata.server.endpoints.covidcast_utils.correlation import CorrelationResult, lag_join, compute_correlations, compute_correlation, Correlation
+
+
+def as_df(*tuples: Tuple[int, float]) -> pd.DataFrame:
+    df = pd.DataFrame.from_records(tuples, columns=["time_value", "value"])
+    df["time_value"] = pd.to_datetime(df["time_value"], format="%Y%m%d")
+    return df.set_index("time_value")
+
+
+def as_xy_df(*tuples: Tuple[float, float]) -> pd.DataFrame:
+    return pd.DataFrame.from_records(tuples, columns=["x", "y"])
 
 
 class UnitTests(unittest.TestCase):
-    def test_compute_trend_value(self):
-        with self.subTest("same"):
-            self.assertEqual(compute_trend_value(0, 0, 0), 0.0)
-            self.assertEqual(compute_trend_value(5, 5, 0), 0.0)
-            self.assertEqual(compute_trend_value(5, 5, 2), 0.0)
+    def test_lag_join(self):
+        with self.subTest("all data"):
+            x = as_df((20201010, 1), (20201011, 2), (20201012, 3))
+            y = as_df((20201010, 11), (20201011, 12), (20201012, 13))
+            with self.subTest("lag = 0"):
+                # xxx
+                # yyy
+                self.assertEqual(lag_join(0, x, y).to_dict("records"), [dict(x=1, y=11), dict(x=2, y=12), dict(x=3, y=13)])
+            with self.subTest("lag = 1"):
+                # xxx
+                #  yyy
+                self.assertEqual(lag_join(1, x, y).to_dict("records"), [dict(x=2, y=11), dict(x=3, y=12)])
+            with self.subTest("lag = -1"):
+                #  xxx
+                # yyy
+                self.assertEqual(lag_join(-1, x, y).to_dict("records"), [dict(x=1, y=12), dict(x=2, y=13)])
+        with self.subTest("missing entry"):
+            x = as_df((20201010, 1), (20201011, 2), (20201012, 3), (20201013, 4))
+            y = as_df((20201010, 11), (20201012, 13), (20201013, 14))
+            with self.subTest("lag = 0"):
+                # xxxx
+                # y yy
+                self.assertEqual(lag_join(0, x, y).to_dict("records"), [dict(x=1, y=11), dict(x=3, y=13), dict(x=4, y=14)])
+            with self.subTest("lag = 1"):
+                # xxxx
+                #  y yy
+                self.assertEqual(lag_join(1, x, y).to_dict("records"), [dict(x=2, y=11), dict(x=4, y=13)])
+            with self.subTest("lag = -1"):
+                #  xxxx
+                # y yy
+                self.assertEqual(lag_join(-1, x, y).to_dict("records"), [dict(x=2, y=13), dict(x=3, y=14)])
 
-        with self.subTest("with min 0"):
-            self.assertAlmostEqual(compute_trend_value(12, 10, 0), 0.2)
-            self.assertAlmostEqual(compute_trend_value(11, 10, 0), 0.1)
-            self.assertAlmostEqual(compute_trend_value(10.5, 10, 0), 0.05)
-            self.assertAlmostEqual(compute_trend_value(10, 10, 0), 0.0)
-            self.assertAlmostEqual(compute_trend_value(9.5, 10, 0), -0.05)
-            self.assertAlmostEqual(compute_trend_value(9, 10, 0), -0.1)
-            self.assertAlmostEqual(compute_trend_value(8, 10, 0), -0.2)
-        with self.subTest("with min 5"):
-            self.assertAlmostEqual(compute_trend_value(12 + 5, 10 + 5, 5), 0.2)
-            self.assertAlmostEqual(compute_trend_value(11 + 5, 10 + 5, 5), 0.1)
-            self.assertAlmostEqual(compute_trend_value(10.5 + 5, 10 + 5, 5), 0.05)
-            self.assertAlmostEqual(compute_trend_value(10 + 5, 10 + 5, 5), 0.0)
-            self.assertAlmostEqual(compute_trend_value(9.5 + 5, 10 + 5, 5), -0.05)
-            self.assertAlmostEqual(compute_trend_value(9 + 5, 10 + 5, 5), -0.1)
-            self.assertAlmostEqual(compute_trend_value(8 + 5, 10 + 5, 5), -0.2)
+    def test_compute_correlation(self):
+        with self.subTest("simple"):
+            xy = as_xy_df((1, 11), (2, 12), (3, 13))
+            self.assertEqual(compute_correlation(xy), Correlation(r2=1, intercept=10, slope=1, samples=3))
+        with self.subTest("inverted"):
+            xy = as_xy_df((1, 13), (2, 12), (3, 11))
+            self.assertEqual(compute_correlation(xy), Correlation(r2=1, intercept=14, slope=-1, samples=3))
+        with self.subTest("none"):
+            xy = as_xy_df((1, 0), (2, 0), (3, 0))
+            self.assertEqual(compute_correlation(xy), Correlation(r2=0, intercept=0, slope=0, samples=3))
 
-    def test_compute_trend_class(self):
-        self.assertEqual(compute_trend_class(-0.3), TrendEnum.decreasing)
-        self.assertEqual(compute_trend_class(-0.2), TrendEnum.decreasing)
-        self.assertEqual(compute_trend_class(-0.1), TrendEnum.decreasing)
-        self.assertEqual(compute_trend_class(-0.05), TrendEnum.steady)
-        self.assertEqual(compute_trend_class(0), TrendEnum.steady)
-        self.assertEqual(compute_trend_class(0.05), TrendEnum.steady)
-        self.assertEqual(compute_trend_class(0.1), TrendEnum.increasing)
-        self.assertEqual(compute_trend_class(0.2), TrendEnum.increasing)
-        self.assertEqual(compute_trend_class(0.3), TrendEnum.increasing)
+    def test_compute_correlations(self):
+        x = as_df((20201010, 1), (20201011, 2), (20201012, 3))
+        y = as_df((20201010, 11), (20201011, 12), (20201012, 13))
 
-    def test_compute_trend(self):
-        self.assertEqual(
-            compute_trend("gt", "gv", "so", "si", 10, 8, [(10, 12), (8, 10), (0, 0)]),
-            Trend(
-                "gt",
-                "gv",
-                "so",
-                "si",
-                value=12,
-                basis_date=8,
-                basis_value=10,
-                basis_trend=TrendEnum.increasing,
-                min_date=0,
-                min_value=0,
-                min_trend=TrendEnum.increasing,
-                max_date=10,
-                max_value=12,
-                max_trend=TrendEnum.steady,
-            ),
-        )
+        r = list(compute_correlations("gt", "gv", "so", "si", 2, x, y))
+        self.assertEqual(len(r), 5)
+        # lag 0
+        self.assertEqual(r[2], CorrelationResult("gt", "gv", "so", "si", 0, r2=1, intercept=10, slope=1, samples=3))
