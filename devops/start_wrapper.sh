368a2fa65b58b4592b0b004cf65e6a5cc8ea0aa2
Brian Clark <clark.bg@gmail.com>
2021-04-16 14:39:00 -0400

    Add simple wrapper script

---
+++ devops/start_wrapper.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+set -e
+
+# If a New Relic license key is found then we start with custom New Relic
+# commands, otherwise we start via start.sh.
+if [[ -z "${NEW_RELIC_LICENSE_KEY}" ]]; then
+  sh start.sh
+else
+  newrelic-admin run-command start.sh
+fi
