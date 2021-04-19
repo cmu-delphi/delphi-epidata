d09c0f311d094b2567da78021a35ea8f81942540
Brian Clark <clark.bg@gmail.com>
2021-04-19 09:34:38 -0400

    [[ not sh compatible, use absolute path for start.sh

--- devops/start_wrapper.sh
+++ devops/start_wrapper.sh
@@ -3,8 +3,8 @@ set -e
 
 # If a New Relic license key is found then we start with custom New Relic
 # commands, otherwise we start via start.sh.
-if [[ -z "${NEW_RELIC_LICENSE_KEY}" ]]; then
-  sh start.sh
+if [ -z "${NEW_RELIC_LICENSE_KEY}" ]; then
+  sh /start.sh
 else
   newrelic-admin run-command start.sh
 fi
