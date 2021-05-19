8c780961598d1a735c74474c300266dfc0f2847c
Dmitry Shemetov <dshemetov@ucdavis.edu>
2021-05-19 12:55:43 -0700

    Client cache: fix call_args_list

--- integrations/client/test_delphi_epidata.py
+++ integrations/client/test_delphi_epidata.py
@@ -292,14 +292,14 @@ class DelphiEpidataPythonClientTests(unittest.TestCase):
       Session = MagicMock()
       _Session.return_value = Session
       Epidata.covidcast('src', 'sig', 'day', 'county', 20200414, '01234')
-      assert call('get') in Session.request.call_args_list()
-      assert call('post') not in Session.request.call_args_list()
+      assert call('get') in Session.request.call_args_list
+      assert call('post') not in Session.request.call_args_list
     with self.subTest(name='get request, cache'):
       CachedSession = MagicMock()
       _CachedSession.return_value = CachedSession
       Epidata.covidcast('src', 'sig', 'day', 'county', 20200414, '01234', cache_timeout=5)
-      assert call('get') in CachedSession.request.call_args_list()
-      assert call('post') not in CachedSession.request.call_args_list()
+      assert call('get') in CachedSession.request.call_args_list
+      assert call('post') not in CachedSession.request.call_args_list
     with self.subTest(name='post request'):
       mock_response = MagicMock()
       mock_response.status_code = 414
@@ -307,8 +307,8 @@ class DelphiEpidataPythonClientTests(unittest.TestCase):
       Session.request.return_value = mock_response
       _Session.return_value = Session
       Epidata.covidcast('src', 'sig', 'day', 'county', 20200414, '01234')
-      assert call('get') in Session.request.call_args_list()
-      assert call('post') in Session.request.call_args_list()
+      assert call('get') in Session.request.call_args_list
+      assert call('post') in Session.request.call_args_list
 
   def test_geo_value(self):
     """test different variants of geo types: single, *, multi."""
