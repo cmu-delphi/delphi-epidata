3730743f8659f308cb235db7acc1726df2e2a099
Samuel Gratzl <sam@sgratzl.com>
2021-06-10 15:15:17 +0200

    fix: load inspector on the fly

--- src/server/_db.py
+++ src/server/_db.py
@@ -7,7 +7,6 @@ from ._config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_ENGINE_OPTIONS
 
 engine: Engine = create_engine(SQLALCHEMY_DATABASE_URI, **SQLALCHEMY_ENGINE_OPTIONS)
 metadata = MetaData(bind=engine)
-inspector: Inspector = inspect(engine)
 
 TABLE_OPTIONS = dict(
     mysql_engine="InnoDB",
@@ -21,6 +20,7 @@ def sql_table_has_columns(table: str, columns: List[str]) -> bool:
     """
     checks whether the given table has all the given columns defined
     """
+    inspector: Inspector = inspect(engine)
     table_columns: List[Dict] = inspector.get_columns(table)
     table_column_names = set(str(d.get("name", "")).lower() for d in table_columns)
     return all(c.lower() in table_column_names for c in columns)
