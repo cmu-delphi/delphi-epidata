2447452a9faa23a6b656b4f8fe80c4ca98f1ce05
然然 <52230092+zhuoran-cheng@users.noreply.github.com>
2021-06-29 12:06:58 -0400

    delete some unneccesary part of logger

--- src/acquisition/covidcast/generate_islatest_fix_sql.py
+++ src/acquisition/covidcast/generate_islatest_fix_sql.py
@@ -1,10 +1,7 @@
-from delphi.epidata.acquisition.covidcast.logger import get_structured_logger
 # what data to operate on
 base_where_clause = "WHERE `source`='jhu-csse' AND `time_type`='day'"
 ### base_where_clause = "WHERE `source`='src2' AND `time_type`='day'" ###
 
-logger = get_structured_logger(
-    "generate_islatest_fix_sql")
 
 # signal name construction
 # NOTE: selecting these (unique) from the database takes 7-8 mins, so reconstructing here for efficiency
@@ -17,15 +14,13 @@ for case in ('confirmed_', 'deaths_'):
       for typ in ('num', 'prop'):
         signals.append(case+period+count+typ)
 ### signals = ['sig2'] ###
-
-
 # variable to split on, 'time_value' is good because its high cardinality is suitable for chunking
 PARTITION_VARIABLE = 'time_value'
 PARTITION_SPLITS = [20200101 + i*100 for i in range(10)] # first day of the month for jan - oct 2020 in YYYYMMDD form
 ### PARTITION_SPLITS = [1,2] ###
 
 
-logger.info('''
+print('''
 -- 
 -- run this as:
 --   python3 generate_islatest_fix_sql.py > islatest_fix.sql
@@ -36,18 +31,17 @@ logger.info('''
 ''')
 
 # create temp table
-logger.info("CREATE TABLE `islatest_fix` (`latest_id` INT(11) NOT NULL, PRIMARY KEY (`latest_id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;")
+print("CREATE TABLE `islatest_fix` (`latest_id` INT(11) NOT NULL, PRIMARY KEY (`latest_id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;")
 
 # find latest issue by partition (and by signal) and save primary ids into temp table
 for partition_index in range(len(PARTITION_SPLITS)+1):
   ge_condition = 'TRUE' if partition_index == 0 else f'`{PARTITION_VARIABLE}` >= {PARTITION_SPLITS[partition_index - 1]}'
   l_condition = 'TRUE' if partition_index == len(PARTITION_SPLITS) else f'`{PARTITION_VARIABLE}` < {PARTITION_SPLITS[partition_index]}'
   partition_condition = f'({ge_condition}) AND ({l_condition})'
-
   for sig in signals:
     where_clause = base_where_clause + " AND `signal`='%s' AND %s"  % (sig, partition_condition)
 
-    logger.info('''
+    print('''
 INSERT INTO `islatest_fix`
   SELECT id FROM
     ( SELECT `source`, `signal`, `time_type`, `geo_type`, `geo_value`, `time_value`, MAX(`issue`) AS `issue` FROM `covidcast`
@@ -59,11 +53,11 @@ INSERT INTO `islatest_fix`
 ''')
 
 # clear any current (potentially erroneous) is_latest_issue flags
-logger.info("UPDATE `covidcast` SET `is_latest_issue`=0 " + base_where_clause + " AND `is_latest_issue`=1;")
+print("UPDATE `covidcast` SET `is_latest_issue`=0 " + base_where_clause + " AND `is_latest_issue`=1;")
 
 # re-set proper is_latest_issue flags
-logger.info("UPDATE (SELECT `latest_id` FROM `islatest_fix`) xxx LEFT JOIN `covidcast` ON `xxx`.`latest_id`=`covidcast`.`id` SET `covidcast`.`is_latest_issue`=1;")
+print("UPDATE (SELECT `latest_id` FROM `islatest_fix`) xxx LEFT JOIN `covidcast` ON `xxx`.`latest_id`=`covidcast`.`id` SET `covidcast`.`is_latest_issue`=1;")
 
 # clean up temp table
-logger.info("-- TODO: drop this table")
-logger.info("-- DROP TABLE `islatest_fix`;")
+print("-- TODO: drop this table")
+print("-- DROP TABLE `islatest_fix`;")
