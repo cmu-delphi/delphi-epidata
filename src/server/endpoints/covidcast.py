e0bf67826fa86d15fd64c9ca95a44ea8154e6e37
Samuel Gratzl <sam@sgratzl.com>
2021-07-20 18:33:27 +0200

    fix: use printer for sending meta data

--- src/server/endpoints/covidcast.py
+++ src/server/endpoints/covidcast.py
@@ -2,7 +2,7 @@ from typing import List, Optional, Union, Tuple, Dict, Any
 from itertools import groupby
 from datetime import date, datetime, timedelta
 from flask import Blueprint, request
-from flask.json import loads, jsonify
+from flask.json import loads
 from bisect import bisect_right
 from sqlalchemy import text
 from pandas import read_csv
@@ -23,7 +23,7 @@ from .._params import (
     parse_single_geo_arg,
 )
 from .._query import QueryBuilder, execute_query, run_query, parse_row, filter_fields
-from .._printer import create_printer, CSVPrinter
+from .._printer import create_printer, CSVPrinter, JSONLPrinter, JSONPrinter
 from .._validate import (
     extract_date,
     extract_dates,
@@ -530,41 +530,42 @@ def handle_meta():
         entry = by_signal.setdefault((row["data_source"], row["signal"]), [])
         entry.append(row)
 
-    sources: List[Dict[str, Any]] = []
-    for source in data_sources:
-        if filter_active is not None and source.active != filter_active:
-            continue
+    def gen():
+        for source in data_sources:
+            if filter_active is not None and source.active != filter_active:
+                continue
 
-        meta_signals: List[Dict[str, Any]] = []
+            meta_signals: List[Dict[str, Any]] = []
 
-        for signal in source.signals:
-            if filter_signal and all((not s.matches(signal.source, signal.signal) for s in filter_signal)):
-                continue
-            if filter_smoothed is not None and signal.is_smoothed != filter_smoothed:
-                continue
-            if filter_weighted is not None and signal.is_weighted != filter_weighted:
-                continue
-            if filter_cumulative is not None and signal.is_cumulative != filter_cumulative:
-                continue
-            if filter_time_type is not None and signal.time_type != filter_time_type:
-                continue
-            meta_data = by_signal.get((source.db_source, signal.signal))
-            if not meta_data:
-                continue
-            row = meta_data[0]
-            entry = CovidcastMetaEntry(signal, row["min_time"], row["max_time"], row["max_issue"])
-            for row in meta_data:
-                entry.intergrate(row)
-            meta_signals.append(entry.asdict())
+            for signal in source.signals:
+                if filter_signal and all((not s.matches(signal.source, signal.signal) for s in filter_signal)):
+                    continue
+                if filter_smoothed is not None and signal.is_smoothed != filter_smoothed:
+                    continue
+                if filter_weighted is not None and signal.is_weighted != filter_weighted:
+                    continue
+                if filter_cumulative is not None and signal.is_cumulative != filter_cumulative:
+                    continue
+                if filter_time_type is not None and signal.time_type != filter_time_type:
+                    continue
+                meta_data = by_signal.get((source.db_source, signal.signal))
+                if not meta_data:
+                    continue
+                row = meta_data[0]
+                entry = CovidcastMetaEntry(signal, row["min_time"], row["max_time"], row["max_issue"])
+                for row in meta_data:
+                    entry.intergrate(row)
+                meta_signals.append(entry.asdict())
 
-        if not meta_signals:  # none found or no signals
-            continue
+            if not meta_signals:  # none found or no signals
+                continue
 
-        s = source.asdict()
-        s["signals"] = meta_signals
-        sources.append(s)
+            s = source.asdict()
+            s["signals"] = meta_signals
+            yield s
 
-    return jsonify(sources)
+    p = JSONLPrinter() if request.values.get('format') == 'jsonl' else JSONPrinter()
+    return p(gen())
 
 
 @bp.route("/coverage", methods=("GET", "POST"))
