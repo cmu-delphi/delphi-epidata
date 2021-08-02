a7f2b081e4ee014662323ad6fabf5a59039b8c6f
Samuel Gratzl <sam@sgratzl.com>
2021-08-02 10:15:58 -0400

    fix: tests

--- tests/server/endpoints/test_covidcast.py
+++ tests/server/endpoints/test_covidcast.py
@@ -30,15 +30,15 @@ class UnitTests(unittest.TestCase):
         rv: Response = self.client.get("/covidcast/", follow_redirects=True)
         msg = rv.get_json()
         self.assertEqual(rv.status_code, 200)
-        self.assertEqual(msg["result"], -1)
         self.assertRegex(msg["message"], r"missing parameter.*")
+        self.assertEqual(msg["result"], -1)
 
     def test_time(self):
         rv: Response = self.client.get("/covidcast/", query_string=dict(signal="src1:*", time="day:20200101", geo="state:*"))
         msg = rv.get_json()
         self.assertEqual(rv.status_code, 200)
-        self.assertEqual(msg["result"], -2)  # no result
         self.assertEqual(msg["message"], "no results")
+        self.assertEqual(msg["result"], -2)  # no result
 
     def test_guess_index_to_use(self):
         self.assertEqual(guess_index_to_use([TimePair("day", True)], [GeoPair("county", ["a"])], issues=None, lag=None, as_of=None), "by_issue")
