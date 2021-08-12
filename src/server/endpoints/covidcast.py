5a7838e0095ee4ad3fa020701ebf426fc68e7a3d
Samuel Gratzl <sam@sgratzl.com>
2021-08-12 10:30:29 -0400

    fix: fix merge error

--- src/server/endpoints/covidcast.py
+++ src/server/endpoints/covidcast.py
@@ -530,30 +530,13 @@ def handle_meta():
         entry = by_signal.setdefault((row["data_source"], row["signal"]), [])
         entry.append(row)
 
-    sources: List[Dict[str, Any]] = []
-    for source in data_sources:
-        meta_signals: List[Dict[str, Any]] = []
-
-        for signal in source.signals:
-            if filter_active is not None and signal.active != filter_active:
-                continue
-            if filter_signal and all((not s.matches(signal.source, signal.signal) for s in filter_signal)):
-                continue
-            if filter_smoothed is not None and signal.is_smoothed != filter_smoothed:
-                continue
-            if filter_weighted is not None and signal.is_weighted != filter_weighted:
-                continue
-            if filter_cumulative is not None and signal.is_cumulative != filter_cumulative:
-                continue
-            if filter_time_type is not None and signal.time_type != filter_time_type:
-                continue
-            meta_data = by_signal.get((source.db_source, signal.signal))
-            if not meta_data:
-                continue
-
+    def gen():
+        for source in data_sources:
             meta_signals: List[Dict[str, Any]] = []
 
             for signal in source.signals:
+                if filter_active is not None and signal.active != filter_active:
+                    continue
                 if filter_signal and all((not s.matches(signal.source, signal.signal) for s in filter_signal)):
                     continue
                 if filter_smoothed is not None and signal.is_smoothed != filter_smoothed:
@@ -567,6 +550,7 @@ def handle_meta():
                 meta_data = by_signal.get((source.db_source, signal.signal))
                 if not meta_data:
                     continue
+
                 row = meta_data[0]
                 entry = CovidcastMetaEntry(signal, row["min_time"], row["max_time"], row["max_issue"])
                 for row in meta_data:
