8d71088f94188390006e02e944c199748a0cf7b6
Tara Lakdawala <taralakdawala@taras-mbp.wifi.local.cmu.edu>
2021-10-26 17:02:31 -0400

    flask queries

--- src/server/endpoints/covidcast_interim.py
+++ src/server/endpoints/covidcast_interim.py
@@ -61,3 +61,58 @@ coverage_sql = f'''
     GROUP BY ref.`source`, ref.`signal`, ref.`time_value` 
     ORDER BY ref.`source` ASC, ref.`signal` ASC, ref.`time_value` ASC
 '''
+
+coverage_sql = '''SELECT 
+count(ref.`geo_value`) as count, ref.`source`, ref.`signal`, ref.`time_value`, 
+FROM 
+data_reference ref 
+WHERE (ref.`source` = "jhu-csse" AND
+ref.`signal` = "deaths_incidence_prop") AND
+ref.`geo_type` = "county" AND
+ref.`time_type` = "day" AND
+ref.`time_value` BETWEEN x and y AND
+ORDER BY
+ref.`source` ASC,
+ref.`signal` ASC,
+data.`time_value` ASC'''
+
+backfill_sql = '''SELECT
+ref.time_value, 
+data.issue, 
+data.value, 
+data.sample_size 
+FROM 
+data_reference ref
+INNER JOIN datapoint data 
+ON ref.latest_datapoint_id = data.id
+WHERE ref.source = :source_0t AND 
+ref.signal = :signal AND 
+ref.geo_type = :geo_type AND 
+ref.geo_value = :geo_Type AND 
+ref.time_type = :time_type AND 
+ref.time_value BETWEEN :time_x AND :time_y
+ORDER BY 
+ref.time_value ASC, 
+data.issue ASC
+'''
+
+csv_sql = '''SELECT ref.geo_value
+ref.signal
+ref.time_value
+data.issue
+data.lag
+data.value
+data.stderr,
+data.sample_size,
+FROM data_reference
+INNER JOIN datapoint data
+ON ref.latest_datapoint_id = data.id
+WHERE ref.source = :source AND
+ref.signal = :signal AND
+ref.time_type = :time_type AND
+ref.time_value = BETWEEN :time x AND :time y AND
+ref.geo_type = :geo_type
+ORDER BY ref.time_value ASC,
+ref.geo_value ASC'''
+
+meta_sql = '''SELECT epidata FROM covidcast_meta_cache'''
