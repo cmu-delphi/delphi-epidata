c36bb6113b95ac7d335cd0c0332097ad3de62695
krivard <krivard@users.noreply.github.com>
2022-01-31 17:33:14 +0000

    chore: release delphi-epidata 0.3.8

--- src/client/delphi_epidata.js
+++ src/client/delphi_epidata.js
@@ -22,7 +22,7 @@
   }
 })(this, function (exports, fetchImpl, jQuery) {
   const BASE_URL = "https://delphi.cmu.edu/epidata/";
-  const client_version = "0.3.7";
+  const client_version = "0.3.8";
 
   // Helper function to cast values and/or ranges to strings
   function _listitem(value) {
