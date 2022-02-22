1b5bf2a8c29cf50cd13156cd35adba950ba6cdc7
Woochan Lee <blee3461@gmail.com>
2022-02-21 22:02:21 -0500

    Change SQLAlchemy logging level and add comment

--- src/server/main.py
+++ src/server/main.py
@@ -69,5 +69,5 @@ else:
     app.logger.setLevel(gunicorn_logger.level)
     sqlalchemy_logger = logging.getLogger("sqlalchemy")
     sqlalchemy_logger.handlers = gunicorn_logger.handlers
+    # Change SQLAlchemy logging level to "ERROR" in order to prevent query details of API keys from being logged 
     sqlalchemy_logger.setLevel(logging.ERROR)
-    #sqlalchemy_logger.setLevel(gunicorn_logger.level)
