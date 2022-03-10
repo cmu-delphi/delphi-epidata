7bb3d73b02460ac56b4da7d4c2e91cb1a93f3749
george haff <george.haff@gmail.com>
2022-03-10 15:23:15 -0500

    comments and TODOs on v4 DDL (SQL) code for consideration note im not actually changing functional stuff to keep it in sync with Joe's copy of the table creation statements

--- src/ddl/v4_schema.sql
+++ src/ddl/v4_schema.sql
@@ -25,24 +25,24 @@ CREATE TABLE signal_history (
     `signal_data_id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
     `signal_key_id` BIGINT(20) UNSIGNED,
     `geo_key_id` BIGINT(20) UNSIGNED,
-    `demog_key_id` BIGINT(20) UNSIGNED,
+    `demog_key_id` BIGINT(20) UNSIGNED,  -- TODO: for future use ; also rename s/demog/stratification/  ...remove?
     `issue` INT(11),
-    `data_as_of_dt` DATETIME(0),
+    `data_as_of_dt` DATETIME(0),  -- TODO: for future use ; also "as_of" is problematic and should be renamed  ...remove?
     `time_type` VARCHAR(12) NOT NULL,
     `time_value` INT(11) NOT NULL,
-    `reference_dt` DATETIME(0),
+    `reference_dt` DATETIME(0),  -- TODO: for future use  ...remove?
     `value` DOUBLE NULL DEFAULT NULL,
     `stderr` DOUBLE NULL DEFAULT NULL,
     `sample_size` DOUBLE NULL DEFAULT NULL,
     `lag` INT(11) NOT NULL,
     `value_updated_timestamp` INT(11) NOT NULL,
-    `computation_as_of_dt` DATETIME(0),
-    `is_latest_issue` BINARY(1) NOT NULL DEFAULT '0',
+    `computation_as_of_dt` DATETIME(0),  -- TODO: for future use ; also "as_of" is problematic and should be renamed  ...remove?
+    `is_latest_issue` BINARY(1) NOT NULL DEFAULT '0', -- TODO: delete this, its hard to keep updated and its not currently used
     `missing_value` INT(1) NULL DEFAULT '0',
     `missing_stderr` INT(1) NULL DEFAULT '0',
     `missing_sample_size` INT(1) NULL DEFAULT '0',
-    `id` BIGINT(20) UNSIGNED NULL DEFAULT NULL,
-    
+    `id` BIGINT(20) UNSIGNED NULL DEFAULT NULL, -- TODO: delete this, it is not used or needed
+ 
     PRIMARY KEY (`signal_data_id`) USING BTREE,
     UNIQUE INDEX `value_key` (`signal_key_id`,`geo_key_id`,`issue`,`time_type`,`time_value`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=4000000001;
@@ -52,18 +52,18 @@ CREATE TABLE signal_latest (
     `signal_data_id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
     `signal_key_id` BIGINT(20) UNSIGNED,
     `geo_key_id` BIGINT(20) UNSIGNED,
-    `demog_key_id` BIGINT(20) UNSIGNED,
+    `demog_key_id` BIGINT(20) UNSIGNED,  -- TODO: for future use ; also rename s/demog/stratification/  ...remove?
     `issue` INT(11),
-    `data_as_of_dt` DATETIME(0),
+    `data_as_of_dt` DATETIME(0),  -- TODO: for future use ; also "as_of" is problematic and should be renamed  ...remove?
     `time_type` VARCHAR(12) NOT NULL,
     `time_value` INT(11) NOT NULL,
-    `reference_dt` DATETIME(0),
+    `reference_dt` DATETIME(0),  -- TODO: for future use  ...remove?
     `value` DOUBLE NULL DEFAULT NULL,
     `stderr` DOUBLE NULL DEFAULT NULL,
     `sample_size` DOUBLE NULL DEFAULT NULL,
     `lag` INT(11) NOT NULL,
     `value_updated_timestamp` INT(11) NOT NULL,
-    `computation_as_of_dt` DATETIME(0),
+    `computation_as_of_dt` DATETIME(0),  -- TODO: for future use ; also "as_of" is problematic and should be renamed  ...remove?
     `missing_value` INT(1) NULL DEFAULT '0',
     `missing_stderr` INT(1) NULL DEFAULT '0',
     `missing_sample_size` INT(1) NULL DEFAULT '0',
@@ -77,31 +77,32 @@ CREATE TABLE signal_load (
     `signal_data_id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
     `signal_key_id` BIGINT(20) UNSIGNED,
     `geo_key_id` BIGINT(20) UNSIGNED,
-    `demog_key_id` BIGINT(20) UNSIGNED,
+    `demog_key_id` BIGINT(20) UNSIGNED,  -- TODO: for future use ; also rename s/demog/stratification/  ...remove?
     `issue` INT(11),
-    `data_as_of_dt` DATETIME(0),
+    `data_as_of_dt` DATETIME(0),  -- TODO: for future use ; also "as_of" is problematic and should be renamed  ...remove?
     `source` VARCHAR(32) NOT NULL,
     `signal` VARCHAR(64) NOT NULL,
     `geo_type` VARCHAR(12) NOT NULL,
     `geo_value` VARCHAR(12) NOT NULL,
     `time_type` VARCHAR(12) NOT NULL,
     `time_value` INT(11) NOT NULL,
-    `reference_dt` DATETIME(0),
+    `reference_dt` DATETIME(0),  -- TODO: for future use  ...remove?
     `value` DOUBLE NULL DEFAULT NULL,
     `stderr` DOUBLE NULL DEFAULT NULL,
     `sample_size` DOUBLE NULL DEFAULT NULL,
     `lag` INT(11) NOT NULL,
     `value_updated_timestamp` INT(11) NOT NULL,
-    `computation_as_of_dt` DATETIME(0),
+    `computation_as_of_dt` DATETIME(0),  -- TODO: for future use ; also "as_of" is problematic and should be renamed  ...remove?
     `is_latest_issue` BINARY(1) NOT NULL DEFAULT '0',
     `missing_value` INT(1) NULL DEFAULT '0',
     `missing_stderr` INT(1) NULL DEFAULT '0',
     `missing_sample_size` INT(1) NULL DEFAULT '0',
-    `id` BIGINT(20) UNSIGNED,
+    `id` BIGINT(20) UNSIGNED, -- TODO: delete this, it is not needed or used
     `compressed_signal_key` VARCHAR(100),
     `compressed_geo_key` VARCHAR(100),
-    `compressed_demog_key` VARCHAR(100),
-    `process_status` VARCHAR(2) DEFAULT 'l',
+    `compressed_demog_key` VARCHAR(100),  -- TODO: for future use ; also rename s/demog/stratification/  ...remove?
+    `process_status` VARCHAR(2) DEFAULT 'l', -- using codes: 'i' (I) for "inserting", 'l' (L) for "loaded", and 'b' for "batching"
+        -- TODO: change `process_status` default to 'i' (I) "inserting" or even 'x'/'u' "undefined" ?
 
     PRIMARY KEY (`signal_data_id`) USING BTREE,
     INDEX `comp_signal_key` (`compressed_signal_key`) USING BTREE,
@@ -118,24 +119,24 @@ CREATE OR REPLACE VIEW signal_history_v AS
         `t2`.`signal` AS `signal`,
         `t3`.`geo_type` AS `geo_type`,
         `t3`.`geo_value` AS `geo_value`,
-        `t1`.`signal_data_id` AS `signal_data_id`,
-        `t1`.`demog_key_id` AS `demog_key_id`,
+        `t1`.`signal_data_id` AS `signal_data_id`, -- TODO: unnecessary  ...remove?
+        `t1`.`demog_key_id` AS `demog_key_id`, -- TODO: for future use ; also rename s/demog/stratification/  ...remove?
         `t1`.`issue` AS `issue`,
-        `t1`.`data_as_of_dt` AS `data_as_of_dt`,
+        `t1`.`data_as_of_dt` AS `data_as_of_dt`, -- TODO: for future use ; also "as_of" is problematic and should be renamed  ...remove?
         `t1`.`time_type` AS `time_type`,
         `t1`.`time_value` AS `time_value`,
-        `t1`.`reference_dt` AS `reference_dt`,
+        `t1`.`reference_dt` AS `reference_dt`, -- TODO: for future use  ...remove?
         `t1`.`value` AS `value`,
         `t1`.`stderr` AS `stderr`,
         `t1`.`sample_size` AS `sample_size`,
         `t1`.`lag` AS `lag`,
         `t1`.`value_updated_timestamp` AS `value_updated_timestamp`,
-        `t1`.`computation_as_of_dt` AS `computation_as_of_dt`,
+        `t1`.`computation_as_of_dt` AS `computation_as_of_dt`, -- TODO: for future use ; also "as_of" is problematic and should be renamed  ...remove?
         `t1`.`missing_value` AS `missing_value`,
         `t1`.`missing_stderr` AS `missing_stderr`,
         `t1`.`missing_sample_size` AS `missing_sample_size`,
-        `t1`.`signal_key_id` AS `signal_key_id`,
-        `t1`.`geo_key_id` AS `geo_key_id` 
+        `t1`.`signal_key_id` AS `signal_key_id`, -- TODO: unnecessary  ...remove?
+        `t1`.`geo_key_id` AS `geo_key_id`  -- TODO: unnecessary  ...remove?
     FROM ((`signal_history` `t1` 
         JOIN `signal_dim` `t2` 
             USE INDEX (PRIMARY) 
@@ -153,24 +154,24 @@ CREATE OR REPLACE VIEW signal_latest_v AS
         `t2`.`signal` AS `signal`,
         `t3`.`geo_type` AS `geo_type`,
         `t3`.`geo_value` AS `geo_value`,
-        `t1`.`signal_data_id` AS `signal_data_id`,
-        `t1`.`demog_key_id` AS `demog_key_id`,
+        `t1`.`signal_data_id` AS `signal_data_id`, -- TODO: unnecessary  ...remove?
+        `t1`.`demog_key_id` AS `demog_key_id`, -- TODO: for future use ; also rename s/demog/stratification/  ...remove?
         `t1`.`issue` AS `issue`,
-        `t1`.`data_as_of_dt` AS `data_as_of_dt`,
+        `t1`.`data_as_of_dt` AS `data_as_of_dt`, -- TODO: for future use ; also "as_of" is problematic and should be renamed  ...remove?
         `t1`.`time_type` AS `time_type`,
         `t1`.`time_value` AS `time_value`,
-        `t1`.`reference_dt` AS `reference_dt`,
+        `t1`.`reference_dt` AS `reference_dt`, -- TODO: for future use  ...remove?
         `t1`.`value` AS `value`,
         `t1`.`stderr` AS `stderr`,
         `t1`.`sample_size` AS `sample_size`,
         `t1`.`lag` AS `lag`,
         `t1`.`value_updated_timestamp` AS `value_updated_timestamp`,
-        `t1`.`computation_as_of_dt` AS `computation_as_of_dt`,
+        `t1`.`computation_as_of_dt` AS `computation_as_of_dt`, -- TODO: for future use ; also "as_of" is problematic and should be renamed  ...remove?
         `t1`.`missing_value` AS `missing_value`,
         `t1`.`missing_stderr` AS `missing_stderr`,
         `t1`.`missing_sample_size` AS `missing_sample_size`,
-        `t1`.`signal_key_id` AS `signal_key_id`,
-        `t1`.`geo_key_id` AS `geo_key_id` 
+        `t1`.`signal_key_id` AS `signal_key_id`, -- TODO: unnecessary  ...remove?
+        `t1`.`geo_key_id` AS `geo_key_id`  -- TODO: unnecessary  ...remove?
     FROM ((`signal_latest` `t1` 
         JOIN `signal_dim` `t2` 
             USE INDEX (PRIMARY) 
