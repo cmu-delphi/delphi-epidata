9b99e1c2093895d93f0207ef844299a44d12e89c
krivard <krivard@users.noreply.github.com>
2022-03-28 19:22:56 +0000

    chore: release delphi-epidata 0.3.16

--- src/client/packaging/pypi/setup.py
+++ src/client/packaging/pypi/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="delphi_epidata",
-    version="0.3.15",
+    version="0.3.16",
     author="David Farrow",
     author_email="dfarrow0@gmail.com",
     description="A programmatic interface to Delphi's Epidata API.",
