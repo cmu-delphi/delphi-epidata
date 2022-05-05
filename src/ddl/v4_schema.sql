1c1ce15440fa139c77e4a2f48a34aeca18ea833b
Joe Greene <jbgreene@the-greenes.com>
2022-05-05 12:30:10 +0000

    changes for merged dim
    * schema
    * database:run_dbjobs

--- src/ddl/v4_schema.sql
+++ src/ddl/v4_schema.sql
@@ -22,8 +22,25 @@ CREATE TABLE signal_dim (
 ) ENGINE=InnoDB;
 
 
+-- Merged dim table combines geo_dim and signal_dim
+-- merged_key_id added to signal tables and views
+CREATE TABLE merged_dim
+    (`merged_key_id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `signal_key_id` INT,
+    `geo_key_id` INT,
+    `source` VARCHAR(32),
+    `signal` VARCHAR(64),
+    `geo_type` VARCHAR(12),
+    `geo_value` VARCHAR(12),
+    PRIMARY KEY (`merged_key_id`) USING BTREE,
+    UNIQUE INDEX `values` (`source`, `signal`, `geo_type`, `geo_value`) USING BTREE,
+    INDEX `dim_ids` (`signal_key_id`, `geo_key_id`) USING BTREE
+) COLLATE='utf8mb4_0900_ai_ci' ENGINE=InnoDB;
+
+
 CREATE TABLE signal_history (
     `signal_data_id` BIGINT(20) UNSIGNED NOT NULL,
+    `merged_key_id` BIGINT(19) NULL DEFAULT NULL,
     `signal_key_id` BIGINT(20) UNSIGNED,
     `geo_key_id` BIGINT(20) UNSIGNED,
     `demog_key_id` BIGINT(20) UNSIGNED,  -- TODO: for future use ; also rename s/demog/stratification/
@@ -65,6 +82,7 @@ CREATE TABLE signal_load (
     `signal_data_id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
     `signal_key_id` BIGINT(20) UNSIGNED,
     `geo_key_id` BIGINT(20) UNSIGNED,
+    `merged_key_id` BIGINT(19) NULL DEFAULT NULL,
     `demog_key_id` BIGINT(20) UNSIGNED,  -- TODO: for future use ; also rename s/demog/stratification/
     `issue` INT(11),
     `data_as_of_dt` DATETIME(0),  -- TODO: for future use ; also "as_of" is problematic and should be renamed
@@ -97,75 +115,68 @@ CREATE TABLE signal_load (
 ) ENGINE=InnoDB AUTO_INCREMENT=4000000001;
 
 
-CREATE OR REPLACE VIEW signal_history_v AS
+CREATE OR REPLACE VIEW covid.signal_history_v AS
     SELECT
-        0 AS is_latest_issue, -- provides column-compatibility to match `covidcast` table
-        -- ^ this value is essentially undefined in this view, the notion of a 'latest' issue is not encoded here and must be drawn from the 'latest' table or view or otherwise computed...
-        NULL AS direction, -- provides column-compatibility to match `covidcast` table
+        0 AS `is_latest_issue`,
+        NULL AS `direction`,
         `t2`.`source` AS `source`,
         `t2`.`signal` AS `signal`,
-        `t3`.`geo_type` AS `geo_type`,
-        `t3`.`geo_value` AS `geo_value`,
+        `t2`.`geo_type` AS `geo_type`,
+        `t2`.`geo_value` AS `geo_value`,
         `t1`.`signal_data_id` AS `signal_data_id`,
         `t1`.`demog_key_id` AS `demog_key_id`, -- TODO: for future use ; also rename s/demog/stratification/  ...remove?
         `t1`.`issue` AS `issue`,
-        `t1`.`data_as_of_dt` AS `data_as_of_dt`, -- TODO: for future use ; also "as_of" is problematic and should be renamed  ...remove?
+        `t1`.`data_as_of_dt` AS `data_as_of_dt`,
         `t1`.`time_type` AS `time_type`,
         `t1`.`time_value` AS `time_value`,
-        `t1`.`reference_dt` AS `reference_dt`, -- TODO: for future use  ...remove?
+        `t1`.`reference_dt` AS `reference_dt`,
         `t1`.`value` AS `value`,
         `t1`.`stderr` AS `stderr`,
         `t1`.`sample_size` AS `sample_size`,
         `t1`.`lag` AS `lag`,
         `t1`.`value_updated_timestamp` AS `value_updated_timestamp`,
-        `t1`.`computation_as_of_dt` AS `computation_as_of_dt`, -- TODO: for future use ; also "as_of" is problematic and should be renamed  ...remove?
+        `t1`.`computation_as_of_dt` AS `computation_as_of_dt`,
         `t1`.`missing_value` AS `missing_value`,
         `t1`.`missing_stderr` AS `missing_stderr`,
         `t1`.`missing_sample_size` AS `missing_sample_size`,
         `t1`.`signal_key_id` AS `signal_key_id`,
         `t1`.`geo_key_id` AS `geo_key_id`
-    FROM ((`signal_history` `t1` 
-        JOIN `signal_dim` `t2` 
-            USE INDEX (PRIMARY) 
-            ON `t1`.`signal_key_id` = `t2`.`signal_key_id`)
-        JOIN `geo_dim` `t3` 
-            USE INDEX (PRIMARY) 
-            ON `t1`.`geo_key_id` = `t3`.`geo_key_id`);
+    FROM covid.`signal_history` `t1`
+         JOIN covid.`merged_dim` `t2`
+             USE INDEX (PRIMARY)
+             ON `t1`.`merged_key_id` = `t2`.`merged_key_id`;
 
 
-CREATE OR REPLACE VIEW signal_latest_v AS 
+CREATE OR REPLACE VIEW covid.signal_latest_v AS
     SELECT
-        1 AS is_latest_issue, -- provides column-compatibility to match `covidcast` table
-        NULL AS direction, -- provides column-compatibility to match `covidcast` table
+        1 AS `is_latest_issue`,
+        NULL AS `direction`,
         `t2`.`source` AS `source`,
         `t2`.`signal` AS `signal`,
-        `t3`.`geo_type` AS `geo_type`,
-        `t3`.`geo_value` AS `geo_value`,
+        `t2`.`geo_type` AS `geo_type`,
+        `t2`.`geo_value` AS `geo_value`,
         `t1`.`signal_data_id` AS `signal_data_id`,
         `t1`.`demog_key_id` AS `demog_key_id`, -- TODO: for future use ; also rename s/demog/stratification/  ...remove?
         `t1`.`issue` AS `issue`,
-        `t1`.`data_as_of_dt` AS `data_as_of_dt`, -- TODO: for future use ; also "as_of" is problematic and should be renamed  ...remove?
+        `t1`.`data_as_of_dt` AS `data_as_of_dt`,
         `t1`.`time_type` AS `time_type`,
         `t1`.`time_value` AS `time_value`,
-        `t1`.`reference_dt` AS `reference_dt`, -- TODO: for future use  ...remove?
+        `t1`.`reference_dt` AS `reference_dt`,
         `t1`.`value` AS `value`,
         `t1`.`stderr` AS `stderr`,
         `t1`.`sample_size` AS `sample_size`,
         `t1`.`lag` AS `lag`,
         `t1`.`value_updated_timestamp` AS `value_updated_timestamp`,
-        `t1`.`computation_as_of_dt` AS `computation_as_of_dt`, -- TODO: for future use ; also "as_of" is problematic and should be renamed  ...remove?
+        `t1`.`computation_as_of_dt` AS `computation_as_of_dt`,
         `t1`.`missing_value` AS `missing_value`,
         `t1`.`missing_stderr` AS `missing_stderr`,
         `t1`.`missing_sample_size` AS `missing_sample_size`,
         `t1`.`signal_key_id` AS `signal_key_id`,
         `t1`.`geo_key_id` AS `geo_key_id`
-    FROM ((`signal_latest` `t1` 
-        JOIN `signal_dim` `t2` 
-            USE INDEX (PRIMARY) 
-            ON `t1`.`signal_key_id` = `t2`.`signal_key_id`) 
-        JOIN `geo_dim` `t3` 
-            USE INDEX (PRIMARY) 
-            ON `t1`.`geo_key_id` = `t3`.`geo_key_id`);
+    FROM covid.`signal_latest` `t1`
+        JOIN covid.`merged_dim` `t2`
+            USE INDEX (PRIMARY)
+            ON `t1`.`merged_key_id` = `t2`.`merged_key_id`;
 
 
 CREATE TABLE `covidcast_meta_cache` (
@@ -174,4 +185,6 @@ CREATE TABLE `covidcast_meta_cache` (
 
     PRIMARY KEY (`timestamp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 INSERT INTO covidcast_meta_cache VALUES (0, '[]');
