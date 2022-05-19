d5d876771027320ddfe0d9b9b3440eef8aeed5ff
Kathryn M Mazaitis <krivard@cs.cmu.edu>
2022-05-19 17:40:16 -0400

    passing: i/server/test_covidcast.py

--- integrations/acquisition/covidcast/test_db.py
+++ integrations/acquisition/covidcast/test_db.py
@@ -2,39 +2,13 @@ import unittest
 
 from delphi_utils import Nans
 from delphi.epidata.acquisition.covidcast.database import Database, CovidcastRow
+from delphi.epidata.acquisition.covidcast.test_utils import CovidcastBase
 import delphi.operations.secrets as secrets
 
 # all the Nans we use here are just one value, so this is a shortcut to it:
 nmv = Nans.NOT_MISSING.value
 
-class TestTest(unittest.TestCase):
-
-    def setUp(self):
-        # use the local test instance of the database
-        secrets.db.host = 'delphi_database_epidata'
-        secrets.db.epi = ('user', 'pass')
-
-        self._db = Database()
-        self._db.connect()
-
-        # empty all of the data tables
-        for table in "signal_load  signal_latest  signal_history  geo_dim  signal_dim".split():
-            self._db._cursor.execute(f"TRUNCATE TABLE {table}")
-
-    def tearDown(self):
-        # close and destroy conenction to the database
-        self._db.disconnect(False)
-        del self._db
-
-    def _make_dummy_row(self):
-        return CovidcastRow('src', 'sig', 'day', 'state', 2022_02_22, 'pa', 2, 22, 222, nmv,nmv,nmv, 2022_02_22, 0)
-        #             cols:                               ^ timeval         v  se  ssz               ^issue      ^lag
-
-    def _insert_rows(self, rows):
-        # inserts rows into the database using the full acquisition process, including 'dbjobs' load into history & latest tables
-        self._db.insert_or_update_bulk(rows)
-        self._db.run_dbjobs()
-        ###db._connection.commit()  # NOTE: this isnt needed here, but would be if using external access (like through client lib)
+class TestTest(CovidcastBase):
 
     def _find_matches_for_row(self, row):
         # finds (if existing) row from both history and latest views that matches long-key of provided CovidcastRow
