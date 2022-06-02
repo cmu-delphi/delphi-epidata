b54069927f4a4f43213f3333fcba674dd0fc71a0
george haff <george.haff@gmail.com>
2022-06-02 15:36:53 -0400

    fix to re-do changes the last commit undid from the commit before it...  and also restore a method that got deleted??

--- src/acquisition/covidcast/database.py
+++ src/acquisition/covidcast/database.py
@@ -74,9 +74,11 @@ class Database:
 
   DATABASE_NAME = 'covid'
 
-  latest_table = "signal_latest_v" # technically VIEW and not a TABLE, but...
-  history_table = "signal_history_v" # ...also a VIEW
   load_table = "signal_load"
+  latest_table = "signal_latest" # NOTE: careful!  probably want to use variable `latest_view` instead for semantics purposes
+  latest_view = latest_table + "_v"
+  history_table = "signal_history" # NOTE: careful!  probably want to use variable `history_view` instead for semantics purposes
+  history_view = history_table + "_v"
 
 
   def connect(self, connector_impl=mysql.connector):
@@ -113,7 +115,7 @@ class Database:
     """Return the total number of rows in table `covidcast`."""
 
     if tablename is None:
-      tablename = self.history_table
+      tablename = self.history_view
 
     self._cursor.execute(f'SELECT count(1) FROM `{tablename}`')
 
@@ -121,10 +123,10 @@ class Database:
       return num
 
   def count_all_history_rows(self):
-    return self.count_all_rows(self.history_table)
+    return self.count_all_rows(self.history_view)
 
   def count_all_latest_rows(self):
-    return self.count_all_rows(self.latest_table)
+    return self.count_all_rows(self.latest_view)
 
   def count_insertstatus_rows(self):
     self._cursor.execute(f"SELECT count(1) from `{self.load_table}` where `process_status`='{PROCESS_STATUS.INSERTING}'")
@@ -162,10 +164,10 @@ class Database:
     # if an entry *IS* in both load and latest tables, but latest table issue is newer, unmark is_latest_issue in load.
     fix_is_latest_issue_sql = f'''
         UPDATE 
-            `{self.load_table}` JOIN `{self.latest_table}` 
+            `{self.load_table}` JOIN `{self.latest_view}` 
                 USING (`source`, `signal`, `geo_type`, `geo_value`, `time_type`, `time_value`) 
             SET `{self.load_table}`.`is_latest_issue`=0 
-            WHERE `{self.load_table}`.`issue` < `{self.latest_table}`.`issue` 
+            WHERE `{self.load_table}`.`issue` < `{self.latest_view}`.`issue` 
                   AND `process_status` = '{PROCESS_STATUS.INSERTING}'
     '''
 
@@ -476,13 +478,143 @@ class Database:
 
     return self
 
+  def delete_batch(self, cc_deletions):
+    """
+    Remove rows specified by a csv file or list of tuples.
+
+    If cc_deletions is a filename, the file should include a header row and use the following field order:
+    - geo_id
+    - value (ignored)
+    - stderr (ignored)
+    - sample_size (ignored)
+    - issue (YYYYMMDD format)
+    - time_value (YYYYMMDD format)
+    - geo_type
+    - signal
+    - source
+
+    If cc_deletions is a list of tuples, the tuples should use the following field order (=same as above, plus time_type):
+    - geo_id
+    - value (ignored)
+    - stderr (ignored)
+    - sample_size (ignored)
+    - issue (YYYYMMDD format)
+    - time_value (YYYYMMDD format)
+    - geo_type
+    - signal
+    - source
+    - time_type
+    """
+
+    tmp_table_name = "tmp_delete_table"
+    # composite keys:
+    short_comp_key = "`source`, `signal`, `time_type`, `geo_type`, `time_value`, `geo_value`"
+    long_comp_key = short_comp_key + ", `issue`"
+
+    create_tmp_table_sql = f'''
+CREATE OR REPLACE TABLE {tmp_table_name} LIKE {self.load_table};
+'''
+
+    amend_tmp_table_sql = f'''
+ALTER TABLE {tmp_table_name} ADD COLUMN delete_history_id BIGINT UNSIGNED,
+                             ADD COLUMN delete_latest_id BIGINT UNSIGNED,
+                             ADD COLUMN update_latest BINARY(1) DEFAULT 0;
+'''
+
+    load_tmp_table_infile_sql = f'''
+LOAD DATA INFILE "{cc_deletions}"
+INTO TABLE {tmp_table_name}
+FIELDS TERMINATED BY ","
+IGNORE 1 LINES
+(`geo_value`, `value`, `stderr`, `sample_size`, `issue`, `time_value`, `geo_type`, `signal`, `source`)
+SET time_type="day";
+'''
+
+    load_tmp_table_insert_sql = f'''
+INSERT INTO {tmp_table_name}
+(`geo_value`, `value`, `stderr`, `sample_size`, `issue`, `time_value`, `geo_type`, `signal`, `source`, `time_type`,
+`value_updated_timestamp`, `lag`, `is_latest_issue`)
+VALUES
+(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
+0, 0, 0)
+'''
+
+    add_history_id_sql = f'''
+UPDATE {tmp_table_name} d INNER JOIN {self.history_view} h USING ({long_comp_key})
+SET d.delete_history_id=h.signal_data_id;
+'''
+
+    # if a row we are deleting also appears in the 'latest' table (with a matching 'issue')...
+    mark_for_update_latest_sql = f'''
+UPDATE {tmp_table_name} d INNER JOIN {self.latest_view} ell USING ({long_comp_key})
+SET d.update_latest=1, d.delete_latest_id=ell.signal_data_id;
+'''
+
+    delete_history_sql = f'''
+DELETE h FROM {tmp_table_name} d INNER JOIN {self.history_table} h ON d.delete_history_id=h.signal_data_id;
+'''
+
+    # ...remove it from 'latest'...
+    delete_latest_sql = f'''
+DELETE ell FROM {tmp_table_name} d INNER JOIN {self.latest_table} ell ON d.delete_latest_id=ell.signal_data_id;
+'''
+
+    # ...and re-write that record with its next-latest issue (from 'history') instead.
+    # NOTE: this must be executed *AFTER* `delete_history_sql` to ensure we get the correct `issue`
+    #       AND also after `delete_latest_sql` so that we dont get a key collision on insert.
+    update_latest_sql = f'''
+INSERT INTO signal_latest
+  (issue,
+  signal_data_id, signal_key_id, geo_key_id, time_type, time_value,
+  value, stderr, sample_size, `lag`, value_updated_timestamp,
+  missing_value, missing_stderr, missing_sample_size)
+SELECT
+  MAX(h.issue),
+  h.signal_data_id, h.signal_key_id, h.geo_key_id, h.time_type, h.time_value,
+  h.value, h.stderr, h.sample_size, h.`lag`, h.value_updated_timestamp,
+  h.missing_value, h.missing_stderr, h.missing_sample_size
+FROM {self.history_view} h JOIN {tmp_table_name} d USING ({short_comp_key})
+WHERE d.update_latest=1 GROUP BY {short_comp_key};
+'''
+
+    drop_tmp_table_sql = f'DROP TABLE {tmp_table_name}'
+
+    total = None
+    try:
+      self._cursor.execute(create_tmp_table_sql)
+      self._cursor.execute(amend_tmp_table_sql)
+      if isinstance(cc_deletions, str):
+        self._cursor.execute(load_tmp_table_infile_sql)
+      elif isinstance(cc_deletions, list):
+        self._cursor.executemany(load_tmp_table_insert_sql, cc_deletions)
+      else:
+        raise Exception(f"Bad deletions argument: need a filename or a list of tuples; got a {type(cc_deletions)}")
+      self._cursor.execute(add_history_id_sql)
+      self._cursor.execute(mark_for_update_latest_sql)
+      self._cursor.execute(delete_history_sql)
+      total = self._cursor.rowcount
+      # TODO: consider reporting rows removed and/or replaced in latest table as well
+      self._cursor.execute(delete_latest_sql)
+      self._cursor.execute(update_latest_sql)
+      self._connection.commit()
+
+      if total == -1:
+        # the SQL connector does not support returning number of rows affected (see PEP 249)
+        total = None
+    except Exception as e:
+      raise e
+    finally:
+      self._cursor.execute(drop_tmp_table_sql)
+    return total
+
+
 
   def compute_covidcast_meta(self, table_name=None):
     """Compute and return metadata on all COVIDcast signals."""
     logger = get_structured_logger("compute_covidcast_meta")
 
     if table_name is None:
-      table_name = self.latest_table
+      table_name = self.latest_view
 
     n_threads = max(1, cpu_count()*9//10) # aka number of concurrent db connections, which [sh|c]ould be ~<= 90% of the #cores available to SQL server
     # NOTE: this may present a small problem if this job runs on different hardware than the db,
