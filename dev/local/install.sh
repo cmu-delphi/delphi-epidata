184a385ac8deeb6f8488025130761077a05f0e9b
Dmitry Shemetov <dshemetov@ucdavis.edu>
2022-06-17 18:01:13 -0700

    Add Docker controller Makefile and boostrap script * combine with Katie and Xavier's work in epidata-refresh.sh * simplify directory structure using .dockerignore * add in missing dependencies to install.sh * add in changes reviews from Xavier's review

--- dev/local/install.sh
+++ dev/local/install.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
+# Bootstrap delphi-epidata
+#
+# Downloads the repos needed for local delphi-epidata development into current dir 
+# and provides a Makefile with Docker control commands.
 
-mkdir -p driver/repos/delphi driver-logs/delphi_database_epidata driver-logs/delphi_web_epidata
-cd driver/repos/delphi
+mkdir -p repos/delphi
+cd repos/delphi
 git clone https://github.com/cmu-delphi/operations
 git clone https://github.com/cmu-delphi/delphi-epidata
 git clone https://github.com/cmu-delphi/utils
+git clone https://github.com/cmu-delphi/flu-contest
+git clone https://github.com/cmu-delphi/nowcast
+git clone https://github.com/cmu-delphi/github-deploy-repo
 cd ../../
-ln -s repos/delphi/delphi-epidata/dev/local/epidata-refresh.sh
+
+mkdir -p repos/undefx
+cd repos/undefx
+git clone https://github.com/undefx/py3tester
+git clone https://github.com/undefx/undef-analysis
+cd ../../
+
+mkdir -p docker-logs
+
+ln -s repos/delphi/delphi-epidata/dev/local/Makefile
+ln -s repos/delphi/delphi-epidata/dev/local/.dockerignore
