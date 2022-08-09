c12031623b269ed129d19050153deaf52af0380c
Xavier Xia <boxia@andrew.cmu.edu>
2022-08-09 10:14:55 -0400

    refined comments and added tests for geo_type, geo_value pairs

--- integrations/acquisition/covidcast/test_dim_tables.py
+++ integrations/acquisition/covidcast/test_dim_tables.py
@@ -61,12 +61,12 @@ class CovidcastDimensionTablesTests(unittest.TestCase):
         self._db._connection.close()
 
     # We want to test src_sig to make sure rows are added to *_dim only when needed
-    #new src, sig (ensure that it is added into *_dim)
-    #old src, sig (ensure that it is NOT added into *_dim)
-    #new geo (added)
-    #old geo (not added)
+        #new src, sig (ensure that it is added into *_dim)
+        #old src, sig (ensure that it is NOT added into *_dim)
+        #new geo (added)
+        #old geo (not added)
     def test_src_sig(self):
-    #BASE CASES
+        #BASE CASES
         rows = [
         CovidcastRow('src', 'sig', 'day', 'state', 20200414, 'pa', 
         2, 2, 2, nmv, nmv, nmv, 20200414, 0),
@@ -87,12 +87,12 @@ class CovidcastDimensionTablesTests(unittest.TestCase):
         record = self._db._cursor.fetchall()
         sigLatestRowCount = len(list(record))
 
-        #test not added first
+        #test same src, sig not added
         with self.subTest(name='older src and sig not added into sig_dim'):
             oldSrcSig = [
-                CovidcastRow('src', 'sig', 'day', 'state', 20211111, 'pa', #new src, new sig
+                CovidcastRow('src', 'sig', 'day', 'state', 20211111, 'pa', #same src, sig but diff timevalue and issue
                             99, 99, 99, nmv, nmv, nmv, 20211111, 1),
-                CovidcastRow('src', 'sig', 'day', 'county', 20211111, '11111', #new src, new sig, same geo
+                CovidcastRow('src', 'sig', 'day', 'county', 20211111, '11111', #same src, sig but diff timevalue and issue
                             99, 99, 99, nmv, nmv, nmv, 20211111, 1)
                 ]  
             self._db.insert_or_update_batch(oldSrcSig)
@@ -107,14 +107,15 @@ class CovidcastDimensionTablesTests(unittest.TestCase):
             #ensure new entries are added in latest
             self._db._cursor.execute(self.viewSignalLatest)
             record = self._db._cursor.fetchall()
-            sigLatestRowCount = len(list(record))
-            self.assertEqual(len(list(record)), sigLatestRowCount) #2 original, 2 added (updated)
+            sigLatestRowCount = len(list(record)) #updating sigLatestRowCount
+            self.assertEqual(4 , sigLatestRowCount) #diff timevalue and issue, 2 older + 2 newer = 4
             
-            #ensure nothing in geo
+            #ensure nothing changed in geoDim
             self._db._cursor.execute(self.viewGeoDim)
             record = self._db._cursor.fetchall()
-            self.assertEqual(len(list(record)),geoDimRowCount)
+            self.assertEqual(len(list(record)),geoDimRowCount) #geoDimRowCount remains unchanged 
 
+        #testing new src, sig added 
         with self.subTest(name='newer src and sig added in sig_dim'):
             newSrcSig = [
                 CovidcastRow('new_src', 'sig', 'day', 'state', 20200414, 'pa', # new_src
@@ -128,33 +129,42 @@ class CovidcastDimensionTablesTests(unittest.TestCase):
             #testing src, sig
             self._db._cursor.execute(self.viewSignalDim)
             record = self._db._cursor.fetchall()
-            sigDimRowCount = len(list(record))
-            res = set([('new_src', 'sig'), ('src', 'new_sig'), ('src', 'sig')]) # the sequence of adding changed
-            self.assertEqual(res , set(record))
-            self.assertEqual(3, sigDimRowCount)
-            #ensure nothing in geo
+            sigDimRowCount = len(list(record)) #update sigDimRowCount
+            res = set([('new_src', 'sig'), ('src', 'new_sig'), ('src', 'sig')]) # turn into set to ignore ordering
+            self.assertEqual(res , set(record)) 
+            self.assertEqual(3, sigDimRowCount) #originally had (src , sig) added 2 new pairs
+
+            #ensure new entries are added in latest
+            self._db._cursor.execute(self.viewSignalLatest)
+            record = self._db._cursor.fetchall()
+            sigLatestRowCount = len(list(record)) #updating sigLatestRowCount
+            self.assertEqual(6 , sigLatestRowCount) #diff timevalue and issue, 2 more added ontop of 4 previously
+
+            #ensure nothing changed in geoDim
             self._db._cursor.execute(self.viewGeoDim)
             record = self._db._cursor.fetchall()
             self.assertEqual(len(list(record)),geoDimRowCount)
 
+        #testing repeated geo not added
         with self.subTest(name='old geo not added in geo_dim'): 
-            repeatedGeoValues = [                   #geo_type          #geo_value
-                CovidcastRow('src', 'sig', 'day', 'state', 20200415, 'pa', # same geo_type, geo_value
+            repeatedGeoValues = [                   
+                CovidcastRow('src', 'sig', 'day', 'state', 20200415, 'pa', # same geo_type, geo_value ('pa')
                         2, 2, 2, nmv, nmv, nmv, 20200415, 0),
-                CovidcastRow('src', 'sig', 'day', 'county', 20200415, '11111', # same geo_type, geo_value
+                CovidcastRow('src', 'sig', 'day', 'county', 20200415, '11111', # same geo_type, geo_value ('11111')
                         3, 3, 3, nmv, nmv, nmv, 20200415, 0),
                 ]  
             self._db.insert_or_update_batch(repeatedGeoValues)
             self._db.run_dbjobs()
 
-            self._db._cursor.execute(self.viewGeoDim)
-            record = self._db._cursor.fetchall()
-            self.assertEqual(len(list(record)),geoDimRowCount) #geoDimRowCount unchanged
-
             self._db._cursor.execute(self.viewSignalLatest)
             record = self._db._cursor.fetchall()
             sigLatestRowCount = len(list(record)) #update
-            self.assertEqual(8,sigLatestRowCount) #total entries = 2(initial) + 6(test)
+            self.assertEqual(8, sigLatestRowCount) #total entries = 2 + 6 in previous subtest
+
+            #ensure nothing changed in geoDim with repeated geo_type, geo_value pairs
+            self._db._cursor.execute(self.viewGeoDim)
+            record = self._db._cursor.fetchall()
+            self.assertEqual(len(list(record)),geoDimRowCount) #geoDimRowCount unchanged
 
         with self.subTest(name='newer geo added in geo_dim'): 
             newGeoValues = [                   #geo_type          #geo_value
@@ -170,11 +180,13 @@ class CovidcastDimensionTablesTests(unittest.TestCase):
 
             self._db._cursor.execute(f'SELECT `geo_type`,`geo_value` FROM `geo_dim`')
             record = self._db._cursor.fetchall()
-            geoDimRowCount = len(list(record))
-            self.assertEqual(5,geoDimRowCount) #2 + 3 new
+            res = set([('state', 'nj'), ('county', '15217'), ('county', '15451'), ('state', 'pa'), ('county', '11111')]) # turn into set to ignore ordering
+            self.assertEqual(res , set(record)) #ensure the values are the same, 3 new ones and 2 older ones
+            geoDimRowCount = len(list(record)) 
+            self.assertEqual(5,geoDimRowCount) #2 + 3 new pairs
 
             self._db._cursor.execute(self.viewSignalLatest)
             record = self._db._cursor.fetchall()
-            sigLatestRowCount = len(list(record)) #update
-            self.assertEqual(11,sigLatestRowCount) #total entries = 2(initial) + 6(test) + 3
+            sigLatestRowCount = len(list(record)) #update sigLatestRowCount
+            self.assertEqual(11,sigLatestRowCount) #total entries = 8 previously + 3 new ones
         
