2dc967f8ce4c5edc2295461b346bb7248d4285d7
Dmitry Shemetov <dshemetov@ucdavis.edu>
2022-08-23 14:28:30 -0700

    JIT tests cleanup: * Revert the spacing in test_covidcast_meta_cache_updater for cleaner diffs

--- tests/acquisition/covidcast/test_covidcast_meta_cache_updater.py
+++ tests/acquisition/covidcast/test_covidcast_meta_cache_updater.py
@@ -12,52 +12,52 @@ from delphi.epidata.acquisition.covidcast.covidcast_meta_cache_updater import ge
 
 
 class UnitTests(unittest.TestCase):
-    """Basic unit tests."""
+	"""Basic unit tests."""
 
-    def test_get_argument_parser(self):
-        """Return a parser for command-line arguments."""
-        self.assertIsInstance(get_argument_parser(), argparse.ArgumentParser)
+	def test_get_argument_parser(self):
+		"""Return a parser for command-line arguments."""
+		self.assertIsInstance(get_argument_parser(), argparse.ArgumentParser)
 
-    def test_main_successful(self):
-        """Run the main program successfully."""
+	def test_main_successful(self):
+		"""Run the main program successfully."""
 
-        api_response = {
-            "result": 1,
-            "message": "yes",
-            "epidata": [{"foo": "bar"}],
-        }
+		api_response = {
+			"result": 1,
+			"message": "yes",
+			"epidata": [{"foo": "bar"}],
+		}
 
-        args = MagicMock(log_file="log")
-        mock_epidata_impl = MagicMock()
-        mock_epidata_impl.covidcast_meta.return_value = api_response
-        mock_database = MagicMock()
-        mock_database.compute_covidcast_meta.return_value = api_response["epidata"]
-        fake_database_impl = lambda: mock_database
+		args = MagicMock(log_file="log")
+		mock_epidata_impl = MagicMock()
+		mock_epidata_impl.covidcast_meta.return_value = api_response
+		mock_database = MagicMock()
+		mock_database.compute_covidcast_meta.return_value = api_response["epidata"]
+		fake_database_impl = lambda: mock_database
 
-        main(args, epidata_impl=mock_epidata_impl, database_impl=fake_database_impl)
+		main(args, epidata_impl=mock_epidata_impl, database_impl=fake_database_impl)
 
-        self.assertTrue(mock_database.connect.called)
+		self.assertTrue(mock_database.connect.called)
 
-        self.assertTrue(mock_database.update_covidcast_meta_cache.called)
-        actual_args = mock_database.update_covidcast_meta_cache.call_args[0]
-        expected_args = (api_response["epidata"],)
-        self.assertEqual(actual_args, expected_args)
+		self.assertTrue(mock_database.update_covidcast_meta_cache.called)
+		actual_args = mock_database.update_covidcast_meta_cache.call_args[0]
+		expected_args = (api_response["epidata"],)
+		self.assertEqual(actual_args, expected_args)
 
-        self.assertTrue(mock_database.disconnect.called)
-        self.assertTrue(mock_database.disconnect.call_args[0][0])
+		self.assertTrue(mock_database.disconnect.called)
+		self.assertTrue(mock_database.disconnect.call_args[0][0])
 
-    def test_main_failure(self):
-        """Run the main program with a query failure."""
-        api_response = {
-            "result": -123,
-            "message": "no",
-        }
+	def test_main_failure(self):
+		"""Run the main program with a query failure."""
+		api_response = {
+			"result": -123,
+			"message": "no",
+		}
 
-        args = MagicMock(log_file="log")
-        mock_database = MagicMock()
-        mock_database.compute_covidcast_meta.return_value = list()
-        fake_database_impl = lambda: mock_database
+		args = MagicMock(log_file="log")
+		mock_database = MagicMock()
+		mock_database.compute_covidcast_meta.return_value = list()
+		fake_database_impl = lambda: mock_database
 
-        main(args, epidata_impl=None, database_impl=fake_database_impl)
+		main(args, epidata_impl=None, database_impl=fake_database_impl)
 
-        self.assertTrue(mock_database.compute_covidcast_meta.called)
+		self.assertTrue(mock_database.compute_covidcast_meta.called)
