8bb278995ea4c90104613d7b327a72ebe4463a59
Dmytro Trotsko <dmytrotsko@gmail.com>
2022-10-18 10:46:53 +0300

    add record flag to user

--- src/server/admin/__init__.py
+++ src/server/admin/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from os import environ
-from typing import List, Set
+from typing import Dict, List, Set
 from flask import Blueprint, render_template_string, request
 from werkzeug.exceptions import Unauthorized, NotFound
 from werkzeug.utils import redirect
@@ -24,20 +24,22 @@ def _require_admin():
 def _parse_roles(roles: List[str]) -> Set[str]:
     return set(sorted(roles))
 
-def _render(mode: str, token: str, **kwargs):
+def _render(mode: str, token: str, flags: Dict, **kwargs):
     template = (templates_dir / 'index.html').read_text('utf8')
-    return render_template_string(template, mode=mode, token=token, roles=[r.value for r in UserRole], **kwargs)
+    return render_template_string(template, mode=mode, token=token, flags=flags, roles=[r.value for r in UserRole], **kwargs)
 
 
 @bp.route('/', methods=['GET', 'POST'])
 def _index():
     token = _require_admin()
+    flags = dict()
     if request.method == 'POST':
         # register a new user
-        DBUser.insert(request.values['email'], request.values['api_key'], _parse_roles(request.values.getlist('roles')))
+        DBUser.insert(request.values['email'], request.values['api_key'], _parse_roles(request.values.getlist('roles')), request.values.get('record') == 'True')
+        flags['banner'] = 'Successfully Added'
 
     users = DBUser.list()
-    return _render('overview', token, users=users, user=dict())
+    return _render('overview', token, flags, users=users, user=dict())
 
 
 @bp.route('/<int:user_id>', methods=['GET', 'PUT', 'POST', 'DELETE'])
@@ -49,6 +51,8 @@ def _detail(user_id: int):
     if request.method == 'DELETE' or 'delete' in request.values:
         user.delete()
         return redirect(f"./?auth={token}")
+    flags = dict()
     if request.method == 'PUT' or request.method == 'POST':
-        user = user.update(request.values['email'], request.values['api_key'], _parse_roles(request.values.getlist('roles')))
-    return _render('detail', token, user=user)
+        user = user.update(request.values['email'], request.values['api_key'], _parse_roles(request.values.getlist('roles')), request.values.get('record') == 'True')
+        flags['banner'] = 'Successfully Saved'
+    return _render('detail', token, flags, user=user)
