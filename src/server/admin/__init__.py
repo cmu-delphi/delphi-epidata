d066a90c702abdb3c001492d1e6e9296baee0362
Dmytro Trotsko <dmytrotsko@gmail.com>
2022-10-18 10:34:04 +0300

    list all known roles

--- src/server/admin/__init__.py
+++ src/server/admin/__init__.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 from os import environ
-from typing import Set
+from typing import List, Set
 from flask import Blueprint, render_template_string, request
 from werkzeug.exceptions import Unauthorized, NotFound
 from werkzeug.utils import redirect
-from .._security import resolve_auth_token, DBUser
+from .._security import resolve_auth_token, DBUser, UserRole
 
 
 ADMIN_PASSWORD = environ.get('API_KEY_ADMIN_PASSWORD')
@@ -21,17 +21,12 @@ def _require_admin():
         raise Unauthorized()
     return token
 
-def _parse_roles(roles: str) -> Set[str]:
-    parsed: Set[str] = set()
-    for role in roles.split('\n'):
-        role = role.strip()
-        if role:
-            parsed.add(role)
-    return parsed
+def _parse_roles(roles: List[str]) -> Set[str]:
+    return set(sorted(roles))
 
 def _render(mode: str, token: str, **kwargs):
     template = (templates_dir / 'index.html').read_text('utf8')
-    return render_template_string(template, mode=mode, token=token, **kwargs)
+    return render_template_string(template, mode=mode, token=token, roles=[r.value for r in UserRole], **kwargs)
 
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -39,22 +34,22 @@ def _index():
     token = _require_admin()
     if request.method == 'POST':
         # register a new user
-        DBUser.insert(request.values['email'], request.values['api_key'], _parse_roles(request.values['roles']))
+        DBUser.insert(request.values['email'], request.values['api_key'], _parse_roles(request.values.getlist('roles')))
 
     users = DBUser.list()
-    return _render('overview', token, users=users)
+    return _render('overview', token, users=users, user=dict())
 
 
 @bp.route('/<int:user_id>', methods=['GET', 'PUT', 'POST', 'DELETE'])
 def _detail(user_id: int):
     token = _require_admin()
     user = DBUser.find(user_id)
-    print(request.values.to_dict(), flush=True)
+    print(request.values.to_dict(False), flush=True)
     if not user:
         raise NotFound()
     if request.method == 'DELETE' or 'delete' in request.values:
         user.delete()
         return redirect(f"./?auth={token}")
     if request.method == 'PUT' or request.method == 'POST':
-        user = user.update(request.values['email'], request.values['api_key'], _parse_roles(request.values['roles']))
+        user = user.update(request.values['email'], request.values['api_key'], _parse_roles(request.values.getlist('roles')))
     return _render('detail', token, user=user)
