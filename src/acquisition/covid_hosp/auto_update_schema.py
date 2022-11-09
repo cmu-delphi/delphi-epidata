e2f44eb99b8daef6da891ec12b9f040db90d8009
Kathryn M Mazaitis <krivard@cs.cmu.edu>
2022-11-09 11:32:36 -0500

    [wip] infer type, extend columns from json

--- src/acquisition/covid_hosp/auto_update_schema.py
+++ src/acquisition/covid_hosp/auto_update_schema.py
@@ -4,6 +4,7 @@ import csv
 from dataclasses import dataclass
 import importlib.util
 import sys
+import re
 
 import itertools
 from collections import defaultdict
@@ -46,13 +47,14 @@ def split_sql(sql):
 
 def annotate_columns_using_ddl(all_columns, sql):
     """Parse a .sql DDL file string and annotate a set of Column representations with SQL type and index information."""
-    result = defaultdict(list)
+    result = defaultdict(dict)
     for stmt in split_sql(sql):
         if stmt[:15].lower().startswith("create table"):
             ast = parse_mysql(sql)['create table']
             if ast['name'] not in all_columns:
                 continue
-            for pydb_columns in all_columns[ast['name']]:
+            for pydb_module in all_columns[ast['name']]:
+                pydb_columns = all_columns[ast['name']][pydb_module]
                 remapped = {
                     ci.sql_name: ci
                     for ci in pydb_columns.values()
@@ -73,8 +75,112 @@ def annotate_columns_using_ddl(all_columns, sql):
                             mks = ci['index']['columns']
                             for mki in mks:
                                 remapped[mki].sql_key = "mul"
-                result[ast['name']].append(remapped)
+                result[ast['name']][pydb_module] = remapped
     return result
+
+def url_as_json_data(json_url):
+    resp = requests.get(json_url)
+    resp.raise_for_status()
+    return resp.json()['columns']
+
+KEYS="name fieldName position description dataTypeName cachedContents".split()
+TABLE_HEADERS = "Field Type Null Key Default Extra".split()
+HEADER = {
+    "publication_date",
+    "address",
+    "ccn",
+    "city",
+    "collection_week",
+    "fips_code",
+    "geocoded_hospital_address",
+    "hhs_ids",
+    "hospital_name",
+    "hospital_pk",
+    "hospital_subtype",
+    "is_metro_micro",
+    "state",
+    "zip",
+    "issue",
+    "date"
+}
+REQUIRED = {
+    "publication_date",
+    "collection_week",
+    "hospital_pk",
+    "state",
+    "issue",
+    "date"
+}
+RE_NUMBERED_PREFIX = re.compile(r'^[0-9]*\.')
+#RE_THIRD=re.compile(",[^,]*,$")
+#RE_NAME=re.compile("`[^`]*`")
+#RE_REQUIRED=re.compile(" NOT NULL")
+#RE_TYPE=re.compile("^([A-Z]*)(\([0-9]*\))*")
+
+def infer_type(col):
+    sqltys = -1
+    if col['dataTypeName'] == 'text':
+        if col['cachedContents']['cardinality'] == "2":
+            pyty = "Utils.parse_bool"
+            sqlty = "BOOLEAN"
+        else:
+            pyty = "str"
+            max_len = len(col['cachedContents']['largest'])
+            min_len = len(col['cachedContents']['smallest'])
+            if max_len - min_len == 0:
+                sqlty = "CHAR"
+                sqltys = max_len
+            else:
+                sqlty = "VARCHAR"
+                sqltys = max_len+5
+    if col['dataTypeName'] == 'calendar_date':
+        pyty = "Utils.int_from_date"
+        sqlty = "INT"
+        #sqltys = 11
+    if col['dataTypeName'] == 'number':
+        if col['cachedContents']['largest'].find('.') < 0:
+            pyty = "int"
+            sqlty = "INT"
+        else:
+            pyty = "float"
+            sqlty = "DOUBLE"
+    if col['dataTypeName'] == 'point':
+        pyty = "str"
+        sqlty = "VARCHAR"
+        sqltys = 32
+    if col['dataTypeName'] == 'checkbox':
+        pyty = "Utils.parse_bool"
+        sqlty = "BOOLEAN"
+    ret = {
+        "py_type": pyty,
+        "sql_type": sqlty
+    }
+    if sqltys > 0:
+        ret["sql_type_size"] = sqltys
+    return ret
+
+
+def extend_columns_with_current_json(columns, json_data):
+    hits = []
+    misses = []
+    for json_column in json_data:
+        if 'computationStrategy' in json_column:
+            next
+        if not all(key in json_column for key in KEYS):
+            print(f"Bad column: {json.dumps(json_column, indent=2)}")
+            next
+        if json_column['name'] in columns:
+            hits.append(json_column['name'])
+        else:
+            misses.append(Column(
+                csv_name=json_column['name'],
+                csv_order=json_column['position'],
+                desc=re.sub(RE_NUMBERED_PREFIX, "", json_column['description']).strip(),
+                sql_name=json_column['fieldName'].lower(),
+                **infer_type(json_column)
+            ))
+    return hits, misses
+
 
 @dataclass
 class Column:
@@ -130,16 +236,22 @@ class Column:
             self.sql_extra.upper()
         ]
 
-#    state: "https://healthdata.gov/api/views/g62h-syeh.json"
-# facility: "https://healthdata.gov/api/views/anag-cw7u.json"
+
+#    state ts: "https://healthdata.gov/api/views/g62h-syeh.json"
+# state daily: "https://healthdata.gov/api/views/6xf2-c3ie.json"
+#    facility: "https://healthdata.gov/api/views/anag-cw7u.json"
 
 def main():
     all_columns = {
-        "covid_hosp_state_timeseries": [
-            db_to_columns(STDatabase),
-            db_to_columns(SDDatabase)
-        ],
-        "covid_hosp_facility": [
-            db_to_columns(FDatabase)
-        ]
+        "covid_hosp_state_timeseries": {
+            "state_timeseries": db_to_columns(STDatabase),
+            "state_daily": db_to_columns(SDDatabase)
+        },
+        "covid_hosp_facility": {
+            "facility": db_to_columns(FDatabase)
+        }
     }
+    with open("src/ddl/covid_hosp.sql") as f:
+        ddl = f.read()
+    annotate_columns_using_ddl(all_columns, ddl)
+    
--- tests/acquisition/covid_hosp/test_auto_update_schema.py
+++ tests/acquisition/covid_hosp/test_auto_update_schema.py
@@ -1,14 +1,21 @@
+from collections import namedtuple
+from dataclasses import dataclass
 from delphi.epidata.acquisition.covid_hosp.auto_update_schema import \
     split_sql, \
     db_to_columns, \
     annotate_columns_using_ddl, \
+    extend_columns_with_current_json, \
+    infer_type, \
     Column
 from delphi.epidata.acquisition.covid_hosp.common.utils import Utils
 import pytest
 
+Case = namedtuple("testcase","given expected")
+
 @pytest.fixture
-def split_sql_given():
-    return """
+def split_sql_example():
+    return Case(
+        """
 USE covidcast;
 
 /*
@@ -30,37 +37,32 @@ CREATE TABLE `maze` (
 `direction` CHAR(2) NOT NULL,
 -- test comment
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-"""
-
-@pytest.fixture
-def split_sql_expected():
-    # x[1:] to drop the initial newline (included for readability)
-    return list(x[1:] for x in [
-        """
+""",
+        # x[1:] to drop the initial newline (included for readability)
+        list(x[1:] for x in [
+            """
 USE covidcast""",
-        """
+            """
 xyzzy""",
-        """
+            """
 CREATE TABLE `kitchen` (
 `id` INT NOT NULL AUTO_INCREMENT,
 sink VARCHAR(8)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8""",
-        """
+            """
 quux
 | bar |""",
-        """
+            """
 CREATE TABLE `maze` (
 `turn` INT NOT NULL AUTO_INCREMENT,
 `direction` CHAR(2) NOT NULL,
 -- test comment
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8"""
-    ])
+        ]))
 
 
-def test_split_sql(split_sql_given, split_sql_expected):
-    assert split_sql(split_sql_given) == split_sql_expected
-
-
+def test_split_sql(split_sql_example):
+    assert split_sql(split_sql_example.given) == split_sql_example.expected
 
 @pytest.fixture
 def columns_as_Database():
@@ -132,11 +134,11 @@ def annotated_columns(columns_as_Columns):
 
 def test_annotate_columns_using_ddl(columns_as_Columns, columns_as_SQL, annotated_columns):
     remapped = annotate_columns_using_ddl(
-        {'table1': [columns_as_Columns]},
+        {'table1': {'module1': columns_as_Columns}},
         columns_as_SQL
     )
     assert 'id' not in annotated_columns
-    assert 'id' in remapped['table1'][0]
+    assert 'id' in remapped['table1']['module1']
     for ki in annotated_columns:
         _match_fields(
             columns_as_Columns[ki],
@@ -144,3 +146,182 @@ def test_annotate_columns_using_ddl(columns_as_Columns, columns_as_SQL, annotate
             *"sql_type sql_type_size required".split()
         )
 
+@pytest.fixture(
+    params=["{i}. Description of {name}", "Description of {name}"],
+    ids=["numbered descriptions", "non-numbered descriptions"]
+)
+def old_columns_as_JSON(request):
+    columns = [
+        ("int", "number"),
+        ("str", "text"),
+        ("float", "number"),
+        ("date", "calendar_date"),
+    ]
+    return [
+        {
+            "id": 100 + i,
+            "name": f"{name}_csv",
+            "dataTypeName": dataTypeName,
+            "description": request.param.format(i=i, name=name),
+            "fieldName": f"{name}_csv",
+            "position": i,
+            "cachedContents": [],
+        }
+        for i, (name, dataTypeName) in enumerate(columns)
+    ]
+
+
+@dataclass
+class Case_new_column:
+    name: str
+    given: dict
+    expected_type: dict
+    expected_column: Column=None
+
+json_columns = [
+    Case_new_column(
+        "text_bool", # literal "true" / "false" strings
+        {
+            "dataTypeName": "text",
+            "cachedContents": {
+                "cardinality": "2",
+                
+            }
+        },
+        {
+            "py_type": "Utils.parse_bool",
+            "sql_type": "BOOLEAN"
+        }
+    ),
+    Case_new_column(
+        "text_same_length",
+        {
+            "dataTypeName": "text",
+            "cachedContents": {
+                "cardinality": 0,
+                "largest": "x",
+                "smallest": "x"
+            }
+        },
+        {
+            "py_type": "str",
+            "sql_type": "CHAR",
+            "sql_type_size": 1
+        }
+    ),
+    Case_new_column(
+        "text_different_lengths",
+        {
+            "dataTypeName": "text",
+            "cachedContents": {
+                "cardinality": 0,
+                "largest": "xx",
+                "smallest": "x",
+            }
+        },
+        {
+            "py_type": "str",
+            "sql_type": "VARCHAR",
+            "sql_type_size": 7
+        }
+    ),
+    Case_new_column(
+        "dates",
+        {
+            "dataTypeName": "calendar_date",
+        },
+        {
+            "py_type": "Utils.int_from_date",
+            "sql_type": "INT",
+        }
+    ),
+    Case_new_column(
+        "ints", # number: integer
+        {
+            "dataTypeName": "number",
+            "cachedContents": {
+                "largest": "0"
+            }
+        },
+        {
+            "py_type": "int",
+            "sql_type": "INT",
+        }
+    ),
+    Case_new_column(
+        "floats", # number: float
+        {
+            "dataTypeName": "number",
+            "cachedContents": {
+                "largest": "0.0"
+            }
+        },
+        {
+            "py_type": "float",
+                "sql_type": "DOUBLE",
+        }
+    ),
+    Case_new_column(
+        "points", # lat/long point
+        {
+            "dataTypeName": "point",
+        },
+        {
+            "py_type": "str",
+            "sql_type": "VARCHAR",
+            "sql_type_size": 32
+        }
+    ),
+    Case_new_column(
+        "checkboxes",
+        {
+            "dataTypeName": "checkbox",
+        },
+        {
+            "py_type": "Utils.parse_bool",
+            "sql_type": "BOOLEAN",
+        }
+    )
+]
+
+@pytest.fixture(
+    params=json_columns,
+    ids=[j.name for j in json_columns]
+)
+def new_columns(request):
+    return Case_new_column(
+        request.param.name,
+        {
+            "id": 500,
+            "name": f"xyzzy_csv",
+            "dataTypeName": request.param.given["dataTypeName"],
+            "description": "Description of xyzzy",
+            "fieldName": f"xyzzy_csv",
+            "position": 10,
+            "cachedContents": request.param.given.get("cachedContents", {}),
+        },
+        request.param.expected_type,
+        Column(
+            csv_name="xyzzy_csv",
+            csv_order=10,
+            desc="Description of xyzzy",
+            sql_name="xyzzy_csv",
+            **request.param.expected_type
+        ),
+    )
+
+def test_infer_type(new_columns):
+    assert infer_type(new_columns.given) == new_columns.expected_type    
+
+def test_extend_columns_with_current_json(columns_as_Columns, old_columns_as_JSON, new_columns):
+    hits, misses = extend_columns_with_current_json(
+        columns_as_Columns,
+        old_columns_as_JSON + [new_columns.given]
+    )
+    assert hits == [
+        "int_csv",
+        "str_csv",
+        "float_csv",
+        "date_csv"
+    ]
+    assert misses == [new_columns.expected_column]
