32e1a42aa7bad20fb8761ab99c0577b25d2195ae
george haff <george.haff@gmail.com>
2022-11-09 13:29:20 -0500

    added copy of new table definition to migration file

--- src/ddl/migrations/covid_hosp_facility_v0.3-v0.3.1.sql
+++ src/ddl/migrations/covid_hosp_facility_v0.3-v0.3.1.sql
@@ -1,3 +1,28 @@
+-- table definition copied from ../covid_hosp.sql
+CREATE TABLE `covid_hosp_facility_key` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `address` VARCHAR(128),
+  `ccn` VARCHAR(20),
+  `city` VARCHAR(64),
+  `fips_code` CHAR(5),
+  `geocoded_hospital_address` VARCHAR(32), --  <--- not currently exposed by endpoint
+  `hhs_ids` VARCHAR(127), --  <--- not currently exposed by endpoint
+  `hospital_name` VARCHAR(256),
+  `hospital_pk` VARCHAR(128) NOT NULL,
+  `hospital_subtype` VARCHAR(64),
+  `is_metro_micro` BOOLEAN,
+  `state` CHAR(2) NOT NULL,
+  `zip` CHAR(5),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`hospital_pk`),
+  -- for fast lookup of hospitals in a given location
+  KEY (`state`, `hospital_pk`),
+  KEY (`ccn`, `hospital_pk`),
+  KEY (`city`, `hospital_pk`),
+  KEY (`zip`, `hospital_pk`),
+  KEY (`fips_code`, `hospital_pk`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 INSERT INTO `covid_hosp_facility_key`
   (`address`,`ccn`,`city`,`fips_code`,`geocoded_hospital_address`,`hhs_ids`,`hospital_name`,`hospital_pk`,`hospital_subtype`,`is_metro_micro`,`state`,`zip`)
 SELECT
