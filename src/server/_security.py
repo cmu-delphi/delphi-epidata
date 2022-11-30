8fbae7d22baca68faba6e1d2e912f22cac8bb0e1
Dmytro Trotsko <dmytrotsko@gmail.com>
2022-11-30 16:31:41 +0200

    Removed user_role FK in api_user table. Consolidated DBUser and APIUser into 1 class -> APIUser.

--- src/server/_security.py
+++ src/server/_security.py
@@ -1,251 +1,159 @@
-from typing import Set, Optional, List, Dict, cast
-from datetime import datetime, timedelta, date
-from uuid import uuid4
 import re
+from datetime import timedelta, date
 from functools import wraps
-
-from sqlalchemy import ForeignKey, Column, Integer, String, Boolean, DateTime, delete, update
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
-
-from werkzeug.local import LocalProxy
+from typing import Set, Optional, List, Dict, cast
+from uuid import uuid4
 
 from flask import g, Response
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from sqlalchemy import Column, Integer, String, Boolean, delete, update, Table, select
+from sqlalchemy.exc import IntegrityError
+from werkzeug.local import LocalProxy
 
-from ._db import engine
-from ._config import API_KEY_EXPIRE_AFTER, API_KEY_REQUIRED_STARTING_AT, URL_PREFIX, RATELIMIT_STORAGE_URL
-from ._common import request, app
+from ._common import request, app, db
+from ._config import URL_PREFIX, RATELIMIT_STORAGE_URL, API_KEY_REQUIRED_STARTING_AT
+from ._db import metadata
 from ._exceptions import MissingAPIKeyException, UnAuthenticatedException
 from ._logger import get_structured_logger
-
-
-Base = declarative_base()
-Session = sessionmaker(bind=engine)
-session = Session()
-
-DTIME_NOW = datetime.now()
-DATE_TODAY = date.today()
 
 API_KEY_HARD_WARNING = API_KEY_REQUIRED_STARTING_AT - timedelta(days=14)
 API_KEY_SOFT_WARNING = API_KEY_HARD_WARNING - timedelta(days=14)
 
-API_KEY_WARNING_TEXT = (
-    "an api key will be required starting at {}, go to https://delphi.cmu.edu to request one".format(
-        API_KEY_REQUIRED_STARTING_AT
-    )
+API_KEY_WARNING_TEXT = "an api key will be required starting at {}, go to https://delphi.cmu.edu to request one".format(
+    API_KEY_REQUIRED_STARTING_AT
 )
 
 TESTING_MODE = app.config.get("TESTING", False)
 
+api_user_table = Table(
+    "api_user",
+    metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("api_key", String(50), unique=True),
+    Column("tracking", Boolean, default=True),
+    Column("registered", Boolean, default=False)
+)
 
-class User(Base):
-    __tablename__ = "api_user"
+user_role_table = Table(
+    "user_role",
+    metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("name", String(50), unique=True)
+)
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    api_key = Column(String(50), unique=True)
-    roles = relationship("UserRole", secondary="user_role_link", cascade="all, delete")
-    tracking = Column(Boolean, default=True)
-    registered = Column(Boolean, default=False)
-    creation_date = Column(DateTime)
-    expiration_date = Column(DateTime)
-    last_api_access_date = Column(DateTime)
-
-
-class UserRole(Base):
-    __tablename__ = "user_role"
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(50), unique=True)
-
-
-class UserRoleLink(Base):
-    __tablename__ = "user_role_link"
-
-    user_id = Column(Integer, ForeignKey("api_user.id", ondelete="CASCADE"), primary_key=True)
-    role_id = Column(Integer, ForeignKey("user_role.id", ondelete="CASCADE"), primary_key=True)
+user_role_link_table = Table(
+    "user_role_link",
+    metadata,
+    Column("user_id", Integer, primary_key=True),
+    Column("role_id", Integer, primary_key=True)
+)
 
 
-Base.metadata.create_all(engine)
-
-
-def create_user_role(role_name: str) -> None:
-    user_role = UserRole(name=role_name)
-    session.add(user_role)
-    session.commit()
-
-class AbstractUser:
+class APIUser:
     id: int
     api_key: str
+    authenticated: bool = False
     roles: Set[str] = set()
     tracking: bool = True
     registered: bool = False
-    creation_date: datetime = DTIME_NOW
-    expiration_date: datetime = DTIME_NOW + timedelta(days=API_KEY_EXPIRE_AFTER)
-    last_api_access_date: datetime = DTIME_NOW
 
     @staticmethod
-    def find_user(user_id: Optional[int] = None, api_key: Optional[str] = None):
-        user = None
-        if user_id is not None:
-            user = session.query(User).filter(User.id == user_id).first()
-        if api_key is not None:
-            user = session.query(User).filter(User.api_key == api_key).first()
-        if user is None:
-            return None
-        return user
+    def get_user_roles(user_id: int) -> Set[str]:
+        user_role_ids = db.execute(
+            select(user_role_link_table.c.role_id)
+            .where(user_role_link_table.c.user_id == user_id)
+        ).fetchall()
+        user_roles = db.execute(
+            select(user_role_table.c.name)
+            .where(user_role_table.c.id.in_([role.role_id for role in user_role_ids]))
+        ).fetchall()
+        return set([role.name for role in user_roles])
 
-
-class DBUser(AbstractUser):
     @property
     def roles_str(self):
         return "\n".join(self.roles)
 
     @staticmethod
-    def roles_to_assign(roles: Set[str]):
-        return session.query(UserRole).filter(UserRole.name.in_(roles)).all()
-
-    @staticmethod
-    def assign_new_roles(roles: Set[str], user_id: int):
-        user = session.query(User).filter(User.id == user_id).first()
-        roles_to_assign = DBUser.roles_to_assign(roles)
-        user.roles = []
-        user.roles.extend(roles_to_assign)
-        session.commit()
-        return user
-
-    @staticmethod
-    def _parse(r: Dict) -> "DBUser":
-        user_roles = session.query(User).filter(User.id == r["id"]).first().roles
-        u = DBUser()
+    def _parse(r: Dict) -> "APIUser":
+        u = APIUser()
         u.id = r["id"]
         u.api_key = r["api_key"]
-        u.roles = set(role.name for role in user_roles)
+        u.authenticated = True
+        u.roles = APIUser.get_user_roles(r["id"])
         u.tracking = r["tracking"] != False
         u.registered = r["registered"] == True
-        u.creation_date = r["creation_date"]
-        u.expiration_date = r["expiration_date"]
-        u.last_api_access_date = r["last_api_access_date"]
         return u
 
     @staticmethod
-    def find_user(user_id: Optional[int] = None, api_key: Optional[str] = None) -> Optional["DBUser"]:
-        user = AbstractUser.find_user(user_id, api_key)
+    def find_user(user_id: Optional[int] = None, api_key: Optional[str] = None) -> Optional["APIUser"]:
+        stmt = None
+        if user_id is not None:
+            stmt = api_user_table.select().where(api_user_table.c.id == user_id)
+        if api_key is not None:
+            stmt = api_user_table.select().where(api_user_table.c.api_key == api_key)
+        user = db.execute(stmt).first() if stmt is not None else APIUser.get_anonymous_user()
         if user is None:
-            return None
-        return DBUser._parse(user.__dict__)
+            return APIUser.get_anonymous_user()
+        return APIUser._parse(user) if user.api_key != "anonymous" else user
 
     @staticmethod
-    def list() -> List["DBUser"]:
-        return [DBUser._parse(u.__dict__) for u in session.query(User).all()]
-
-    @staticmethod
-    def insert(
-        api_key: str,
-        roles: Set[str] = set(),
-        tracking: bool = True,
-        registered: bool = False,
-        creation_date: datetime = DTIME_NOW,
-        expiration_date: datetime = DTIME_NOW + timedelta(days=API_KEY_EXPIRE_AFTER),
-        last_api_access_date: datetime = DTIME_NOW,
-    ):
-        new_user = User(
-            api_key=api_key,
-            tracking=tracking,
-            registered=registered,
-            creation_date=creation_date,
-            expiration_date=expiration_date,
-            last_api_access_date=last_api_access_date,
-        )
-        session.add(new_user)
-        session.commit()
-        DBUser.assign_new_roles(roles, new_user.id)
+    def list() -> List["APIUser"]:
+        stmt = api_user_table.select()
+        return [APIUser._parse(u) for u in db.execute(stmt).all()]
 
     @staticmethod
     def register_new_key() -> str:
         api_key = str(uuid4())
-        DBUser.insert(api_key)
+        db.execute(api_user_table.insert().values(api_key=api_key))
         return api_key
 
-    def delete_user(self):
-        stmt = delete(User).filter(User.id == self.id).execution_options(synchronize_session="fetch")
-        session.execute(stmt)
-        session.commit()
+    @staticmethod
+    def assign_user_roles(user_id: int, roles: Set[str]) -> None:
+        if len(roles) == 0:
+            return
+        db.execute(delete(user_role_link_table).where(user_role_link_table.c.user_id == user_id))
+        roles = db.execute(user_role_table.select().where(user_role_table.c.name.in_(roles))).fetchall()
+        for role in roles:
+            stmt = user_role_link_table.insert().values(user_id=user_id, role_id=role.id)
+            db.execute(stmt)
 
-    def update_user(self, api_key: str, roles: Set[str], tracking: bool = True, registered: bool = False) -> "DBUser":
+    @staticmethod
+    def insert_user(api_key: str, roles: Set[str], tracking: bool = True, registered: bool = False):
+        insert_user_stmt = api_user_table.insert().values(api_key=api_key, tracking=tracking, registered=registered)
+        db.execute(insert_user_stmt)
+        u = APIUser.find_user(api_key=api_key)
+        if len(roles) > 0:
+            APIUser.assign_user_roles(u.id, roles)
+        return u
+
+    def delete_user(self) -> None:
+        db.execute(delete(api_user_table).where(api_user_table.c.id == self.id))
+        db.execute(delete(user_role_link_table).where(user_role_link_table.c.user_id == self.id))
+
+    def update_user(self, api_key: str, roles: Set[str], tracking: bool = True, registered: bool = False) -> "APIUser":
         stmt = (
-            update(User)
-            .where(User.id == self.id)
+            update(api_user_table)
+            .where(api_user_table.c.id == self.id)
             .values(api_key=api_key, tracking=tracking, registered=registered)
-            .execution_options(synchronize_session="fetch")
         )
-        session.execute(stmt)
-        session.commit()
-        DBUser.assign_new_roles(roles, self.id)
+        db.execute(stmt)
+        APIUser.assign_user_roles(self.id, roles)
         self.api_key = api_key
+        self.roles = roles
         self.tracking = tracking
         self.registered = registered
-        self.roles = roles
         return self
-
-    def update_last_api_access_date(self) -> None:
-        dtime_now = datetime.now()
-        stmt = (
-            update(User)
-            .where(User.id == self.id)
-            .values(last_api_access_date=dtime_now)
-            .execution_options(synchronize_session="fetch")
-        )
-        session.execute(stmt)
-        session.commit()
-        self.last_api_access_date = dtime_now
-
-
-class APIUser(AbstractUser):
-    def __init__(
-        self,
-        api_key: str,
-        authenticated: bool,
-        roles: Set[str],
-        tracking: bool = True,
-        registered: bool = False,
-        creation_date: datetime = DTIME_NOW,
-        expiration_date: datetime = DTIME_NOW + timedelta(days=API_KEY_EXPIRE_AFTER),
-        last_api_access_date: datetime = DTIME_NOW,
-    ) -> None:
-        self.api_key = api_key
-        self.authenticated = authenticated
-        self.roles = roles
-        self.tracking = tracking
-        self.registered = registered
-        self.creation_date = creation_date
-        self.expiration_date = expiration_date
-        self.last_api_access_date = last_api_access_date
 
     @staticmethod
     def get_anonymous_user():
-        return APIUser("anonymous", False, set())
+        anon_user = APIUser()
+        anon_user.api_key = "anonymous"
+        anon_user.authenticated = False
+        return anon_user
 
-    
     def has_role(self, required_role: str) -> bool:
         return required_role in [role for role in self.roles]
-    
-    @staticmethod
-    def find_user(user_id: Optional[int] = None, api_key: Optional[str] = None) -> "APIUser":
-        user = AbstractUser.find_user(user_id, api_key)
-        if user is None:
-            return APIUser.get_anonymous_user()
-        return APIUser(
-            api_key=user.api_key,
-            authenticated=True,
-            roles=set([role.name for role in user.roles]),
-            tracking=user.tracking,
-            registered=user.registered,
-            creation_date=user.creation_date,
-            expiration_date=user.expiration_date,
-            last_api_access_date=user.last_api_access_date,
-        )
 
     def log_info(self, msg: str, *args, **kwargs) -> None:
         logger = get_structured_logger("api_key_logs", filename="api_keys_log.log")
@@ -262,13 +170,13 @@ def resolve_auth_token() -> Optional[str]:
     for n in ("auth", "api_key", "token"):
         if n in request.values:
             return request.values[n]
-    # user name password
+    # username password
     if request.authorization and request.authorization.username == "epidata":
         return request.authorization.password
     # bearer token authentication
     auth_header = request.headers.get("Authorization")
     if auth_header and auth_header.startswith("Bearer "):
-        return auth_header[len("Bearer ") :]
+        return auth_header[len("Bearer "):]
     return None
 
 
@@ -281,10 +189,11 @@ def mask_apikey(path: str) -> str:
 
 
 def require_api_key() -> bool:
-    return DATE_TODAY >= API_KEY_REQUIRED_STARTING_AT and not TESTING_MODE
+    n = date.today()
+    return n >= API_KEY_REQUIRED_STARTING_AT and not TESTING_MODE
 
 
-def _get_current_user() -> APIUser:
+def _get_current_user():
     if "user" not in g:
         api_key = resolve_auth_token()
         user = APIUser.find_user(api_key=api_key)
@@ -299,20 +208,30 @@ def _get_current_user() -> APIUser:
     return g.user
 
 
-current_user: APIUser = cast(APIUser, LocalProxy(_get_current_user))
+def get_api_user_id(api_key: str) -> int:
+    stmt = api_user_table.select().where(api_user_table.c.api_key == api_key)
+    user_id = db.execute(stmt).first().id
+    return user_id
 
 
 def show_soft_api_key_warning() -> bool:
-    return (
-        not current_user.authenticated
-        and not TESTING_MODE
-        and DATE_TODAY > API_KEY_SOFT_WARNING
-        and DATE_TODAY < API_KEY_HARD_WARNING
-    )
+    n = date.today()
+    return not current_user.authenticated and not TESTING_MODE and API_KEY_SOFT_WARNING < n < API_KEY_HARD_WARNING
 
 
 def show_hard_api_key_warning() -> bool:
-    return not current_user.authenticated and DATE_TODAY > API_KEY_HARD_WARNING and not TESTING_MODE
+    n = date.today()
+    return not current_user.authenticated and n > API_KEY_HARD_WARNING and not TESTING_MODE
+
+
+def register_user_role(role_name: str) -> None:
+    try:
+        db.execute(user_role_table.insert().values(name=role_name))
+    except IntegrityError as e:
+        app.logger.error(e)
+
+
+current_user: APIUser = cast(APIUser, LocalProxy(_get_current_user))
 
 
 def _is_public_route() -> bool:
@@ -329,8 +248,6 @@ def resolve_user():
         return
     try:
         _get_current_user()
-        dbuser = DBUser().find_user(api_key=g.user.api_key)
-        dbuser.update_last_api_access_date()
     except MissingAPIKeyException as e:
         raise e
     except:
@@ -339,6 +256,7 @@ def resolve_user():
             raise MissingAPIKeyException()
         else:
             g.user = APIUser.get_anonymous_user()
+
 
 def require_role(required_role: str):
     def decorator_wrapper(f):
