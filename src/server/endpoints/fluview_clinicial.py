7643299d4b507bc5a232cb6addce2727ab9687dc
Rostyslav Zatserkovnyi <zatserkovnyi.rostyslav@gmail.com>
2023-01-18 18:23:16 +0200

    Fix missing imports (#1067)

--- src/server/endpoints/fluview_clinicial.py
+++ src/server/endpoints/fluview_clinicial.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-from .._params import extract_integers, extract_strings
+from .._params import extract_integer, extract_integers, extract_strings
 from .._query import execute_query, QueryBuilder
 from .._validate import require_all
 
