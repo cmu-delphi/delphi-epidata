aeadd7c891bbbc9939ac7db2986f12cf7bf9cb4b
krivard <krivard@users.noreply.github.com>
2023-02-09 18:24:00 +0000

    chore: release delphi-epidata 0.4.6

--- src/client/delphi_epidata.R
+++ src/client/delphi_epidata.R
@@ -15,7 +15,7 @@ Epidata <- (function() {
   # API base url
   BASE_URL <- 'https://delphi.cmu.edu/epidata/api.php'
 
-  client_version <- '0.4.5'
+  client_version <- '0.4.6'
 
   # Helper function to cast values and/or ranges to strings
   .listitem <- function(value) {
