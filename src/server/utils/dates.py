0eb38a4f289d4dbd8281b4ac3b354c46644e649a
BrainIsDead <pro100Istomin1988@gmail.com>
2023-02-13 18:32:16 +0200

    dates flake8

--- src/server/utils/dates.py
+++ src/server/utils/dates.py
@@ -1,11 +1,5 @@
 from datetime import date, timedelta
-from typing import (
-    Callable,
-    Optional,
-    Sequence,
-    Tuple,
-    Union
-)
+from typing import Callable, Optional, Sequence, Tuple, Union
 
 from epiweeks import Week, Year
 from typing_extensions import TypeAlias
@@ -16,6 +10,7 @@ from .logger import get_structured_logger
 IntRange: TypeAlias = Union[Tuple[int, int], int]
 TimeValues: TypeAlias = Sequence[IntRange]
 
+
 def time_value_to_day(value: int) -> date:
     year, month, day = value // 10000, (value % 10000) // 100, value % 100
     if year < date.min.year:
@@ -23,6 +18,7 @@ def time_value_to_day(value: int) -> date:
     if year > date.max.year:
         return date.max
     return date(year=year, month=month, day=day)
+
 
 def time_value_to_week(value: int) -> Week:
     year, week = value // 100, value % 100
@@ -32,22 +28,28 @@ def time_value_to_week(value: int) -> Week:
         return Week(date.max.year - 1, 1)  # minus 1 since internally it does some checks with a year + 1
     return Week(year=year, week=week)
 
+
 def guess_time_value_is_day(value: int) -> bool:
     # YYYYMMDD type and not YYYYMM
     return len(str(value)) == 8
+
 
 def guess_time_value_is_week(value: int) -> bool:
     # YYYYWW type and not YYYYMMDD
     return len(str(value)) == 6
 
+
 def day_to_time_value(d: date) -> int:
     return int(d.strftime("%Y%m%d"))
+
 
 def week_to_time_value(w: Week) -> int:
     return w.year * 100 + w.week
 
+
 def time_value_to_iso(value: int) -> str:
     return time_value_to_day(value).strftime("%Y-%m-%d")
+
 
 def shift_day_value(time_value: int, days: int) -> int:
     if days == 0:
@@ -56,12 +58,14 @@ def shift_day_value(time_value: int, days: int) -> int:
     shifted = d + timedelta(days=days)
     return day_to_time_value(shifted)
 
+
 def shift_week_value(week_value: int, weeks: int) -> int:
     if weeks == 0:
         return week_value
     week = time_value_to_week(week_value)
     shifted = week + weeks
     return week_to_time_value(shifted)
+
 
 def days_in_range(range: Tuple[int, int]) -> int:
     """
@@ -73,6 +77,7 @@ def days_in_range(range: Tuple[int, int]) -> int:
     delta = end - start
     return delta.days + 1  # same date should lead to 1 day that will be queried
 
+
 def weeks_in_range(week_range: Tuple[int, int]) -> int:
     start = time_value_to_week(week_range[0])
     end = time_value_to_week(week_range[1])
@@ -82,6 +87,7 @@ def weeks_in_range(week_range: Tuple[int, int]) -> int:
         year = Year(y)
         acc += year.totalweeks()
     return acc + 1  # same week should lead to 1 week that will be queried
+
 
 def time_values_to_ranges(values: Optional[TimeValues]) -> Optional[TimeValues]:
     """
@@ -107,11 +113,14 @@ def time_values_to_ranges(values: Optional[TimeValues]) -> Optional[TimeValues]:
         logger.info("Time value unclear, not optimizing", time_value=first_element)
         return values
 
+
 def days_to_ranges(values: TimeValues) -> TimeValues:
     return _to_ranges(values, time_value_to_day, day_to_time_value, timedelta(days=1))
 
+
 def weeks_to_ranges(values: TimeValues) -> TimeValues:
     return _to_ranges(values, time_value_to_week, week_to_time_value, 1)
+
 
 def _to_ranges(values: TimeValues, value_to_date: Callable, date_to_value: Callable, time_unit: Union[int, timedelta]) -> TimeValues:
     try:
@@ -122,7 +131,7 @@ def _to_ranges(values: TimeValues, value_to_date: Callable, date_to_value: Calla
             if isinstance(v, int):
                 # 20200101 -> [20200101, 20200101]
                 intervals.append([value_to_date(v), value_to_date(v)])
-            else: # tuple
+            else:  # tuple
                 # (20200101, 20200102) -> [20200101, 20200102]
                 intervals.append([value_to_date(v[0]), value_to_date(v[1])])
 
@@ -147,7 +156,12 @@ def _to_ranges(values: TimeValues, value_to_date: Callable, date_to_value: Calla
             else:
                 ranges.append((date_to_value(m[0]), date_to_value(m[1])))
 
-        get_structured_logger('server_utils').info("Optimized list of date values", original=values, optimized=ranges, original_length=len(values), optimized_length=len(ranges))
+        get_structured_logger('server_utils').info(
+            "Optimized list of date values",
+            original=values, optimized=ranges,
+            original_length=len(values),
+            optimized_length=len(ranges)
+        )
 
         return ranges
     except Exception as e:
