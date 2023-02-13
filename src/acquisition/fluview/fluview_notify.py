53cea052b37dc7a7245399dd54280368c55e20fa
BrainIsDead <pro100Istomin1988@gmail.com>
2023-02-13 17:57:52 +0200

    fluview_notify flake8

--- src/acquisition/fluview/fluview_notify.py
+++ src/acquisition/fluview/fluview_notify.py
@@ -21,51 +21,46 @@ the automation flow 'New FluView Available'. This will cause, at least:
   + Initial version
 """
 
-# standard library
 import argparse
 
-# third party
+import delphi.operations.secrets as secrets
 import mysql.connector
 
-# first party
-import delphi.operations.secrets as secrets
-
-
 if __name__ == '__main__':
-  # Args and usage
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-t', '--test', action='store_const', const=True, default=False, help="do dry run only, don't update the database")
-  args = parser.parse_args()
+    # Args and usage
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--test', action='store_const', const=True, default=False, help="do dry run only, don't update the database")
+    args = parser.parse_args()
 
-  # connect
-  u, p = secrets.db.epi
-  cnx = mysql.connector.connect(user=u, password=p, database='epidata')
-  cur = cnx.cursor()
+    # connect
+    u, p = secrets.db.epi
+    cnx = mysql.connector.connect(user=u, password=p, database='epidata')
+    cur = cnx.cursor()
 
-  # get the last known issue from the automation table `variables`
-  cur.execute('SELECT `value` FROM automation.`variables` WHERE `name` = %s', ('most_recent_issue',))
-  for (issue1,) in cur:
-    issue1 = int(issue1)
-  print('last known issue:', issue1)
-  # get the most recent issue from the epidata table `fluview`
-  cur.execute('SELECT max(`issue`) FROM `fluview`')
-  for (issue2,) in cur:
-    issue2 = int(issue2)
-  print('most recent issue:', issue2)
+    # get the last known issue from the automation table `variables`
+    cur.execute('SELECT `value` FROM automation.`variables` WHERE `name` = %s', ('most_recent_issue',))
+    for (issue1,) in cur:
+        issue1 = int(issue1)
+    print('last known issue:', issue1)
+    # get the most recent issue from the epidata table `fluview`
+    cur.execute('SELECT max(`issue`) FROM `fluview`')
+    for (issue2,) in cur:
+        issue2 = int(issue2)
+    print('most recent issue:', issue2)
 
-  if issue2 > issue1:
-    print('new data is available!')
-    if args.test:
-      print('test mode - not making any changes')
-    else:
-      # update the variable
-      cur.execute('UPDATE automation.`variables` SET `value` = %s WHERE `name` = %s', (issue2, 'most_recent_issue'))
-      # queue the 'New FluView Available' flow
-      cur.execute('CALL automation.RunStep(36)')
-  elif issue2 < issue2:
-    raise Exception('most recent issue is older than the last known issue')
+    if issue2 > issue1:
+        print('new data is available!')
+        if args.test:
+            print('test mode - not making any changes')
+        else:
+            # update the variable
+            cur.execute('UPDATE automation.`variables` SET `value` = %s WHERE `name` = %s', (issue2, 'most_recent_issue'))
+            # queue the 'New FluView Available' flow
+            cur.execute('CALL automation.RunStep(36)')
+    elif issue2 < issue2:  # TODO: expression has no any sense
+        raise Exception('most recent issue is older than the last known issue')
 
-  # cleanup
-  cnx.commit()
-  cur.close()
-  cnx.close()
+    # cleanup
+    cnx.commit()
+    cur.close()
+    cnx.close()
