382baa5b82488ed2d499ce267dc6fb56d57bf2e4
BrainIsDead <pro100Istomin1988@gmail.com>
2023-02-13 16:52:51 +0200

    network flake8

--- src/acquisition/covid_hosp/common/network.py
+++ src/acquisition/covid_hosp/common/network.py
@@ -1,49 +1,47 @@
-# third party
 import pandas
 
 
 class Network:
-  METADATA_URL_TEMPLATE = \
-      'https://healthdata.gov/api/views/%s/rows.csv'
+    METADATA_URL_TEMPLATE = 'https://healthdata.gov/api/views/%s/rows.csv'
 
-  def fetch_metadata_for_dataset(dataset_id):
-    """Download and return metadata.
+    def fetch_metadata_for_dataset(dataset_id):
+        """Download and return metadata.
 
-    Parameters
-    ----------
-    dataset_id : str
-      healthdata.gov dataset identifier of the dataset.
+        Parameters
+        ----------
+        dataset_id : str
+        healthdata.gov dataset identifier of the dataset.
 
-    Returns
-    -------
-    object
-      The metadata object.
-    """
-    url = Network.METADATA_URL_TEMPLATE % dataset_id
-    print(f'fetching metadata at {url}')
-    df = Network.fetch_dataset(url)
-    df["Update Date"] = pandas.to_datetime(df["Update Date"])
-    df.sort_values("Update Date", inplace=True)
-    df.set_index("Update Date", inplace=True)
-    return df
+        Returns
+        -------
+        object
+        The metadata object.
+        """
+        url = Network.METADATA_URL_TEMPLATE % dataset_id
+        print(f'fetching metadata at {url}')
+        df = Network.fetch_dataset(url)
+        df["Update Date"] = pandas.to_datetime(df["Update Date"])
+        df.sort_values("Update Date", inplace=True)
+        df.set_index("Update Date", inplace=True)
+        return df
 
-  def fetch_dataset(url, pandas_impl=pandas):
-    """Download and return a dataset.
+    def fetch_dataset(url, pandas_impl=pandas):
+        """Download and return a dataset.
 
-    Type inference is disabled in favor of explicit type casting at the
-    database abstraction layer. Pandas behavior is to represent non-missing
-    values as strings and missing values as `math.nan`.
+        Type inference is disabled in favor of explicit type casting at the
+        database abstraction layer. Pandas behavior is to represent non-missing
+        values as strings and missing values as `math.nan`.
 
-    Parameters
-    ----------
-    url : str
-      URL to the dataset in CSV format.
+        Parameters
+        ----------
+        url : str
+        URL to the dataset in CSV format.
 
-    Returns
-    -------
-    pandas.DataFrame
-      The dataset.
-    """
+        Returns
+        -------
+        pandas.DataFrame
+        The dataset.
+        """
 
-    print(f'fetching dataset at {url}')
-    return pandas_impl.read_csv(url, dtype=str)
+        print(f'fetching dataset at {url}')
+        return pandas_impl.read_csv(url, dtype=str)
