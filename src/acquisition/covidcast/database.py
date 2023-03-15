2b2da06a722493cab74d0c01a9cac59132b4873d
Rostyslav Zatserkovnyi <zatserkovnyi.rostyslav@gmail.com>
2023-03-15 19:16:16 +0200

    update review comments & covidcastrow

--- src/acquisition/covidcast/database.py
+++ src/acquisition/covidcast/database.py
@@ -15,7 +15,7 @@ import mysql.connector
 # first party
 import delphi.operations.secrets as secrets
 from delphi.epidata.common.logger import get_structured_logger
-from delphi.epidata.acquisition.covidcast.covidcast_row import CovidcastRow
+from delphi.epidata.common.covidcast_row import CovidcastRow
 
 
 class DBLoadStateException(Exception):
--- tests/acquisition/covidcast/test_covidcast_row.py
+++ tests/acquisition/covidcast/test_covidcast_row.py
@@ -4,7 +4,7 @@ from pandas import DataFrame
 from pandas.testing import assert_frame_equal
 
 from delphi_utils.nancodes import Nans
-from delphi.epidata.acquisition.covidcast.covidcast_row import CovidcastRow, set_df_dtypes
+from delphi.epidata.common.covidcast_row import CovidcastRow, set_df_dtypes
 from delphi.epidata.acquisition.covidcast.test_utils import (
     CovidcastTestRow,
     covidcast_rows_as_api_compatibility_row_df, 
