43a232ec64df927a53a16d8c4b4a128354766d94
LeonLu2 <suhan.lu37@gmail.com>
2023-03-19 22:48:03 -0400

    1. minor changes

--- tests/acquisition/covidcast/test_covidcast_row.py
+++ tests/acquisition/covidcast/test_covidcast_row.py
@@ -12,6 +12,7 @@ from delphi.epidata.acquisition.covidcast.test_utils import (
     covidcast_rows_from_args, 
     transpose_dict, 
 )
+from delphi.epidata.acquisition.covidcast.test_utils import MSA
 
 # py3tester coverage target (equivalent to `import *`)
 __test_target__ = 'delphi.epidata.acquisition.covidcast.covidcast_row'
@@ -24,7 +25,7 @@ class TestCovidcastRows(unittest.TestCase):
         "time_type": ["day"] * 10,
         "geo_type": ["msa"] * 10,
         "time_value": [2021_05_01 + i for i in range(5)] * 2,
-        "geo_value": ["40660"] * 10,
+        "geo_value": [MSA[0]] * 10,
         "value": range(10),
         "stderr": [10.0] * 10,
         "sample_size": [10.0] * 10,
