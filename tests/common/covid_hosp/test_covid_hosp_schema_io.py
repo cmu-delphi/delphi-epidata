0febd0fde4948fbe5ad9efe1efa409eaf3c03ef5
Rostyslav Zatserkovnyi <zatserkovnyi.rostyslav@gmail.com>
2023-03-31 01:22:33 +0300

    ordered csv

--- tests/common/covid_hosp/test_covid_hosp_schema_io.py
+++ tests/common/covid_hosp/test_covid_hosp_schema_io.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 import unittest
 
+from delphi.epidata.acquisition.covid_hosp.common.database import Columndef
+from delphi.epidata.acquisition.covid_hosp.common.utils import Utils
 from delphi.epidata.common.covid_hosp.covid_hosp_schema_io import CovidHospSomething
 
 
@@ -17,4 +19,12 @@ class TestCovidHospSchemaIo(unittest.TestCase):
         assert TABLE_NAME == "covid_hosp_facility"
         assert KEY_COLS == ["hospital_pk", "collection_week"]
         assert AGGREGATE_KEY_COLS == ["address", "ccn", "city", "fips_code", "geocoded_hospital_address", "hhs_ids", "hospital_name", "hospital_pk", "hospital_subtype", "is_metro_micro", "state", "zip"]
-        assert ORDERED_CSV_COLUMNS == []
+        assert ORDERED_CSV_COLUMNS == [
+            Columndef('hospital_pk', 'hospital_pk', str),
+            Columndef('collection_week', 'collection_week', Utils.int_from_date),
+            Columndef('all_adult_hospital_beds_7_day_avg', 'all_adult_hospital_beds_7_day_avg', float),
+            Columndef('all_adult_hospital_beds_7_day_coverage', 'all_adult_hospital_beds_7_day_coverage', int),
+            Columndef('fips_code', 'fips_code', str),
+            Columndef('geocoded_hospital_address', 'geocoded_hospital_address', Utils.limited_geocode),
+            Columndef('is_corrected', 'is_corrected', Utils.parse_bool)
+        ]
