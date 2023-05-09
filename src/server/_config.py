c50e6a78ca445faa3845bc41ed674b04871bf21e
Dmytro Trotsko <dmytrotsko@gmail.com>
2023-05-09 16:10:19 +0300

    Formatting, added AUTH

--- src/server/_config.py
+++ src/server/_config.py
@@ -1,9 +1,9 @@
 import json
 import os
-from dotenv import load_dotenv
-import json
-from enum import Enum
 from datetime import date
+from enum import Enum
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -17,11 +17,10 @@ SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///t
 # defaults
 SQLALCHEMY_ENGINE_OPTIONS = {
     "pool_pre_ping": True,  # enable ping test for validity of recycled pool connections on connect() calls
-    "pool_recycle": 5       # seconds after which a recycled pool connection is considered invalid
+    "pool_recycle": 5,  # seconds after which a recycled pool connection is considered invalid
 }
 # update with overrides of defaults or additions from external configs
-SQLALCHEMY_ENGINE_OPTIONS.update(
-    json.loads(os.environ.get("SQLALCHEMY_ENGINE_OPTIONS", "{}")))
+SQLALCHEMY_ENGINE_OPTIONS.update(json.loads(os.environ.get("SQLALCHEMY_ENGINE_OPTIONS", "{}")))
 
 SECRET = os.environ.get("FLASK_SECRET", "secret")
 URL_PREFIX = os.environ.get("FLASK_PREFIX", "/")
@@ -33,7 +32,7 @@ URL_PREFIX = os.environ.get("FLASK_PREFIX", "/")
 # it should _only_ be set to 'TRUE' if it really is behind a reverse proxy, as remote addresses can be "spoofed"
 # which can carry security/identity implications.  conversely, if this is set to 'FALSE' when in fact
 # running behind a reverse proxy, it can hinder logging accuracy.  it defaults to 'FALSE' for safety.
-REVERSE_PROXIED = (os.environ.get("REVERSE_PROXIED", "FALSE").upper() == 'TRUE')
+REVERSE_PROXIED = os.environ.get("REVERSE_PROXIED", "FALSE").upper() == "TRUE"
 
 REGION_TO_STATE = {
     "hhs1": ["VT", "CT", "ME", "MA", "NH", "RI"],
@@ -58,23 +57,23 @@ REGION_TO_STATE = {
 }
 NATION_REGION = "nat"
 
-API_KEY_REQUIRED_STARTING_AT = date.fromisoformat(os.environ.get('API_REQUIRED_STARTING_AT', '3000-01-01'))
+API_KEY_REQUIRED_STARTING_AT = date.fromisoformat(os.environ.get("API_REQUIRED_STARTING_AT", "3000-01-01"))
 # password needed for the admin application if not set the admin routes won't be available
-ADMIN_PASSWORD = os.environ.get('API_KEY_ADMIN_PASSWORD', 'abc')
+ADMIN_PASSWORD = os.environ.get("API_KEY_ADMIN_PASSWORD", "abc")
 # secret for the google form to give to the admin/register endpoint
-REGISTER_WEBHOOK_TOKEN = os.environ.get('API_KEY_REGISTER_WEBHOOK_TOKEN')
+REGISTER_WEBHOOK_TOKEN = os.environ.get("API_KEY_REGISTER_WEBHOOK_TOKEN")
 
-REDIS_HOST = os.environ.get('REDIS_HOST', 'delphi_redis')
-REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD', '1234')
+REDIS_HOST = os.environ.get("REDIS_HOST", "delphi_redis")
+REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", "1234")
 
 # https://flask-limiter.readthedocs.io/en/stable/#rate-limit-string-notation
-RATE_LIMIT = os.environ.get('RATE_LIMIT', '100/hour')
+RATE_LIMIT = os.environ.get("RATE_LIMIT", "100/hour")
 # fixed-window, fixed-window-elastic-expiry, or moving-window
 # see also https://flask-limiter.readthedocs.io/en/stable/#rate-limiting-strategies
-RATELIMIT_STRATEGY = os.environ.get('RATELIMIT_STRATEGY', 'fixed-window')
+RATELIMIT_STRATEGY = os.environ.get("RATELIMIT_STRATEGY", "fixed-window")
 
 # see https://flask-limiter.readthedocs.io/en/stable/#configuration
-RATELIMIT_STORAGE_URL = f'redis://:{REDIS_PASSWORD}@{REDIS_HOST}:6379'
+RATELIMIT_STORAGE_URL = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:6379"
 
 
 class ConfigUserRoles(str, Enum):
@@ -93,6 +92,7 @@ class ConfigUserRoles(str, Enum):
     sensor_quid = "sensor_quid"
     sensor_wiki = "sensor_wiki"
     twitter = "twitter"
+
 
 # Begin sensor query authentication configuration
 # A multimap of sensor names to the "granular" auth tokens that can be used to access them;
@@ -113,3 +113,15 @@ OPEN_SENSORS = [
     "epic",
     "arch",
 ]
+
+
+AUTH = {
+    "twitter": os.environ.get("SECRET_TWITTER"),
+    "ght": os.environ.get("SECRET_GHT"),
+    "fluview": os.environ.get("SECRET_FLUVIEW"),
+    "cdc": os.environ.get("SECRET_CDC"),
+    "sensors": os.environ.get("SECRET_SENSORS", "322188asdf"),
+    "quidel": os.environ.get("SECRET_QUIDEL"),
+    "norostat": os.environ.get("SECRET_NOROSTAT"),
+    "afhsb": os.environ.get("SECRET_AFHSB"),
+}
