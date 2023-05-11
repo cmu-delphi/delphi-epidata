e98c65383e2c1f7a65f74aec67e12fe6368b454d
Dmytro Trotsko <dmytrotsko@gmail.com>
2023-05-11 15:33:36 +0300

    Unmuted api_key validation

--- src/server/_common.py
+++ src/server/_common.py
@@ -14,7 +14,6 @@ from ._exceptions import DatabaseErrorException, EpiDataException
 from ._security import _get_current_user, _is_public_route, resolve_auth_token, update_key_last_time_used
 
 
-
 app = Flask("EpiData", static_url_path="")
 app.config["SECRET"] = SECRET
 
@@ -26,20 +25,23 @@ def _get_db() -> Connection:
     return g.db
 
 
-def get_real_ip_addr(req): # `req` should be a Flask.request object
-  if REVERSE_PROXIED:
-    # NOTE: ONLY trust these headers if reverse proxied!!!
-    if 'X-Forwarded-For' in req.headers:
-      return req.headers['X-Forwarded-For'].split(',')[0] # take the first (or only) address from the comma-sep list
-    if 'X-Real-Ip' in req.headers:
-      return req.headers['X-Real-Ip']
-  return req.remote_addr
+def get_real_ip_addr(req):  # `req` should be a Flask.request object
+    if REVERSE_PROXIED:
+        # NOTE: ONLY trust these headers if reverse proxied!!!
+        if "X-Forwarded-For" in req.headers:
+            return req.headers["X-Forwarded-For"].split(",")[
+                0
+            ]  # take the first (or only) address from the comma-sep list
+        if "X-Real-Ip" in req.headers:
+            return req.headers["X-Real-Ip"]
+    return req.remote_addr
 
 
 """
 access to the SQL Alchemy connection for this request
 """
 db: Connection = cast(Connection, LocalProxy(_get_db))
+
 
 @event.listens_for(engine, "before_cursor_execute")
 def before_cursor_execute(conn, cursor, statement, parameters, context, executemany):
@@ -56,7 +58,9 @@ def after_cursor_execute(conn, cursor, statement, parameters, context, executema
 
     # Convert to milliseconds
     total_time *= 1000
-    get_structured_logger('server_api').info("Executed SQL", statement=statement, params=parameters, elapsed_time_ms=total_time)
+    get_structured_logger("server_api").info(
+        "Executed SQL", statement=statement, params=parameters, elapsed_time_ms=total_time
+    )
 
 
 @app.before_request
@@ -68,28 +72,34 @@ def before_request_execute():
     api_key = resolve_auth_token()
 
     # TODO: remove after testing -- what does `user` look like in logs?  dont forget, we DONT wanna print `user` info to logs in prod!!!
-    get_structured_logger('server_api').info("USER INFO", user=(user and user.as_dict))
+    get_structured_logger("server_api").info("USER INFO", user=(user and user.as_dict))
 
     # Log statement
-    get_structured_logger('server_api').info("Received API request", method=request.method, url=request.url, form_args=request.form, req_length=request.content_length, remote_addr=request.remote_addr, real_remote_addr=get_real_ip_addr(request), user_agent=request.user_agent.string, api_key=api_key)
+    get_structured_logger("server_api").info(
+        "Received API request",
+        method=request.method,
+        url=request.url,
+        form_args=request.form,
+        req_length=request.content_length,
+        remote_addr=request.remote_addr,
+        real_remote_addr=get_real_ip_addr(request),
+        user_agent=request.user_agent.string,
+        api_key=api_key,
+    )
 
-    # NOTE: if we want to use 'old' password based endpoint authentication, we need to remove this
-    #      as endpoint 'password' comes with `auth` param and then we check for user with that api key
-    #      or we need to add one more check if such 'password' exists in _common.AUTH passwords
-    
-    # if not _is_public_route() and api_key and not user:
-    #     # if this is a privleged endpoint, and an api key was given but it does not look up to a user, raise exception:
-    #     get_structured_logger('server_api').info("bad api key used", api_key=api_key)
-    #     raise Unauthorized("Provided API Key does not exist. Please, check your API Key and try again.")
+    if not _is_public_route() and api_key and not user:
+        # if this is a privleged endpoint, and an api key was given but it does not look up to a user, raise exception:
+        get_structured_logger("server_api").info("bad api key used", api_key=api_key)
+        raise Unauthorized("Provided API Key does not exist. Please, check your API Key and try again.")
 
-    if request.path.startswith('/lib'):
+    if request.path.startswith("/lib"):
         # files served from 'lib' directory don't need the database, so we can exit this early...
         return
     # try to get the db
     try:
         _get_db()
     except Exception as e:
-        get_structured_logger('server_error').error('database connection error', exception=e)
+        get_structured_logger("server_error").error("database connection error", exception=e)
         raise DatabaseErrorException()
 
 
@@ -103,9 +113,23 @@ def after_request_execute(response):
 
     update_key_last_time_used(g.user)
 
-    get_structured_logger('server_api').info('Served API request', method=request.method, url=request.url, form_args=request.form, req_length=request.content_length, remote_addr=request.remote_addr, real_remote_addr=get_real_ip_addr(request), user_agent=request.user_agent.string, api_key=api_key,
-                                             values=request.values.to_dict(flat=False), blueprint=request.blueprint, endpoint=request.endpoint,
-                                             response_status=response.status, content_length=response.calculate_content_length(), elapsed_time_ms=total_time)
+    get_structured_logger("server_api").info(
+        "Served API request",
+        method=request.method,
+        url=request.url,
+        form_args=request.form,
+        req_length=request.content_length,
+        remote_addr=request.remote_addr,
+        real_remote_addr=get_real_ip_addr(request),
+        user_agent=request.user_agent.string,
+        api_key=api_key,
+        values=request.values.to_dict(flat=False),
+        blueprint=request.blueprint,
+        endpoint=request.endpoint,
+        response_status=response.status,
+        content_length=response.calculate_content_length(),
+        elapsed_time_ms=total_time,
+    )
     return response
 
 
@@ -122,9 +146,9 @@ def teardown_db(exception=None):
 def handle_exception(e):
     # Log error and pass through; EpiDataExceptions are HTTPExceptions which are valid WSGI responses (see https://werkzeug.palletsprojects.com/en/2.2.x/exceptions/ )
     if isinstance(e, DatabaseErrorException):
-        get_structured_logger('server_error').error('Received DatabaseErrorException', exception=str(e), exc_info=True)
+        get_structured_logger("server_error").error("Received DatabaseErrorException", exception=str(e), exc_info=True)
     else:
-        get_structured_logger('server_error').warn('Encountered user-side error', exception=str(e))
+        get_structured_logger("server_error").warn("Encountered user-side error", exception=str(e))
     return e
 
 
@@ -132,7 +156,7 @@ def is_compatibility_mode() -> bool:
     """
     checks whether this request is in compatibility mode
     """
-    return 'compatibility' in g and g.compatibility
+    return "compatibility" in g and g.compatibility
 
 
 def set_compatibility_mode():
