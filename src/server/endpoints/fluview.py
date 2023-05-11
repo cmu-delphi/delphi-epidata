6b61aab40b9f80e5db1ddb3710ed6d505a1ea605
Dmytro Trotsko <dmytrotsko@gmail.com>
2023-05-11 15:40:28 +0300

    Fixed fluview endpoint authentication

--- src/server/endpoints/fluview.py
+++ src/server/endpoints/fluview.py
@@ -2,16 +2,10 @@ from typing import Any, Dict, List, Tuple
 
 from flask import Blueprint, request
 
-from .._security import current_user
-from .._params import (
-    extract_integer,
-    extract_integers,
-    extract_strings,
-)
+from .._params import extract_integer, extract_integers, extract_strings
 from .._query import execute_queries, filter_integers, filter_strings
-from .._validate import (
-    require_all,
-)
+from .._security import current_user
+from .._validate import require_all
 
 # first argument is the endpoint name
 bp = Blueprint("fluview", __name__)
@@ -20,7 +14,7 @@ alias = None
 
 @bp.route("/", methods=("GET", "POST"))
 def handle():
-    authorized = current_user.has_role("fluview")
+    authorized = False if current_user is None else current_user.has_role("fluview")
 
     require_all(request, "epiweeks", "regions")
 
