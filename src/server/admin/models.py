6f6e813d52fe127f1561a06f43a8894408d038ef
george haff <george.haff@gmail.com>
2023-05-23 15:17:05 -0400

    fixed scope for db_user

--- src/server/admin/models.py
+++ src/server/admin/models.py
@@ -60,9 +60,12 @@ class User(Base):
 
     @staticmethod
     def _assign_roles(user: "User", roles: Optional[Set[str]], session) -> None:
+        # NOTE: this uses a borrowed/existing `session`, and thus does not do a `session.commit()`...
+        #       that is the responsibility of the caller!
         get_structured_logger("api_user_models").info("setting roles", roles=roles, user_id=user.id, api_key=user.api_key)
+        db_user = session.query(User).filter(User.id == user.id).first()
+        # TODO: would it be sufficient to use the passed-in `user` instead of looking up this `db_user`?
         if roles:
-            db_user = session.query(User).filter(User.id == user.id).first() 
             roles_to_assign = session.query(UserRole).filter(UserRole.name.in_(roles)).all()
             db_user.roles = roles_to_assign
         else:
@@ -72,6 +75,7 @@ class User(Base):
     def find_user(*, # asterisk forces explicit naming of all arguments when calling this method
         user_id: Optional[int] = None, api_key: Optional[str] = None, user_email: Optional[str] = None
     ) -> "User":
+        # NOTE: be careful, using multiple arguments could match multiple users, but this will return only one!
         # TODO
         with Session() as session:
             user = (
@@ -87,6 +91,7 @@ class User(Base):
         get_structured_logger("api_user_models").info("creating user", api_key=api_key)
         with Session() as session:
             new_user = User(api_key=api_key, email=email)
+            # TODO: do we need to populate 'created' field/column here?
             session.add(new_user)
             User._assign_roles(new_user, user_roles, session)
             session.commit()
