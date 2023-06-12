ccfbd6e4e35e6e8e2f4cfc69e7615e7c9890845d
Rostyslav Zatserkovnyi <zatserkovnyi.rostyslav@gmail.com>
2023-06-13 00:38:10 +0300

    Automatically parse columns from YAML file (#1114)
    * Automatically parse columns from YAML file
    
    * Use constructor
    
    * try alt filename
    
    * Try absolute path
    
    * AGGREGATE_KEY_COLS
    
    * what if none aggregate?
    
    * fix table name
    
    * tests
    
    * ordered csv
    
    * use cols method
    
    * try pkgutil
    
    * cleanup unneeded methods
    
    * Review fixes
    
    * Revert to old filename processing
    
    * thread the name of the yaml schemadefs file through the whole process, and readd method to write that file
    
    Co-authored-by: rzats <zatserkovnyi.rostyslav@gmail.com>
    
    ---------
    
    Co-authored-by: george haff <george.haff@gmail.com>

--- src/common/covid_hosp/covid_hosp_schema_io.py
+++ src/common/covid_hosp/covid_hosp_schema_io.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from pathlib import Path
 import re
 import sys
 
-# UNCOMMENT: from delphi.epidata.acquisition.covid_hosp.common.utils import Utils
+from delphi.epidata.acquisition.covid_hosp.common.utils import Utils
+from delphi.epidata.acquisition.covid_hosp.common.database import Columndef
 
 # ruamel preserves key ordering, comments, and some formatting for a "round trip" of a yaml file import-->export
 from ruamel.yaml.main import (
@@ -19,16 +21,14 @@ RoundTripRepresenter.add_representer(type(None), RoundTripRepresenter.represent_
 
 class CovidHospSomething:
 
-  YAML_FILENAME = 'covid_hosp_schemadefs.yaml'
-
   PYTHON_TYPE_MAPPING = {
     'int': int,
     'float': float,
     'str': str,
-    'fixedstr', str,
-    'bool': int, # UNCOMMENT: Utils.parse_bool,
-    'intdate': int, # UNCOMMENT: Utils.int_from_date,
-    'geocode': str, # UNCOMMENT: Utils.limited_geocode,
+    'fixedstr': str,
+    'bool': Utils.parse_bool,
+    'intdate': Utils.int_from_date,
+    'geocode': Utils.limited_geocode,
   }
 
   SQL_TYPE_MAPPING = {
@@ -54,22 +54,22 @@ class CovidHospSomething:
   MYSQL_COL_NAME_PATTERN = re.compile(r'^[a-zA-Z0-9_]{3,64}$')
 
 
-  def __init__(self):
-    self.read_schemadefs()
+  def __init__(self, yaml_filename=None):
+    if not yaml_filename:
+      # the default schema definitions yaml file is a member of the current package
+      # TODO: use `importlib_resources` instead of generic file Path stuff to load this, once we are past python 3.8
+      yaml_filename = str(Path(__file__).parent.absolute()) + "/covid_hosp_schemadefs.yaml"
+    self.yaml_filename = yaml_filename
 
-
-  def read_schemadefs(self):
-    # TODO: put the yaml file inside the package structure and access it there, with something like:
-    #   from importlib import resources
-    #   import delphi.epidata.common.covid_hosp
-    #   self.yaml_content = resources.read_text(delphi.epidata.common.covid_hosp, YAML_FILENAME)
-    with open(CovidHospSomething.YAML_FILENAME, 'r') as yaml_file:
+    with open(yaml_filename, 'r') as yaml_file:
       self.yaml_content = yaml_load(yaml_file, preserve_quotes=True)
-    return self.yaml_content
 
 
-  def write_schemadefs(self, filename=CovidHospSomething.YAML_FILENAME):
-    with open(filename, 'w') as yaml_file:
+  def write_schemadefs(self, yaml_filename=None):
+    if not yaml_filename:
+      yaml_filename = self.yaml_filename
+
+    with open(yaml_filename, 'w') as yaml_file:
       # NOTE: `width` specification is to prevent dump from splitting long lines
       # TODO: consider `block_seq_indent=2` to make list under ORDERED_CSV_COLUMNS look a little better
       yaml_dump(self.yaml_content, yaml_file, width=200)
@@ -93,7 +93,7 @@ class CovidHospSomething:
       else:
         dtype = dtype_cplx
         col_width = None
-      yield {'name': name, 'sql_name': sql_name, 'dtype': dtype, 'col_width:' col_width, 'marshaller': CovidHospSomething.PYTHON_TYPE_MAPPING[dtype]}
+      yield {'name': name, 'sql_name': sql_name, 'dtype': dtype, 'col_width': col_width, 'marshaller': CovidHospSomething.PYTHON_TYPE_MAPPING[dtype]}
 
 
   def add_column(self, ds_name, col_name, dtype, sql_name=None, col_width=None):
@@ -119,6 +119,32 @@ class CovidHospSomething:
     self.dataset(ds_name)['ORDERED_CSV_COLUMNS'].append([dtype_cplx, col_name, sql_name])
 
 
+  def get_ds_table_name(self, ds_name):
+    return self.dataset(ds_name)['TABLE_NAME']
+
+
+  def get_ds_key_cols(self, ds_name):
+    return self.dataset(ds_name)['KEY_COLS']
+
+
+  def get_ds_aggregate_key_cols(self, ds_name):
+    return self.dataset(ds_name).get('AGGREGATE_KEY_COLS', None)
+
+
+  def get_ds_ordered_csv_cols(self, ds_name):
+    return [
+      Columndef(
+        # Original name for the column
+        column['name'],
+        # Updated name for the column; same name if it's not specified in the YAML files
+        column['sql_name'],
+        # Datatype for the column, ignoring SQL column lengths: e.g. fixedstr:1 -> fixedstr -> str
+        column['marshaller']
+      )
+      for column in self.columns(ds_name)
+    ]
+
+
   def detect_changes(self, ds_name):
     # TODO
     ds = self.dataset(ds_name)
@@ -131,20 +157,21 @@ class CovidHospSomething:
 
 
   def write_new_definitions(self):
+
+    # write updated yaml file
+    self.write_schemadefs()
+
+    # then write out the ddl and migration files
+
     today_str = datetime.now().strftime("%Y_%m_%d")
 
-    yaml_file = CovidHospSomething.YAML_FILENAME
     ddl_file = 'covid_hosp.sql'
     migration_file = f"covid_hosp_v{today_str}.sql"
 
     # TODO: do these with proper python `pathlib.Path` objects
-    repo_root = './'
-    yaml_file_directory = repo_root + 'src/ddl/'
+    repo_root = './' # TODO: get this from somewhere
     ddl_file_directory = repo_root + 'src/ddl/'
     migration_file_directory = repo_root + 'src/ddl/migrations/'
-
-    # write updated yaml file
-    self.write_schemadefs()
 
     # write newly generated sql definition file
     with open(ddl_file, 'w') as f:
@@ -158,7 +185,6 @@ class CovidHospSomething:
       f.write("\n")
 
     # move files into proper locations
-    # UNCOMMENT: os.system(f"mv -f {yaml_file} {yaml_file_directory}")
     # UNCOMMENT: os.system("mv -f {ddl_file} {ddl_file_directory}")
     # UNCOMMENT: os.system("mv -f {migration_file} {migration_file_directory")
 
@@ -168,7 +194,7 @@ if __name__ == "__main__":
   chs = CovidHospSomething()
   changed = False
 
-  for ds_name in chs.datasets():
+  for ds_name in chs.dataset_names():
     ds = chs.dataset(ds_name)
     new_cols = chs.detect_changes(ds_name)
     if new_cols:
---
+++ tests/common/covid_hosp/test_covid_hosp_schema_io.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import unittest
+
+from delphi.epidata.acquisition.covid_hosp.common.database import Columndef
+from delphi.epidata.acquisition.covid_hosp.common.utils import Utils
+from delphi.epidata.common.covid_hosp.covid_hosp_schema_io import CovidHospSomething
+
+
+# py3tester coverage target (equivalent to `import *`)
+__test_target__ = "delphi.epidata.common.covid_hosp.covid_hosp_schema_io"
+
+
+class TestCovidHospSchemaIo(unittest.TestCase):
+    chs = CovidHospSomething(str(Path(__file__).parent.absolute()) + "/test_covid_hosp_schemadefs.yaml")
+
+    def test_get_ds_info(self):
+        TABLE_NAME = self.chs.get_ds_table_name('covid_hosp_facility')
+        KEY_COLS = self.chs.get_ds_key_cols('covid_hosp_facility')
+        AGGREGATE_KEY_COLS = self.chs.get_ds_aggregate_key_cols('covid_hosp_facility')
+        ORDERED_CSV_COLUMNS = self.chs.get_ds_ordered_csv_cols('covid_hosp_facility')
+
+        assert TABLE_NAME == "covid_hosp_facility"
+        assert KEY_COLS == ["hospital_pk", "collection_week"]
+        assert AGGREGATE_KEY_COLS == ["address", "ccn", "city", "fips_code", "geocoded_hospital_address", "hhs_ids", "hospital_name", "hospital_pk", "hospital_subtype", "is_metro_micro", "state", "zip"]
+        assert ORDERED_CSV_COLUMNS == [
+            Columndef('hospital_pk', 'hospital_pk', str),
+            Columndef('collection_week', 'collection_week', Utils.int_from_date),
+            Columndef('reporting_cutoff_start', 'date', Utils.int_from_date),
+            Columndef('all_adult_hospital_beds_7_day_avg', 'all_adult_hospital_beds_7_day_avg', float),
+            Columndef('all_adult_hospital_beds_7_day_coverage', 'all_adult_hospital_beds_7_day_coverage', int),
+            Columndef('fips_code', 'fips_code', str),
+            Columndef('geocoded_hospital_address', 'geocoded_hospital_address', Utils.limited_geocode),
+            Columndef('is_corrected', 'is_corrected', Utils.parse_bool)
+        ]
