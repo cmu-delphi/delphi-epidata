07ed83e5768f717ab0f9a62a9209e4e2cffa058d
Dmitry Shemetov <dshemetov@ucdavis.edu>
2023-06-21 14:08:59 -0700

    style(black): format twitter acquisition

--- src/acquisition/twtr/twitter_update.py
+++ src/acquisition/twtr/twitter_update.py
@@ -1,4 +1,4 @@
-'''
+"""
 ===============
 === Purpose ===
 ===============
@@ -49,7 +49,7 @@ total: the total number of tweets (denominator)
   * Small documentation update
 2015-05-22
   * Original version
-'''
+"""
 
 # third party
 import mysql.connector
@@ -60,46 +60,55 @@ import delphi.operations.secrets as secrets
 
 
 def run():
-  # connect to the database
-  u, p = secrets.db.epi
-  cnx = mysql.connector.connect(user=u, password=p, database='epidata')
-  cur = cnx.cursor()
+    # connect to the database
+    u, p = secrets.db.epi
+    cnx = mysql.connector.connect(user=u, password=p, database="epidata")
+    cur = cnx.cursor()
 
-  def get_num_rows():
-    cur.execute('SELECT count(1) `num` FROM `twitter`')
-    for (num,) in cur:
-      pass
-    return num
+    def get_num_rows():
+        cur.execute("SELECT count(1) `num` FROM `twitter`")
+        for (num,) in cur:
+            pass
+        return num
 
-  # check from 7 days preceeding the last date with data through yesterday (healthtweets.org 404's if today's date is part of the range)
-  cur.execute('SELECT date_sub(max(`date`), INTERVAL 7 DAY) `date1`, date_sub(date(now()), INTERVAL 1 DAY) `date2` FROM `twitter`')
-  for (date1, date2) in cur:
-    date1, date2 = date1.strftime('%Y-%m-%d'), date2.strftime('%Y-%m-%d')
-  print('Checking dates between %s and %s...'%(date1, date2))
+    # check from 7 days preceeding the last date with data through yesterday (healthtweets.org 404's if today's date is part of the range)
+    cur.execute(
+        "SELECT date_sub(max(`date`), INTERVAL 7 DAY) `date1`, date_sub(date(now()), INTERVAL 1 DAY) `date2` FROM `twitter`"
+    )
+    for (date1, date2) in cur:
+        date1, date2 = date1.strftime("%Y-%m-%d"), date2.strftime("%Y-%m-%d")
+    print(f"Checking dates between {date1} and {date2}...")
 
-  # keep track of how many rows were added
-  rows_before = get_num_rows()
+    # keep track of how many rows were added
+    rows_before = get_num_rows()
 
-  # check healthtweets.org for new and/or revised data
-  ht = HealthTweets(*secrets.healthtweets.login)
-  sql = 'INSERT INTO `twitter` (`date`, `state`, `num`, `total`) VALUES (%s, %s, %s, %s) ON DUPLICATE KEY UPDATE `num` = %s, `total` = %s'
-  total_rows = 0
-  for state in sorted(HealthTweets.STATE_CODES.keys()):
-    values = ht.get_values(state, date1, date2)
-    for date in sorted(list(values.keys())):
-      sql_data = (date, state, values[date]['num'], values[date]['total'], values[date]['num'], values[date]['total'])
-      cur.execute(sql, sql_data)
-      total_rows += 1
+    # check healthtweets.org for new and/or revised data
+    ht = HealthTweets(*secrets.healthtweets.login)
+    sql = "INSERT INTO `twitter` (`date`, `state`, `num`, `total`) VALUES (%s, %s, %s, %s) ON DUPLICATE KEY UPDATE `num` = %s, `total` = %s"
+    total_rows = 0
+    for state in sorted(HealthTweets.STATE_CODES.keys()):
+        values = ht.get_values(state, date1, date2)
+        for date in sorted(list(values.keys())):
+            sql_data = (
+                date,
+                state,
+                values[date]["num"],
+                values[date]["total"],
+                values[date]["num"],
+                values[date]["total"],
+            )
+            cur.execute(sql, sql_data)
+            total_rows += 1
 
-  # keep track of how many rows were added
-  rows_after = get_num_rows()
-  print('Inserted %d/%d row(s)'%(rows_after - rows_before, total_rows))
+    # keep track of how many rows were added
+    rows_after = get_num_rows()
+    print("Inserted %d/%d row(s)" % (rows_after - rows_before, total_rows))
 
-  # cleanup
-  cur.close()
-  cnx.commit()
-  cnx.close()
+    # cleanup
+    cur.close()
+    cnx.commit()
+    cnx.close()
 
 
-if __name__ == '__main__':
-  run()
+if __name__ == "__main__":
+    run()
