d63f63f45880a451360b657762f19da5490b0acb
Dmytro Trotsko <dmytrotsko@gmail.com>
2023-06-30 21:46:50 +0300

    Updated send_notification() function

--- src/maintenance/remove_outdated_keys.py
+++ src/maintenance/remove_outdated_keys.py
@@ -1,10 +1,19 @@
 from collections import namedtuple
+from smtplib import SMTP
 
 import delphi.operations.secrets as secrets
 import mysql.connector
-
+from delphi.epidata.server._config import API_KEY_REGISTRATION_FORM_LINK_LOCAL
 
 API_USER_RECORD = namedtuple("APIUser", ("api_key", "email", "date_diff"))
+
+SMTP_HOST = "relay.andrew.cmu.edu"
+SMTP_PORT = 25
+
+EMAIL_SUBJECT = "Your API Key was deleted."
+EMAIL_FROM = "noreply@andrew.cmu.edu"
+EMAIL_MESSAGE = f"""Hi! \n Your API Key was removed due to inactivity.
+To get new one, please use registration form ({API_KEY_REGISTRATION_FORM_LINK_LOCAL}) or contact us."""
 
 
 def get_outdated_keys(cur):
@@ -39,8 +48,10 @@ def remove_outdated_key(cur, api_key):
     )
 
 
-def send_notification(api_key, email):
-    pass
+def send_notification(to_addr):
+    BODY = "\r\n".join((f"FROM: {EMAIL_FROM}", f"TO: {to_addr}", f"Subject: {EMAIL_SUBJECT}", "", EMAIL_MESSAGE))
+    smtp_server = SMTP(host=SMTP_HOST, port=SMTP_PORT)
+    smtp_server.sendmail(EMAIL_FROM, to_addr, BODY)
 
 
 def main():
