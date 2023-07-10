59385b99262d831284b8a087fbd67d8c081f5b9e
melange396 <george.haff@gmail.com>
2023-07-10 17:13:11 -0400

    http caching for metadata (#1222)
    allow http caching of metadata endpoint (plus a small margin), and removed staleness logging

--- src/server/endpoints/covidcast_meta.py
+++ src/server/endpoints/covidcast_meta.py
@@ -26,51 +26,9 @@ class SourceSignal:
         return f"{self.source}:{self.signal}"
 
 
-def fetch_data(
-    time_types: Optional[List[str]],
-    geo_types: Optional[List[str]],
-    signals: Optional[List[SourceSignal]],
-):
-    # complain if the cache is more than 75 minutes old
-    max_age = 75 * 60
-
-    row = db.execute(
-        text(
-            "SELECT UNIX_TIMESTAMP(NOW()) - timestamp AS age, epidata FROM covidcast_meta_cache LIMIT 1"
-        )
-    ).fetchone()
-
-    if not row or not row["epidata"]:
-        get_structured_logger('server_api').warning("no data in covidcast_meta cache")
-        return
-
-    age = row["age"]
-    if age > max_age and row["epidata"]:
-        get_structured_logger('server_api').warning("covidcast_meta cache is stale", cache_age=age)
-
-    epidata = loads(row["epidata"])
-
-    if not epidata:
-        return
-
-    def filter_row(row: Dict):
-        if time_types and row.get("time_type") not in time_types:
-            return False
-        if geo_types and row.get("geo_type") not in geo_types:
-            return False
-        if not signals:
-            return True
-        for signal in signals:
-            # match source and (signal or no signal or signal = *)
-            if row.get("data_source") == signal.source and (
-                signal.signal == "*" or signal.signal == row.get("signal")
-            ):
-                return True
-        return False
-
-    for row in epidata:
-        if filter_row(row):
-            yield row
+# empty generator that never yields
+def _nonerator():
+    return (x for x in [])
 
 
 @bp.route("/", methods=("GET", "POST"))
@@ -79,4 +37,60 @@ def handle():
     signals = [SourceSignal(v) for v in (extract_strings("signals") or [])]
     geo_types = extract_strings("geo_types")
 
-    return create_printer(request.values.get("format"))(filter_fields(fetch_data(time_types, geo_types, signals)))
+    printer = create_printer(request.values.get("format"))
+
+    metadata = db.execute(
+        text(
+            "SELECT UNIX_TIMESTAMP(NOW()) - timestamp AS age, epidata FROM covidcast_meta_cache LIMIT 1"
+        )
+    ).fetchone()
+
+    if not metadata or "epidata" not in metadata:
+        # the db table `covidcast_meta_cache` has no rows
+        get_structured_logger('server_api').warning("no data in covidcast_meta cache")
+        return printer(_nonerator())
+
+    metadata_list = loads(metadata["epidata"])
+
+    if not metadata_list:
+        # the db table has a row, but there is no metadata about any signals in it
+        get_structured_logger('server_api').warning("empty entry in covidcast_meta cache")
+        return printer(_nonerator())
+
+    # the expected metadata regeneration interval in seconds, aka time between runs of
+    # src/acquisition/covidcast/covidcast_meta_cache_updater.py (currently 2h)
+    standard_age = 2 * 60 * 60
+    # a short period when a client can continue to use this metadata even if its slightly stale,
+    # which also gives some padding if the md generation is running slow,
+    # and which also acts as a minimum cacheable time (currently 10 mins)
+    age_margin = 10 * 60
+    # these should be updated if a stale cache will have undue impact on user activities, such as
+    # if we start updating the metadata table much more frequently and having up-to-the-minute
+    # metadata accuracy becomes important to users once more.
+    # TODO: get the above two values ^ from config vars?
+    age = metadata["age"]
+    reported_age = max(0, min(age, standard_age) - age_margin)
+
+    def cache_entry_gen():
+        for entry in metadata_list:
+            if time_types and entry.get("time_type") not in time_types:
+                continue
+            if geo_types and entry.get("geo_type") not in geo_types:
+                continue
+            if not signals:
+                yield entry
+            for signal in signals:
+                # match source and (signal or no signal or signal = *)
+                if entry.get("data_source") == signal.source and (
+                    signal.signal == "*" or signal.signal == entry.get("signal")
+                ):
+                    yield entry
+
+    return printer(
+        filter_fields(cache_entry_gen()),
+        headers={
+            "Cache-Control": f"max-age={standard_age}, public",
+            "Age": f"{reported_age}",
+            # TODO?: "Expires": f"{}", # superseded by Cache-Control: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Expires
+        }
+    )
