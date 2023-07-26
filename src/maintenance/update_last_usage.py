5475310e4be6656ddabe50fa0883378a8ee90fc1
Dmytro Trotsko <dmytrotsko@gmail.com>
2023-07-26 21:56:46 +0300

    Removed pattern arg in redis_get_last_time_used and replaced it with global LAST_USED_KEY_PATTERN. Deleted  function and moved filtering login to the update query in

--- src/maintenance/update_last_usage.py
+++ src/maintenance/update_last_usage.py
@@ -10,26 +10,12 @@ REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", "1234")
 LAST_USED_KEY_PATTERN = "*LAST_USED*"
 
 
-def redis_get_last_time_used(redis_cli, pattern: str):
-    keys = redis_cli.keys(pattern=pattern)
+def redis_get_last_time_used(redis_cli):
+    keys = redis_cli.keys(pattern=LAST_USED_KEY_PATTERN)
     key_val_pairs = {
         str(key).split("/")[1]: dtime.strptime(str(redis_cli.get(key)), "%Y-%m-%d").date() for key in keys
     }
     return key_val_pairs
-
-
-def db_get_last_time_used(api_key: str, cur):
-    cur.execute(
-        f"""
-        SELECT
-            last_time_used
-        FROM api_user
-        WHERE api_key = "{api_key}"
-        LIMIT 1
-        """
-    )
-    last_time_used = cur.fetchone()
-    return last_time_used[0]
 
 
 def main():
@@ -39,16 +25,14 @@ def main():
     cnx = mysql.connector.connect(database="epidata", user=u, password=p, host=secrets.db.host)
     cur = cnx.cursor()
     for api_key, last_time_used in key_val_pairs.items():
-        db_last_time_used = db_get_last_time_used(api_key, cur)
-        if db_last_time_used == None or last_time_used > db_last_time_used:
-            cur.execute(
-                f"""
-                UPDATE
-                    api_user
-                SET last_time_used = "{last_time_used}"
-                WHERE api_key = "{api_key}"
-            """
-            )
+        cur.execute(
+            f"""
+            UPDATE
+                api_user
+            SET last_time_used = "{last_time_used}"
+            WHERE api_key = "{api_key}" AND (last_time_used < "{last_time_used}" OR last_time_used IS NULL)
+        """
+        )
     cur.close()
     cnx.commit()
     cnx.close()
