9497203207abdcbc18ff87c9970b0022dc26c2e2
Dmytro Trotsko <dmytrotsko@gmail.com>
2023-07-27 19:23:23 +0300

    Changed variables names. Added additional level of notifications.

--- src/maintenance/remove_outdated_keys.py
+++ src/maintenance/remove_outdated_keys.py
@@ -5,18 +5,20 @@ import delphi.operations.secrets as secrets
 import mysql.connector
 from delphi.epidata.server._config import API_KEY_REGISTRATION_FORM_LINK_LOCAL
 
-API_USER_RECORD = namedtuple("APIUser", ("api_key", "email", "date_diff"))
+ApiUserRecord = namedtuple("APIUserRecord", ("api_key", "email", "date_diff"))
 
 SMTP_HOST = "relay.andrew.cmu.edu"
 SMTP_PORT = 25
 
 EMAIL_SUBJECT = "Your API Key was deleted."
 EMAIL_FROM = "noreply@andrew.cmu.edu"
-EMAIL_MESSAGE = f"""Hi! \n Your API Key was removed due to inactivity.
+ALERT_EMAIL_MESSAGE = f"""Hi! \n Your API Key is going to be removed due to inactivity.
+To renew it, pelase use it within one month from now."""
+DELETED_EMAIL_MESSAGE = f"""Hi! \n Your API Key was removed due to inactivity.
 To get new one, please use registration form ({API_KEY_REGISTRATION_FORM_LINK_LOCAL}) or contact us."""
 
 
-def get_outdated_keys(cur):
+def get_old_keys(cur):
     cur.execute(
         """
             SELECT
@@ -37,7 +39,7 @@ def get_outdated_keys(cur):
         """
     )
     outdated_keys = cur.fetchall()
-    return outdated_keys
+    return [ApiUserRecord(*item) for item in outdated_keys]
 
 
 def remove_outdated_key(cur, api_key):
@@ -48,8 +50,9 @@ def remove_outdated_key(cur, api_key):
     )
 
 
-def send_notification(to_addr):
-    BODY = "\r\n".join((f"FROM: {EMAIL_FROM}", f"TO: {to_addr}", f"Subject: {EMAIL_SUBJECT}", "", EMAIL_MESSAGE))
+def send_notification(to_addr, alert=True):
+    message = ALERT_EMAIL_MESSAGE if alert else DELETED_EMAIL_MESSAGE
+    BODY = "\r\n".join((f"FROM: {EMAIL_FROM}", f"TO: {to_addr}", f"Subject: {EMAIL_SUBJECT}", "", message))
     smtp_server = SMTP(host=SMTP_HOST, port=SMTP_PORT)
     smtp_server.sendmail(EMAIL_FROM, to_addr, BODY)
 
@@ -58,12 +61,13 @@ def main():
     u, p = secrets.db.epi
     cnx = mysql.connector.connect(database="epidata", user=u, password=p, host=secrets.db.host)
     cur = cnx.cursor()
-    outdated_keys_list = [API_USER_RECORD(*item) for item in get_outdated_keys(cur)]
+    outdated_keys_list = get_old_keys(cur)
     for item in outdated_keys_list:
         if item.date_diff == 5:
             send_notification(item.email)
         else:
             remove_outdated_key(cur, item.api_key)
+            send_notification(item.email, alert=False)
     cur.close()
     cnx.commit()
     cnx.close()
