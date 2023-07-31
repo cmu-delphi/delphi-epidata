c023a13ec337b58dea6cba8a3a7caa63a04a0174
Dmytro Trotsko <dmytrotsko@gmail.com>
2023-07-31 15:51:42 +0300

    Simplified query to get keys where last_used date >= 5 month. Added .starttls() to the smtp server.

--- src/maintenance/remove_outdated_keys.py
+++ src/maintenance/remove_outdated_keys.py
@@ -21,21 +21,12 @@ To get new one, please use registration form ({API_KEY_REGISTRATION_FORM_LINK_LO
 def get_old_keys(cur):
     cur.execute(
         """
-            SELECT
-                diff.api_key,
-                diff.email,
-                diff.date_diff
-            FROM (
-                SELECT
-                    api_key,
-                    email,
-                    created,
-                    last_time_used,
-                    ABS(TIMESTAMPDIFF(MONTH, last_time_used, created)) as date_diff
-                FROM api_user
-            ) diff
-            WHERE diff.date_diff >= 5;
-
+        SELECT
+            api_key,
+            email,
+            TIMESTAMPDIFF(MONTH, last_time_used, NOW()) AS date_diff
+        FROM api_user
+        HAVING date_diff >= 5;
         """
     )
     outdated_keys = cur.fetchall()
@@ -54,6 +45,7 @@ def send_notification(to_addr, alert=True):
     message = ALERT_EMAIL_MESSAGE if alert else DELETED_EMAIL_MESSAGE
     BODY = "\r\n".join((f"FROM: {EMAIL_FROM}", f"TO: {to_addr}", f"Subject: {EMAIL_SUBJECT}", "", message))
     smtp_server = SMTP(host=SMTP_HOST, port=SMTP_PORT)
+    smtp_server.starttls()
     smtp_server.sendmail(EMAIL_FROM, to_addr, BODY)
 
 
