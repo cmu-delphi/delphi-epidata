eba110b24b8427e0c98e9124edd6a0269b184b49
Dmytro Trotsko <dmytrotsko@gmail.com>
2023-09-26 16:10:27 +0300

    Changed base class. Removed unncessary code. Replaced Epidata calls with internal self.epidata_client

--- integrations/server/test_fluview_meta.py
+++ integrations/server/test_fluview_meta.py
@@ -1,50 +1,15 @@
 """Integration tests for the `fluview_meta` endpoint."""
 
-# standard library
-import unittest
-
-# third party
-import mysql.connector
-
 # first party
-from delphi.epidata.client.delphi_epidata import Epidata
+from delphi.epidata.common.delphi_test_base import DelphiTestBase
 
 
-class FluviewMetaTests(unittest.TestCase):
+class FluviewMetaTests(DelphiTestBase):
   """Tests the `fluview_meta` endpoint."""
 
-  @classmethod
-  def setUpClass(cls):
-    """Perform one-time setup."""
-
-    # use the local instance of the Epidata API
-    Epidata.BASE_URL = 'http://delphi_web_epidata/epidata'
-    Epidata.auth = ('epidata', 'key')
-
-  def setUp(self):
+  def localSetUp(self):
     """Perform per-test setup."""
-
-    # connect to the `epidata` database and clear the `fluview` table
-    cnx = mysql.connector.connect(
-        user='user',
-        password='pass',
-        host='delphi_database_epidata',
-        database='epidata')
-    cur = cnx.cursor()
-    cur.execute('truncate table fluview')
-    cur.execute('delete from api_user')
-    cur.execute('insert into api_user(api_key, email) values ("key", "email")')
-    cnx.commit()
-    cur.close()
-
-    # make connection and cursor available to test cases
-    self.cnx = cnx
-    self.cur = cnx.cursor()
-
-  def tearDown(self):
-    """Perform per-test teardown."""
-    self.cur.close()
-    self.cnx.close()
+    self.truncate_tables_list = ["fluview"]
 
   def test_round_trip(self):
     """Make a simple round-trip with some sample data."""
@@ -64,7 +29,7 @@ class FluviewMetaTests(unittest.TestCase):
     self.cnx.commit()
 
     # make the request
-    response = Epidata.fluview_meta()
+    response = self.epidata_client.fluview_meta()
 
     # assert that the right data came back
     self.assertEqual(response, {
