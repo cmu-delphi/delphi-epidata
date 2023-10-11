ecfa3049a60e0202c45fd396d4689a80596b5e96
george haff <george.haff@gmail.com>
2023-10-11 17:16:43 -0400

    cruft removal attempt #1

--- dev/docker/python/setup.sh
+++ dev/docker/python/setup.sh
@@ -12,16 +12,8 @@
 # bail if anything fails
 set -e
 
-# create python package `undefx`
-mkdir undefx
-mv repos/undefx/py3tester/src undefx/py3tester
-mv repos/undefx/undef-analysis undefx/undef_analysis
-
 # create python package `delphi`
 mkdir delphi
 mv repos/delphi/operations/src delphi/operations
 mv repos/delphi/utils/src delphi/utils
-mv repos/delphi/github-deploy-repo/src delphi/github_deploy_repo
 mv repos/delphi/delphi-epidata/src delphi/epidata
-mv repos/delphi/flu-contest/src delphi/flu_contest
-mv repos/delphi/nowcast/src delphi/nowcast
