254692a6c233630ca5ea5336b7d258922b26cf10
Brian Clark <clark.bg@gmail.com>
2023-11-14 14:22:38 -0500

    Updates to Sentry SDK config and env vars
    - Set `debug` and `attach_stacktrace` to `False`
    - Reordered a bit
    - Added a decent way to make an env var work as a boolean
    - Better formatting (spaces around `=`)

--- src/server/main.py
+++ src/server/main.py
@@ -18,12 +18,12 @@ from ._limiter import limiter, apply_limit
 SENTRY_DSN = os.environ.get('SENTRY_DSN')
 if SENTRY_DSN:
     sentry_sdk.init(
-        dsn=SENTRY_DSN,
-        traces_sample_rate=float(os.environ.get('SENTRY_TRACES_SAMPLE_RATE', 1.0)),
-        profiles_sample_rate=float(os.environ.get('SENTRY_PROFILES_SAMPLE_RATE', 1.0)),
-        environment=str(os.environ.get('SENTRY_ENVIRONMENT', 'development')),
-        debug=str(os.environ.get('SENTRY_DEBUG', 'True')),
-        attach_stacktrace=str(os.environ.get('SENTRY_ATTACH_STACKTRACE', 'True'))
+        dsn = SENTRY_DSN,
+        environment = os.environ.get('SENTRY_ENVIRONMENT', 'development'),
+        profiles_sample_rate = float(os.environ.get('SENTRY_PROFILES_SAMPLE_RATE', 1.0)),
+        traces_sample_rate = float(os.environ.get('SENTRY_TRACES_SAMPLE_RATE', 1.0)),
+        attach_stacktrace = os.environ.get('SENTRY_ATTACH_STACKTRACE', 'False').lower() in ('true', '1', 't'),
+        debug = os.environ.get('SENTRY_DEBUG', 'False').lower() in ('true', '1', 't')
     )
 
 
