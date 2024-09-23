7720a24bec074ec743b45651e966b836779dbf5a
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2024-09-23 17:16:48 -0400

    construct urls automatically

--- src/acquisition/rvdss/constants.py
+++ src/acquisition/rvdss/constants.py
@@ -44,36 +44,45 @@ GEOS = {
  }
 
 REGIONS = ['atlantic','atl','at','province of québec','québec','qc','province of ontario','ontario','on',
-            'prairies', 'pr', "british columbia", 'bc',"territories",'terr']
+            'prairies', 'pr', "british columbia",'bc',"territories",'terr']
 NATION = ["canada","can",'ca']
 
-DASHBOARD_BASE_URLS_2023=["https://health-infobase.canada.ca/src/data/respiratory-virus-detections/archive/2024-06-20/",
-"https://health-infobase.canada.ca/src/data/respiratory-virus-detections/archive/2024-06-27/",
-"https://health-infobase.canada.ca/src/data/respiratory-virus-detections/archive/2024-07-04/",
-"https://health-infobase.canada.ca/src/data/respiratory-virus-detections/archive/2024-07-11/",
-"https://health-infobase.canada.ca/src/data/respiratory-virus-detections/archive/2024-07-18/",
-"https://health-infobase.canada.ca/src/data/respiratory-virus-detections/archive/2024-08-01/",
-"https://health-infobase.canada.ca/src/data/respiratory-virus-detections/archive/2024-08-08/",
-"https://health-infobase.canada.ca/src/data/respiratory-virus-detections/archive/2024-08-15/",
-"https://health-infobase.canada.ca/src/data/respiratory-virus-detections/archive/2024-08-22/",
-"https://health-infobase.canada.ca/src/data/respiratory-virus-detections/archive/2024-08-29/",
-"https://health-infobase.canada.ca/src/data/respiratory-virus-detections/archive/2024-09-05/"]
-
-HISTORIC_SEASON_URL = ["https://www.canada.ca/en/public-health/services/surveillance/respiratory-virus-detections-canada/2013-2014.html",
-"https://www.canada.ca/en/public-health/services/surveillance/respiratory-virus-detections-canada/2014-2015.html",
-"https://www.canada.ca/en/public-health/services/surveillance/respiratory-virus-detections-canada/2015-2016.html",
-"https://www.canada.ca/en/public-health/services/surveillance/respiratory-virus-detections-canada/2016-2017.html",
-"https://www.canada.ca/en/public-health/services/surveillance/respiratory-virus-detections-canada/2017-2018.html",
-"https://www.canada.ca/en/public-health/services/surveillance/respiratory-virus-detections-canada/2018-2019.html",
-"https://www.canada.ca/en/public-health/services/surveillance/respiratory-virus-detections-canada/2019-2020.html",
-"https://www.canada.ca/en/public-health/services/surveillance/respiratory-virus-detections-canada/2020-2021.html",
-"https://www.canada.ca/en/public-health/services/surveillance/respiratory-virus-detections-canada/2021-2022.html",
-"https://www.canada.ca/en/public-health/services/surveillance/respiratory-virus-detections-canada/2022-2023.html",
-"https://www.canada.ca/en/public-health/services/surveillance/respiratory-virus-detections-canada/2023-2024.html"]
-
-ALTERNATIVE_SEASON_BASE_URL = "www.phac-aspc.gc.ca/bid-bmi/dsd-dsm/rvdi-divr/"
+DASHBOARD_BASE_URL_2023 = "https://health-infobase.canada.ca/src/data/respiratory-virus-detections/archive/{date}/"
+DASHBOARD_BASE_URLS_2023 = (
+    DASHBOARD_BASE_URL_2023.format(date = date) for date in
+    (
+        "2024-06-20",
+        "2024-06-27",
+        "2024-07-04",
+        "2024-07-11",
+        "2024-07-18",
+        "2024-08-01",
+        "2024-08-08",
+        "2024-08-15",
+        "2024-08-22",
+        "2024-08-29",
+        "2024-09-05"
+    )
+)
 
 SEASON_BASE_URL = "https://www.canada.ca"
+ALTERNATIVE_SEASON_BASE_URL = "www.phac-aspc.gc.ca/bid-bmi/dsd-dsm/rvdi-divr/"
+HISTORIC_SEASON_REPORTS_URL + "/en/public-health/services/surveillance/respiratory-virus-detections-canada/{year_range}.html"
+
+HISTORIC_SEASON_URL = (HISTORIC_SEASON_REPORTS_URL.format(year_range = year_range) for year_range in
+    (
+        "2013-2014",
+        "2014-2015",
+        "2015-2016",
+        "2016-2017",
+        "2017-2018",
+        "2018-2019",
+        "2019-2020",
+        "2020-2021",
+        "2021-2022",
+        "2022-2023",
+        "2023-2024"
+    )
+)
 
 LAST_WEEK_OF_YEAR = 35
-
