c0742fcb39fa3f8926d161ec8afc864baf15b41f
cchuong <cchuong2000@gmail.com>
2025-01-24 10:54:02 -0800

    Update test_utils.py

--- tests/acquisition/rvdss/test_utils.py
+++ tests/acquisition/rvdss/test_utils.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from delphi.epidata.acquisition.rvdss.utils import abbreviate_virus
+from delphi.epidata.acquisition.rvdss.utils import abbreviate_virus, create_geo_types
 
 # py3tester coverage target
 __test_target__ = "delphi.epidata.acquisition.rvdss.utils"
@@ -16,3 +16,9 @@ class TestUtils:
     def test_abbreviate_virus(self):
     	assert abbreviate_virus("influenza") == "flu" # normal case
     	assert abbreviate_virus("flu") == "flu" # already abbreviated
+
+    def test_create_geo_types(self):
+        assert create_geo_types("canada","lab") == "nation"
+        assert create_geo_types("bc","lab") == "region"
+        assert create_geo_types("random lab","lab") == "lab"
+        assert create_geo_types("Canada","province") == "province" #lowercase handling happens upstream
