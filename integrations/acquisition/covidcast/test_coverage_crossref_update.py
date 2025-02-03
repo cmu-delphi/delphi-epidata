16048457c95f67c6e0a5ab179e551c7efbbb3346
Nolan Gormley <nolangormley@gmail.com>
2025-02-03 14:02:19 -0500

    added some test cases

--- integrations/acquisition/covidcast/test_coverage_crossref_update.py
+++ integrations/acquisition/covidcast/test_coverage_crossref_update.py
@@ -14,7 +14,7 @@ from delphi.epidata.client.delphi_epidata import Epidata
 import delphi.operations.secrets as secrets
 import delphi.epidata.acquisition.covidcast.database as live
 from delphi.epidata.maintenance.coverage_crossref_updater import main
-from delphi.epidata.acquisition.covidcast.test_utils import CovidcastBase
+from delphi.epidata.acquisition.covidcast.test_utils import CovidcastBase, CovidcastTestRow
 
 # use the local instance of the Epidata API
 BASE_URL = 'http://delphi_web_epidata/epidata' # NOSONAR
@@ -28,9 +28,7 @@ class CoverageCrossrefTests(CovidcastBase):
     self._db._cursor.execute('TRUNCATE TABLE `coverage_crossref`')
 
   @staticmethod
-  def _make_request(params=None):
-    if params is None:
-        params = {'geo': 'state:*'}
+  def _make_request(params):
     response = requests.get(f"{Epidata.BASE_URL}/covidcast/geo_coverage", params=params, auth=Epidata.auth)
     response.raise_for_status()
     return response.json()
@@ -45,7 +43,7 @@ class CoverageCrossrefTests(CovidcastBase):
         CovidcastTestRow.make_default_row(geo_type="state", geo_value="ny", signal="sig2"),
     ])
 
-    results = self._make_request()
+    results = self._make_request(params = {'geo': 'state:*'})
 
     # make sure the tables are empty
     self.assertEqual(results, {
@@ -57,12 +55,12 @@ class CoverageCrossrefTests(CovidcastBase):
     # update the coverage crossref table
     main()
 
-    results = self._make_request()
+    results = self._make_request(params = {'geo': 'state:*'})
 
     # make sure the data was actually served
     self.assertEqual(results, {
       'result': 1,
-      'epidata': [{'signal': 'sig', 'source': 'src'}],
+      'epidata': [{'signal': 'sig', 'source': 'src'}, {'signal': 'sig2', 'source': 'src'}],
       'message': 'success',
     })
 
@@ -83,3 +81,13 @@ class CoverageCrossrefTests(CovidcastBase):
       'epidata': [{'signal': 'sig', 'source': 'src'}],
       'message': 'success',
     })
+
+    results = self._make_request(params = {'geo': 'state:ny'})
+
+    # make sure the data was actually served
+    self.assertEqual(results, {
+      'result': 1,
+      'epidata': [{'signal': 'sig', 'source': 'src'}, {'signal': 'sig2', 'source': 'src'}],
+      'message': 'success',
+    })
+
