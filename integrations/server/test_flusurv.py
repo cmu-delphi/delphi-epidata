34c2b6e1e34a027e81705c07b97e3bdc5ea49919
nmdefries <42820733+nmdefries@users.noreply.github.com>
2025-10-27 17:57:33 -0400

    add season field to test

--- integrations/server/test_flusurv.py
+++ integrations/server/test_flusurv.py
@@ -15,9 +15,20 @@ class FlusurvTest(DelphiTestBase):
         self.cur.execute(
             """
             INSERT INTO `flusurv`(
-                `release_date`, `issue`, `epiweek`, `location`, `lag`,
-                `rate_age_0`, `rate_age_1`, `rate_age_2`, `rate_age_3`, `rate_age_4`,
-                `rate_overall`, `rate_age_5`, `rate_age_6`, `rate_age_7`,
+                `release_date`,
+                `issue`,
+                `epiweek`,
+                `location`,
+                `lag`,
+                `rate_age_0`,
+                `rate_age_1`,
+                `rate_age_2`,
+                `rate_age_3`,
+                `rate_age_4`,
+                `rate_overall`,
+                `rate_age_5`,
+                `rate_age_6`,
+                `rate_age_7`,
                 `rate_age_18t29`,
                 `rate_age_30t39`,
                 `rate_age_40t49`,
@@ -36,15 +47,16 @@ class FlusurvTest(DelphiTestBase):
                 `rate_sex_male`,
                 `rate_sex_female`,
                 `rate_flu_a`,
-                `rate_flu_b`
+                `rate_flu_b`,
+                `season`
             ) VALUES(
                 %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
-                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
             )
             """,
             (
-                "2012-11-02", "201243", "201143", "CA", "52", "0", "0", "0", "0.151", "0", "0.029", "0", "0", "0",
-                "2.54", "0", "1", "0", "0", "0", "0", "0", "0.68", "0.46", "1", "1", "0", "0", "0", "99", "0", "0", "22.2"
+                "2012-11-02", 201243, 201143, "CA", 52, 0, 0, 0, 0.151, 0, 0.029, 0, 0, 0,
+                2.54, 0, 1, 0, 0, 0, 0, 0, 0.68, 0.46, 1, 1, 0, 0, 0, 99, 0, 0, 22.2, "2012-13"
             ),
         )
         self.cnx.commit()
@@ -58,6 +70,7 @@ class FlusurvTest(DelphiTestBase):
                         "location": "CA",
                         "issue": 201243,
                         "epiweek": 201143,
+                        "season": "2012-13",
                         "lag": 52,
                         "rate_age_0": 0.0,
                         "rate_age_1": 0.0,
